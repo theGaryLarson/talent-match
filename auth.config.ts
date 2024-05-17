027817819c656d49e3c9376486d63767798b6e6b
Keith McAfee <megrims@gmail.com>
2024-05-17 14:31:51 -0700

    added careers root

--- auth.config.ts
+++ auth.config.ts
@@ -7,12 +7,12 @@ export const authConfig = {
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
-      const isOnDashboard = nextUrl.pathname.startsWith('/employers');
+      const isOnDashboard = nextUrl.pathname.startsWith('/careers/employers');
       if (isOnDashboard) {
         if (isLoggedIn) return true;
         return false; // Redirect unauthenticated users to login page
       } else if (isLoggedIn) {
-        return Response.redirect(new URL('/employers/dashboard', nextUrl));
+        return Response.redirect(new URL('/careers/employers/dashboard', nextUrl));
       }
       return true;
     },
