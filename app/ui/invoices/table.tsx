b50be8733aefe9cd28b0b5e593d4dd28e95353d9
not-damien <93546644+not-damien@users.noreply.github.com>
2024-05-17 14:36:29 -0700

    added career service dashboard

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -15,7 +15,7 @@ export default async function InvoicesTable({
 
   return (
     <div className="mt-6 flow-root">
-      <div className="inline-block min-w-full align-middle">
+      {/* <div className="inline-block min-w-full align-middle">
         <div className="rounded-lg bg-gray-50 p-2 md:pt-0">
           <div className="md:hidden">
             {invoices?.map((invoice) => (
@@ -118,7 +118,7 @@ export default async function InvoicesTable({
             </tbody>
           </table>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
