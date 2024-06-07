31c636cada830d74648497eed2c917b14055f3e6
not-damien <93546644+not-damien@users.noreply.github.com>
2024-06-07 16:36:08 -0700

    clean up list view

--- app/ui/components/PhotoCardWithTitle.tsx
+++ app/ui/components/PhotoCardWithTitle.tsx
@@ -5,7 +5,7 @@ export default function PhotoCardWithTitle({title, article}:{title:String; artic
             
             <Image className="rounded-t-xl" width={375} height={212} src="/cfa_images/stock/christina-wocintechchat-com-bPVM4nOy0Rg-unsplash 1.png" alt={""}/>
             <div className="p-5">
-                <div className="w-[241px] h-[61px] absolute left-0 top-[140px] bg-white/[.75] rounded-tr-md rounded-br-md flex items-center" ><h3 className="text-xl font-bold px-3">{title}</h3></div>
+                <div className="w-[200px] h-[61px] absolute left-0 top-[140px] bg-white/[.75] rounded-tr-md rounded-br-md flex items-center" ><h3 className="text-xl font-bold px-3">{title}</h3></div>
             <p>{article}</p>
             <p><strong>Something Catchy Goes Here</strong></p>
             <p>Lorem ipsum dolor sit amet, consectetur</p>
