907d5c5bb687628b41914a86131dca6bf4eccf62
not-damien <93546644+not-damien@users.noreply.github.com>
2024-06-07 11:36:06 -0700

    fix

--- app/services/employers/dashboard/listview/page.tsx
+++ app/services/employers/dashboard/listview/page.tsx
@@ -3,13 +3,10 @@ import JobSeekerCardView from "@/app/ui/components/JobSeekerCardView";
 export default function page(){
     return(
         <main className="py-16 px-[200px] space-y-8">
-            <JobSeekerCardView/>
-            <JobSeekerCardView/>
-            <JobSeekerCardView/>
-            <JobSeekerCardView/>
-            <JobSeekerCardView/>
-            <JobSeekerCardView/>
-            <JobSeekerCardView/>
+            <JobSeekerCardView isLarge={true} name={"Damien Cruz"} school={"University Of Washington"} pathway={"Web Devolpment"} skillsList={["java", "rust","react","figma"]}/>
+            <JobSeekerCardView isLarge={true} name={"Damien Cruz"} school={"University Of Washington"} pathway={"Web Devolpment"} skillsList={["java", "rust","react","figma"]}/>
+            <JobSeekerCardView isLarge={true} name={"Damien Cruz"} school={"University Of Washington"} pathway={"Web Devolpment"} skillsList={["java", "rust","react","figma"]}/>
+            <JobSeekerCardView isLarge={true} name={"Damien Cruz"} school={"University Of Washington"} pathway={"Web Devolpment"} skillsList={["java", "rust","react","figma"]}/>
         </main>
     );
 }
