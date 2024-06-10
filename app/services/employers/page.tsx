d7324005e8f2c7295e51c92753b42fdd4099191b
not-damien <93546644+not-damien@users.noreply.github.com>
2024-06-10 13:13:29 -0700

    content update

--- app/services/employers/page.tsx
+++ app/services/employers/page.tsx
@@ -1,6 +1,7 @@
 //employer landing page
 import BlockQuote from "@/app/ui/components/BlockQuote";
 import GreyBackgroundBulletPointListBlock from "@/app/ui/components/GreyBackgroundBulletPointListBlock";
+import LargeRoundedButtonCard from "@/app/ui/components/LargeRoundedButtonCard";
 import PageBanner from "@/app/ui/components/PageBannner"
 import PhotoCardWithTitle from "@/app/ui/components/PhotoCardWithTitle";
 import Tabs from "@/app/ui/components/Tabs";
@@ -14,7 +15,7 @@ export default function Page(){
           content: <VideoBlock/>,
         },
         {
-          label: 'Training & Development',
+          label: 'Growth',
           content: <div className="flex space-x-20"><GreyBackgroundBulletPointListBlock/><GreyBackgroundBulletPointListBlock/></div>,
         },
         {
@@ -26,11 +27,11 @@ export default function Page(){
             content: <div>Content of Tab 4</div>,
         },
         {
-            label: 'Post Jobs',
+            label: 'Recruitment',
             content: <div>Content of Tab 5</div>,
         },
         {
-          label: 'Company Presence',
+          label: 'Visibility',
           content: <div>Content of Tab 6</div>,
       },
       ];
@@ -38,19 +39,21 @@ export default function Page(){
     return(
         <>
         <PageBanner title={"About Becoming a partner"} bg="bg-employer-hero-1"/>
-        <main className="px-6 md:px-12 py-16 lg:px-24 space-y-8">
-            <strong>Become a partner</strong>
+        <main className="px-6 md:px-12 py-16 lg:px-24 space-y-4">
+            <h1 className="text-2xl">Become a partner</h1>
             <p>
             Computing for All partners with Washington state community and technical colleges and training providers to source diverse students and job seekers for tech careers. We also train candidates in professional skill development to ensure they are career-ready.
             </p> 
+            
             <div className="container mx-auto">
               <Tabs tabs={tabs} />
-              
             </div>
-            <BlockQuote text={"This program provided top-notch training with state-of-the-art equipment, truly preparing me for the real world. A big thank you to the team!"} author={"James Lee"} imgSrc={"/cfa_images/stock/Mask group.png"}/>
-            <p>
-            <strong>Hire the right candidate</strong>
-            </p>
+
+
+            <BlockQuote text={"Partnering with Computing for All brought us an outstanding intern, Maria, whose fresh ideas and dedication have been invaluable to our team."} author={"James Lee"} imgSrc={"/cfa_images/stock/Mask group.png"}/>
+            <h1 className="text-2xl">
+            Hire the right candidate
+            </h1>
             <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat.
             </p>
@@ -59,7 +62,7 @@ export default function Page(){
             <PhotoCardWithTitle title={"Interns"} article={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."}/>
             <PhotoCardWithTitle title={"Apprentice"} article={"Here is a b unch of random information that will be replaced yada yada radda radda"}/>
             </div>
-            
+            <LargeRoundedButtonCard title={"Ready to Hire"} blurb={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris."} buttonContent={"Search For talent"} />
 
         </main>
         </>
--- app/ui/components/LargeRoundedButtonCard.tsx
+++ app/ui/components/LargeRoundedButtonCard.tsx
@@ -1,11 +1,11 @@
 import RoundedButton from "./RoundedButton";
 
-export default function LargeRoundedButtonCard(){
+export default function LargeRoundedButtonCard(props:{title:string, blurb:string, buttonContent:string}){
     return(
         <div className="flex w-full bg-gray-100 flex-col space-y-7 rounded-lg border px-5 py-5 shadow">
-            <h3 className="font-bold">Search for Candidates</h3>
-            <p>Meet pre-screened, diverse candidates through CFA’s career services and events, or get involved to help shape your future workforce.</p>
-            <RoundedButton content={"Search for Talent"} link={"/services/employers/dashboard/listview"} invertColor={false}/>
+            <h3 className="font-bold text-xl">{props.title}</h3>
+            <p>{props.blurb}</p>
+            <RoundedButton content={props.buttonContent} link={"/services/employers/dashboard/listview"} invertColor={false}/>
         </div>
     );
 }
