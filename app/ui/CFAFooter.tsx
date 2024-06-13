031e1fa18245d42ed50120813fe0831865bb676d
not-damien <93546644+not-damien@users.noreply.github.com>
2024-06-13 14:16:24 -0700

    added reverse logo

--- app/ui/CFAFooter.tsx
+++ app/ui/CFAFooter.tsx
@@ -1,8 +1,9 @@
 import CFALogo from "./CFALogo"
-
+import Image from "next/image";
 export default function CFAFooter(){
     return(
-        <div className="w-full h-16 bg-blue-background text-white ">
+        <div className="flex items-center w-full bg-blue-background text-white ">
+            <Image src="/cfa_images/CFA logo_reverse 1.svg" alt={"CFA Logo"} width={150} height={75}/>
             <p className="text-center">Terms of Services | Privacy Policy | Cookie Settings </p>
         </div>
     );
