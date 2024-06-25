8a46f60eca0118aae52d02ebf7ef0583241fddf0
not-damien <93546644+not-damien@users.noreply.github.com>
2024-06-25 13:05:01 -0700

    basic get using prisma

--- app/ui/components/Avatar.tsx
+++ app/ui/components/Avatar.tsx
@@ -1,7 +1,7 @@
 import { AcademicCapIcon, UserCircleIcon } from "@heroicons/react/24/outline";
 import Image from "next/image";
 
-export default function Avatar({imgsrc}:{imgsrc?:string;}){
+export default function Avatar({imgsrc}:{imgsrc?:string|null;}){
     return(
        <div className="w-[85px] h-[85px] flex justify-center items-center">
        {imgsrc?<img src={imgsrc} alt={""} className="rounded-full object-cover w-[85px] h-[85px]"/>:<UserCircleIcon/>}
