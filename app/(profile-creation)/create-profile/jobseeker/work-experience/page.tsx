1649646b5140b8fcba0217f3a31b4272b1324fda
cfa-jonathan <91294137+cfa-jonathan@users.noreply.github.com>
2024-06-26 12:18:00 -0700

    Implement Work Experience page buttons adding experience groups. Create TextareaWithLabel component to match other form field designs. Update InputTextWithLabel component to accept disabled attribute.

--- app/(profile-creation)/create-profile/jobseeker/work-experience/page.tsx
+++ app/(profile-creation)/create-profile/jobseeker/work-experience/page.tsx
@@ -1,13 +1,158 @@
 'use client';
 
-import React from 'react';
+import React, { useState } from 'react';
 import ProgressBarFlat from '@/app/ui/components/ProgressBarFlat';
-import InputTextWithLabel from '@/app/ui/components/InputTextWithLabel';
-import SelectOptionsWithLabel from '@/app/ui/components/SelectOptionsWithLabel';
 import { MdAdd } from "react-icons/md";
-import { Button, Label, Progress, Radio } from "flowbite-react";
+import { Button, Checkbox, Label, Radio, Textarea } from "flowbite-react";
+import InputTextWithLabel from '../../../../ui/components/InputTextWithLabel';
+import TextareaWithLabel from '../../../../ui/components/TextareaWithLabel';
+
+interface WorkExperienceGroupProps {
+  groupNumber: number,
+}
+
+function WorkExperienceGroup({groupNumber}:WorkExperienceGroupProps){
+  const [isCurrent, setCurrent] = useState(false);
+
+  return (
+    <div>
+      <h3>Experience {groupNumber}</h3>
+      <InputTextWithLabel
+        id={"profile-creation-work-experience-group-" + groupNumber + "-company"}
+        className="w-full"
+        placeholder="Your company name"
+        required
+      >
+        Company *
+      </InputTextWithLabel>
+      <InputTextWithLabel
+        id={"profile-creation-work-experience-group-" + groupNumber + "-title"}
+        className="w-full"
+        placeholder="Your title"
+        required
+      >
+        Title *
+      </InputTextWithLabel>
+      <div className="flex">
+        <InputTextWithLabel
+          type="month"
+          id={"profile-creation-work-experience-group-" + groupNumber + "-starts"}
+          className="w-1/2"
+          required={(isCurrent)?false:true}
+          disabled={(isCurrent)?true:false}
+        >
+          Starts *
+        </InputTextWithLabel>
+        <InputTextWithLabel
+          type="month"
+          id={"profile-creation-work-experience-group-" + groupNumber + "-ends"}
+          className="w-1/2"
+          required={(isCurrent)?false:true}
+          disabled={(isCurrent)?true:false}
+        >
+          Ends *
+        </InputTextWithLabel>
+      </div>
+      <Label>
+        <Checkbox
+          id={"profile-creation-work-experience-group-" + groupNumber + "-current"}
+          onClick={()=>setCurrent(!isCurrent)}
+        />
+        Current
+      </Label>
+      <TextareaWithLabel
+        id={"profile-creation-work-experience-group-" + groupNumber + "-experience"}
+        placeholder="Your specific experience"
+        required
+      >
+        Experience *
+      </TextareaWithLabel>
+    </div>
+  );
+}
+
+interface InternshipExperienceGroupProps {
+  groupNumber: number,
+}
+
+function InternshipExperienceGroup({groupNumber}:InternshipExperienceGroupProps){
+  const [isCurrent, setCurrent] = useState(false);
+
+  return (
+    <div>
+      <h3>Experience {groupNumber}</h3>
+      <InputTextWithLabel
+        id={"profile-creation-internship-experience-group-" + groupNumber + "-company"}
+        className="w-full"
+        placeholder="Your company name"
+        required
+      >
+        Company *
+      </InputTextWithLabel>
+      <InputTextWithLabel
+        id={"profile-creation-internship-experience-group-" + groupNumber + "-title"}
+        className="w-full"
+        placeholder="Your title"
+        required
+      >
+        Title *
+      </InputTextWithLabel>
+      <div className="flex">
+        <InputTextWithLabel
+          type="month"
+          id={"profile-creation-internship-experience-group-" + groupNumber + "-starts"}
+          className="w-1/2"
+          required={(isCurrent)?false:true}
+          disabled={(isCurrent)?true:false}
+        >
+          Starts *
+        </InputTextWithLabel>
+        <InputTextWithLabel
+          type="month"
+          id={"profile-creation-internship-experience-group-" + groupNumber + "-ends"}
+          className="w-1/2"
+          required={(isCurrent)?false:true}
+          disabled={(isCurrent)?true:false}
+        >
+          Ends *
+        </InputTextWithLabel>
+      </div>
+      <Label>
+        <Checkbox
+          id={"profile-creation-internship-experience-group-" + groupNumber + "-current"}
+          onClick={()=>setCurrent(!isCurrent)}
+        />
+        Current
+      </Label>
+      <TextareaWithLabel
+        id={"profile-creation-internship-experience-group-" + groupNumber + "-experience"}
+        placeholder="Your specific experience"
+        required
+      >
+        Experience *
+      </TextareaWithLabel>
+    </div>
+  );
+}
 
 export default function CreateJobseekerProfileWorkExperiencePage(){
+  const [workExperienceGroups, setWorkExperienceGroups] = useState<React.ReactNode[]>([]);
+  const [internshipExperienceGroups, setInternshipExperienceGroups] = useState<React.ReactNode[]>([]);
+
+  function addNewWorkExperienceGroup() {
+    setWorkExperienceGroups((prevGroups) => [
+      ...prevGroups,
+      <WorkExperienceGroup key={'WorkGroupKey'+(prevGroups.length+1)} groupNumber={prevGroups.length+1}/>
+    ])
+  }
+
+  function addNewInternshipExperienceGroup() {
+    setInternshipExperienceGroups((prevGroups) => [
+      ...prevGroups,
+      <InternshipExperienceGroup key={'InternshipGroupKey'+(prevGroups.length+1)} groupNumber={prevGroups.length+1}/>
+    ])
+  }
+
   return(
     <main className="flex">
       <aside className="hidden lg:w-2/5 lg:block">
@@ -22,7 +167,25 @@ export default function CreateJobseekerProfileWorkExperiencePage(){
             <legend>
               <h2>Work experience</h2>
             </legend>
-            <Button pill color="gray">
+            {
+              (workExperienceGroups.length === 0)?
+                ""
+              :
+                <InputTextWithLabel
+                  type="number"
+                  id="profile-creation-experience-work-fulltime-years"
+                >
+                  How many years of full-time work experience do you have (not including internship)?
+                </InputTextWithLabel>
+            }
+            {
+              workExperienceGroups
+            }
+            <Button
+              pill
+              color="gray"
+              onClick={addNewWorkExperienceGroup}
+            >
               <MdAdd className="mr-2 h-5 w-5"/>
               Add work experience
             </Button>
@@ -31,7 +194,25 @@ export default function CreateJobseekerProfileWorkExperiencePage(){
             <legend>
               <h2>Internship experience</h2>
             </legend>
-            <Button pill color="gray">
+            {
+              (internshipExperienceGroups.length === 0)?
+                ""
+              :
+                <InputTextWithLabel
+                  type="number"
+                  id="profile-creation-experience-internship-years"
+                >
+                  How many years of internship work experience do you have?
+                </InputTextWithLabel>
+            }
+            {
+              internshipExperienceGroups
+            }
+            <Button
+              pill
+              color="gray"
+              onClick={addNewInternshipExperienceGroup}
+            >
               <MdAdd className="mr-2 h-5 w-5"/>
               Add internship experience
             </Button>
