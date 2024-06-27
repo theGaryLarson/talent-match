ce820c8afdd756a2b70a047de5fecb274efe05ae
quincyp <quincytpham@gmail.com>
2024-06-27 13:51:15 -0400

    completed non-dynamic skeleton of preferences page

--- app/(profile-creation)/create-profile/jobseeker/preferences/page.tsx
+++ app/(profile-creation)/create-profile/jobseeker/preferences/page.tsx
@@ -18,6 +18,26 @@ export default function CreateJobseekerProfilePreferencesPage(){
         <h1>Your preferences</h1>
         <p>* Indicates a required field</p>
         <form>
+          <fieldset>
+              <legend>
+                <h2>Your Preferences</h2>
+              </legend>
+            <div>
+              <h3>What are you looking for?</h3>
+              {/* TODO: Pills need function to select */}
+              <div className="flex">
+                <Button color="gray" pill>Full-time job</Button>
+                <Button color="gray" pill>Part-time job</Button>
+                <Button color="gray" pill>Internship</Button>
+                <Button color="gray" pill>On-campus job</Button>
+              </div>
+              <h3>What is your tech role/pathway targeted?</h3>
+              <Label className="block"><Radio name="profile-creation-preferences-require-role" required/> Software Development</Label>
+              <Label className="block"><Radio name="profile-creation-preferences-require-role" required/> Cloud Computing</Label>
+              <Label className="block"><Radio name="profile-creation-preferences-require-role" required/> Data analytics</Label>
+            </div>
+          </fieldset>
+
           <div className="flex">
             <Button pill color="gray">Previous</Button>
             <Button pill type="submit">Save and continue</Button>
