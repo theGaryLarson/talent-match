1fa3fdb1eed3c94f12c45221f1a1c1d1ae884ac2
not-damien <93546644+not-damien@users.noreply.github.com>
2024-07-12 14:55:17 -0700

    pill linking now works

--- app/ui/components/JobSeekerCardView.tsx
+++ app/ui/components/JobSeekerCardView.tsx
@@ -2,89 +2,101 @@ import Link from 'next/link';
 import Avatar from './Avatar';
 import Pill from './Pill';
 import RoundedButton from './RoundedButton';
-import {SkillDTO} from "@/data/dtos/SkillDTO";
-import {BookmarkIcon, ShareIcon} from '@heroicons/react/24/outline';
-import {JobseekerSkill} from "@/data/dtos/JobSeekerSkill";
-import {JobSeekerCardViewDTO} from "@/data/dtos/JobSeekerCardViewDTO";
+import { SkillDTO } from '@/data/dtos/SkillDTO';
+import { BookmarkIcon, ShareIcon } from '@heroicons/react/24/outline';
+import { JobseekerSkill } from '@/data/dtos/JobSeekerSkill';
+import { JobSeekerCardViewDTO } from '@/data/dtos/JobSeekerCardViewDTO';
 
 export default function JobSeekerCardView({
-                                              isLarge,
-                                              name,
-                                              school,
-                                              pathway,
-                                              skillsList,
-                                              pfpPicSrc,
-                                              aboutMe
-                                          }: {
-    isLarge: boolean;
-    name: string | null;
-    school: string | null | undefined;
-    pathway: string | null | undefined;
-    skillsList: JobSeekerCardViewDTO["jobseeker_has_skills"];
-    pfpPicSrc: string | null;
-    aboutMe: string;
+  isLarge,
+  name,
+  school,
+  pathway,
+  skillsList,
+  pfpPicSrc,
+  aboutMe,
+}: {
+  isLarge: boolean;
+  name: string | null;
+  school: string | null | undefined;
+  pathway: string | null | undefined;
+  skillsList: JobSeekerCardViewDTO['jobseeker_has_skills'];
+  pfpPicSrc?: string | null;
+  aboutMe: string;
 }) {
-    // Extract the first three skills from the jobseeker_has_skills array
-    const firstNSkills: SkillDTO[] = skillsList.slice(0, 3).map((item: JobseekerSkill) => item.skills);
-    if (isLarge) {
-        return (
-            <div className="flex h-[220px] w-full items-center gap-10 rounded-lg border p-6">
-                <div className='flex flex-col items-center text-center w-[128px]'>
-                    <Avatar imgsrc={pfpPicSrc}/>
-                    <span className="font-bold text-wrap">{name}</span>
-                </div>
-                <div className="flex h-full w-3/5 flex-col grow justify-between">
-                    <div className="space-y-2">
-                        <h3>
-                            <span className="font-bold">{pathway}</span>
-                            {/*|{' '}*/}
-                            {/*<span>GPA 3.5</span>*/}
-                        </h3>
-                        {/*<h4>{school} | Senior</h4>*/}
-                        <p>
-                            {aboutMe}
-                        </p>
-                    </div>
-                    <div className="space-x-2">
-                        {firstNSkills.map((pill) => (
-                            <Pill key={pill?.skill_id} text={pill?.skill_name}/>
-                        ))}
-                    </div>
-                </div>
-                <div className="flex h-full flex-col justify-between items-end">
-                    <div className='flex gap-4 w-20 h-7'><ShareIcon/><BookmarkIcon/></div>
-                    <RoundedButton content={'View Profile'} link={'/login'} invertColor={false}/>
-                </div>
-            </div>
-        );
-    }
+  // Extract the first three skills from the jobseeker_has_skills array
+  const firstNSkills: SkillDTO[] = skillsList
+    .slice(0, 3)
+    .map((item: JobseekerSkill) => item.skills);
+  if (isLarge) {
     return (
-        
-            <div className="min-h-[280px] w-[350px] space-y-3 rounded-lg border p-2 hover:bg-gray-100">
-                <Link className='space-y-3' href={'/services/jobseekers/abc123'}>
-      <div className="flex space-x-4 items-center">
-                    <Avatar imgsrc={pfpPicSrc}/>
-                    <h3 className="text-lg font-bold">{name}</h3>
-                </div>
-                
-      <p className='text-xs'>{'"' + aboutMe.substring(0, 100) + '..."'}</p>
-                </Link>
+      <div className="flex h-[220px] w-full items-center gap-10 rounded-lg border p-6">
+        <div className="flex w-[128px] flex-col items-center text-center">
+          <Avatar imgsrc={pfpPicSrc} />
+          <span className="text-wrap font-bold">{name}</span>
+        </div>
+        <div className="flex h-full w-3/5 grow flex-col justify-between">
+          <div className="space-y-2">
+            <h3>
+              <span className="font-bold">{pathway}</span>
+              {/*|{' '}*/}
+              {/*<span>GPA 3.5</span>*/}
+            </h3>
+            {/*<h4>{school} | Senior</h4>*/}
+            <p>{aboutMe}</p>
+          </div>
+          <div className="space-x-2">
+            {firstNSkills.map((pill) => (
+              <Pill
+                key={pill?.skill_id}
+                text={pill?.skill_name}
+                href={pill?.skill_info_url}
+              />
+            ))}
+          </div>
+        </div>
+        <div className="flex h-full flex-col items-end justify-between">
+          <div className="flex h-7 w-20 gap-4">
+            <ShareIcon />
+            <BookmarkIcon />
+          </div>
+          <RoundedButton
+            content={'View Profile'}
+            link={'/login'}
+            invertColor={false}
+          />
+        </div>
+      </div>
+    );
+  }
+  return (
+    <div className="min-h-[280px] w-[350px] space-y-3 rounded-lg border p-2 hover:bg-gray-100">
+      <Link className="space-y-3" href={'/services/jobseekers/abc123'}>
+        <div className="flex items-center space-x-4">
+          <Avatar imgsrc={pfpPicSrc} />
+          <h3 className="text-lg font-bold">{name}</h3>
+        </div>
+
+        <p className="text-xs">{'"' + aboutMe.substring(0, 100) + '..."'}</p>
+      </Link>
       {/* note about me needs to be limited and just trimming to main summary would most likely produce incomplete unprofessional summaries */}
-                <hr/>
+      <hr />
 
-
-                <h3 className="font-bold">{pathway}</h3>
-                <h4 className='text-sm'>
-                    <span className="font-bold">School:</span>
-                    {school}
-                </h4>
-                <hr/>
-                <div className="space-x-2 grow">
-                    {firstNSkills.map((pill: SkillDTO) => (
-                        <Pill key={pill?.skill_id} text={pill?.skill_name}/>
-                    ))}
-                </div>
-            </div>
-
+      <h3 className="font-bold">{pathway}</h3>
+      <h4 className="text-sm">
+        <span className="font-bold">School:</span>
+        {school}
+      </h4>
+      <hr />
+      <div className="">
+        {firstNSkills.map((pill: SkillDTO) => (
+          <Pill
+            key={pill?.skill_id}
+            text={pill?.skill_name}
+            href={pill?.skill_info_url}
+          />
+        ))}
+      </div>
+    </div>
   );
 }
--- app/ui/components/Pill.tsx
+++ app/ui/components/Pill.tsx
@@ -1,10 +1,7 @@
-import Link from "next/link";
-
-export default function ({text}:{text:string}){
+export default function pill({text, href}:{text:string, href:string}){
     return (
-        <Link className="bg-blue-background hover:bg-blue-700 text-white py-0 px-4 rounded-full inline-block mt-1" href={"/skills"}>
+        <a className="bg-blue-background hover:bg-blue-700 text-white py-0 px-4 rounded-full inline-block mt-1" href={href} target="_blank">
             {text}
-         
-        </Link>
+        </a>
     )
 }
