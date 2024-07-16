19d4045c4d8cb4d76407badd8de89c2b21a2d75d
not-damien <93546644+not-damien@users.noreply.github.com>
2024-07-15 18:58:19 -0700

    search bar now updates search term

--- app/(main)/services/employers/dashboard/listview/page.tsx
+++ app/(main)/services/employers/dashboard/listview/page.tsx
@@ -1,17 +1,22 @@
-
+'use client'
 import JobSeekerCardView from '@/app/ui/components/JobSeekerCardView';
 import SearchBar from '@/app/ui/components/SearchBar';
 import {getFilteredJobSeekerCardView} from "@/app/lib/prisma";
 import {JobSeekerCardViewDTO} from "@/data/dtos/JobSeekerCardViewDTO";
+import { Suspense } from 'react'
+import { useSearchParams } from 'next/navigation';
 
 
-export default async function page() {
-    const filteredView = await getFilteredJobSeekerCardView(['']);
+export default function page() {
+  const searchParams = useSearchParams();
+  const search = searchParams.get('search')//this is the search term 
+  //beacuse this need to be a client component we can use the prisma client directly and we need an api route to hit 
+    //const filteredView = await getFilteredJobSeekerCardView(['']);
     return (
         <main className="space-y-8 px-[200px] py-16">
             <h1 className="text-2xl">Search Results</h1>
-            <SearchBar/>
-            {filteredView.map((jobSeeker: JobSeekerCardViewDTO) => (
+              <SearchBar />
+            {/* {filteredView.map((jobSeeker: JobSeekerCardViewDTO) => (
         <JobSeekerCardView
                 key={jobSeeker.jobseeker_id}
                 isLarge={true}
@@ -21,7 +26,7 @@ export default async function page() {
                 skillsList={jobSeeker?.jobseeker_has_skills}
                 pfpPicSrc={jobSeeker?.contacts?.photo_url ?? null}
                 aboutMe={jobSeeker?.intro_headline} id={jobSeeker?.jobseeker_id} />
-      ))}
+      ))} */}
     </main>
   );
 }
