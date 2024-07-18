66fca024affa2717b7bfb30cce690da95c0f9867
not-damien <93546644+not-damien@users.noreply.github.com>
2024-07-18 12:42:51 -0700

    working on profile view inbetween lessons

--- app/(main)/services/jobseekers/[id]/page.tsx
+++ app/(main)/services/jobseekers/[id]/page.tsx
@@ -1,18 +1,26 @@
 import { getJobSeekerEmployerView } from "@/app/lib/prisma";
+import Avatar from "@/app/ui/components/Avatar";
 import JobSeekerCardView from "@/app/ui/components/JobSeekerCardView";
 
 export default async function page({params}:{params: {id:string}}){
     let jobseeker = await getJobSeekerEmployerView(params.id)
     return(
         <main className="space-y-3 py-8 mx-4 md:mx-[150px] lg:mx-[200px] font-['Roboto']">
-            <h1>Job Seeker profile</h1>
-            <p>Student id is:{params.id}</p>
-            <p>
-                {
-                    jobseeker?.contacts.first_name
-                }
-            </p>
-            <JobSeekerCardView isLarge={false} name={jobseeker?.contacts.first_name + ' ' + jobseeker?.contacts.last_name} school={jobseeker?.edu_institutions?.name} pathway={jobseeker?.pathways.pathway_title} skillsList={[]} aboutMe={jobseeker?.intro_headline ? jobseeker?.intro_headline : ''} id={params.id}/>
+           
+            <div className="border w-[500px] h-[200px] flex items-center">
+                <div className="flex items-center gap-5 p-4">
+                <Avatar imgsrc={jobseeker?.contacts.photo_url} scale={3}></Avatar>
+                <div>
+                    <h1 className="font-bold text-2xl">{jobseeker?.contacts.first_name + ' ' + jobseeker?.contacts.last_name}</h1>
+                    <h2>{jobseeker?.pathways?.pathway_title}</h2>
+                    <h2>{jobseeker?.edu_institutions?.name}</h2>
+                </div>
+                </div>
+
+            </div>
+           
+            
+        <p>Student id is:{params.id}</p>
         </main>
     );
 }
