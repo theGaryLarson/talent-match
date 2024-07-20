569bfc92a60d640a9e0ccc37875f3115af2dbdcd
not-damien <93546644+not-damien@users.noreply.github.com>
2024-07-19 17:29:16 -0700

    styling of toggle donw

--- app/ui/components/Toggle.tsx
+++ app/ui/components/Toggle.tsx
@@ -1,19 +1,20 @@
 'use client'
+import { usePathname } from 'next/navigation';
 import React, { useState } from 'react';
 
 export default function Toggle() {
     const [isMoved, setIsMoved] = useState(false);
-
     const handleClick = () => {
         setIsMoved(!isMoved);
     };
 
     return (
-        <div className="w-32 h-14 border" onClick={handleClick}>
+        <div className="relative w-[300px] h-14 border rounded-full cursor-pointer" onClick={handleClick}>
             <div 
-                className={`w-16 h-14 bg-blue-400 border transition-transform duration-500 ${isMoved ? 'translate-x-full' : ''}`} 
-                style={{ transform: isMoved ? 'translateX(100%)' : 'translateX(0)', position: 'relative', left: 0 }}
-            >
+                className={`absolute top-0 left-0 w-[150px] h-14 bg-blue-400 rounded-full border transition-transform duration-500 transform ${isMoved ? 'translate-x-full' : ''}`}
+            /><div className="absolute inset-0 flex justify-between items-center px-2">
+                <span className="w-1/2 text-center">My View</span>
+                <span className="w-1/2 text-center">Showcase View</span>
             </div>
         </div>
     );
