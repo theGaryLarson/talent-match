7662baa7efde9555dbfd558f33c962a02d573f07
theGaryLarson <gary.larson@seattlecolleges.edu>
2024-07-22 16:19:36 -0700

    Refactor create-edu route for jobseekers API
    The code has been updated to address variable renaming and restructuring within the `create-edu` route of the jobseekers API. Further enhancements include adding a utility function to filter out undefined values, improved data selection and structuring for the Prisma query, and adding detailed creation and update conditions for jobseekers' education data. Promises have also been utilized to effectively handle multiple school data.

--- app/api/jobseekers/create-edu/route.ts
+++ app/api/jobseekers/create-edu/route.ts
@@ -1,11 +1,17 @@
 import {NextResponse} from 'next/server';
 import {PrismaClient} from '@prisma/client';
-import {JsEducationDTO} from '@/data/dtos/JobSeekerProfileCreationDTOs';
+import {EducationInfo, JsEducationDTO} from '@/data/dtos/JobSeekerProfileCreationDTOs';
 import {v4 as uuidv4} from 'uuid';
 import getPrismaClient from "@/app/lib/prismaClient.mjs";
 
 const prisma: PrismaClient = getPrismaClient();
 
+// Utility function to filter out undefined values
+const filterUndefined = <T extends object>(obj: T): Partial<T> => {
+    return Object.fromEntries(
+        Object.entries(obj).filter(([, value]) => value !== undefined)
+    ) as Partial<T>;
+};
 export async function POST(request: Request) {
     try {
         const body: JsEducationDTO = await request.json();
@@ -30,10 +36,15 @@ export async function POST(request: Request) {
             // Find the jobseeker_id or generate a new one
             const jobseeker = await prisma.jobseekers.findUnique({
                 where: {user_id},
-                select: {jobseeker_id: true, targeted_pathway: true, is_enrolled_ed_program: true}
+                select: {
+                    jobseeker_id: true,
+                    targeted_pathway: true,
+                    is_enrolled_ed_program: true
+                }
             });
 
             const jobseekerId = jobseeker?.jobseeker_id || uuidv4();
+            const isEnrolledEdProgram = jobseeker?.is_enrolled_ed_program || false;
 
             // Find or create the targeted pathway for 'Undecided'
             let targetedPathway = jobseeker?.targeted_pathway;
@@ -61,20 +72,17 @@ export async function POST(request: Request) {
                     highest_level_of_study_completed: highestLevelOfStudy,
                     current_grade_level: currentGrade,
                     current_enrolled_ed_program: currentEdProgram,
-                    is_enrolled_ed_program: jobseeker?.is_enrolled_ed_program || false,
+                    is_enrolled_ed_program: isEnrolledEdProgram,
                 },
                 create: {
                     jobseeker_id: jobseekerId,
                     user_id: user_id,
                     targeted_pathway: targetedPathway,
-                    is_enrolled_ed_program: isEnrolledInCollege,
+                    is_enrolled_ed_program: isEnrolledEdProgram,
                     highest_level_of_study_completed: highestLevelOfStudy,
                     current_grade_level: currentGrade,
                     current_enrolled_ed_program: currentEdProgram,
-                    degree_type: undefined,
                     intern_hours_required: undefined,
-                    major: undefined,
-                    minor: undefined,
                     intro_headline: undefined,
                     current_job_title: undefined,
                     resume_url: undefined,
@@ -82,11 +90,77 @@ export async function POST(request: Request) {
                     portfolio_url: undefined,
                     video_url: undefined,
                     employment_type_sought: undefined,
-                    edu_start_date: new Date(startDate),
-                    edu_end_date: new Date(graduationDate),
                 },
             });
-            return {jobSeeker}
+
+            certifications.map(async cert => {
+
+            });
+
+            projects.map(async proj => {
+
+            });
+
+            const schoolPromises = schools.map(async school => {
+                const existingEducation = await prisma.jobseekers_education.findFirst({
+                    where: {
+                        jobseekerId: jobseekerId,
+                        edInstitutionId: school.edInstitutionId,
+                        startDate: school.startDate,
+                        gradDate: school.gradDate,
+                    }
+                });
+
+                // Build update object and filter undefined values
+                const updateData: Partial<EducationInfo> = filterUndefined({
+                    jobseekerEdId: school.jobseekerEdId,
+                    jobSeekerId: school.jobSeekerId,
+                    edInstitutionId: school.edInstitutionId,
+                    edProgram: school.edProgram,
+                    edSystem: school.edSystem,
+                    isEnrolled: school.isEnrolled,
+                    startDate: school.startDate,
+                    gradDate: school.gradDate,
+                    degreeType: school.degreeType,
+                    major: school.major,
+                    minor: school.minor,
+                    description: school.description,
+                })
+
+                if (existingEducation) {
+                    return prisma.jobseekers_education.update({
+                        where: {jobseekerEdId: existingEducation.jobseekerId},
+                        data: updateData
+                    });
+                } else {
+                    return prisma.jobseekers_education.create({
+                        data: {
+                            jobseekerEdId: uuidv4(),
+                            edProgram: school.edProgram,
+                            edSystem: school.edSystem,
+                            isEnrolled: school.isEnrolled,
+                            startDate: school.startDate,
+                            gradDate: school.gradDate,
+                            degreeType: school.degreeType,
+                            major: school.major,
+                            minor: school.minor,
+                            description: school.description,
+                            jobseekers: {
+                                connect: {
+                                    jobseeker_id: jobseekerId,
+                                }
+                            },
+                            eduInstitutions: {
+                                connect: {
+                                    edu_institution_id: school.edInstitutionId
+                                }
+                            }
+
+                        }
+                    });
+                }
+            });
+            await Promise.all(schoolPromises)
         });
         return NextResponse.json(result, {status: 200});
     } catch (e: any) {
