88fce4d0e75548583b9e4b076330ab0a6953f409
theGaryLarson <gary.larson@seattlecolleges.edu>
2024-07-28 17:20:51 -0700

    Add jobseeker project delete route.

---
+++ app/api/jobseekers/proj-record-delete/route.ts
@@ -0,0 +1,37 @@
+import { NextResponse } from 'next/server';
+import { PrismaClient } from '@prisma/client';
+import getPrismaClient from "@/app/lib/prismaClient.mjs";
+
+const prisma: PrismaClient = getPrismaClient();
+
+export async function DELETE(request: Request) {
+    let projId = null;
+    try {
+        const body = await request.json();
+        const { projectId } = body;
+        projId = projectId;
+
+        const skillsCount = await prisma.project_has_skills.deleteMany({
+            where: {
+                project_experiences: {
+                    projectId: projId,
+                }
+            }
+        });
+
+        const deletedEntry = await prisma.projectExperiences.delete({
+            where: {
+                projectId: projId,
+            }
+        });
+
+        return NextResponse.json({ success: true, result: {deletedEntry: deletedEntry, skills: skillsCount }});
+
+    } catch (e: any) {
+        console.log(e.message);
+        const msg = projId ? `(No Jobseeker Project record with id ${projId})` : `unknown id`;
+        return NextResponse.json({ error: `Failed to delete jobseeker project with id: ${msg}` });
+    } finally {
+        await prisma.$disconnect();
+    }
+}
