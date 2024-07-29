112abf14eb80e56b32df66d5ef56016dddb45b3e
quincyp <quincytpham@gmail.com>
2024-07-29 18:41:46 -0400

    added setNewFieldValue

--- app/(profile-creation)/create-profile/jobseeker/intro/page.tsx
+++ app/(profile-creation)/create-profile/jobseeker/intro/page.tsx
@@ -47,6 +47,7 @@ export default function CreateJobseekerProfileIntroPage(){
       setNewFieldId('');
       setNewFieldLabel('');
       setNewFieldType('text');
+      setNewFieldValue('');
       setNewFieldOptions([]);
     }
   };
