2ca79ff4f31b34c4d97bdf4bb2f1bfc8c2342a1e
theGaryLarson <gary.larson@seattlecolleges.edu>
2024-07-31 17:30:38 -0700

    Add disclosures profile upsert API route.

---
+++ app/api/jobseekers/disclosures-profile-upsert/route.ts
@@ -0,0 +1,94 @@
+import getPrismaClient from "@/app/lib/prismaClient.mjs";
+import {jobseekers_private_data, PrismaClient} from "@prisma/client";
+import {NextResponse} from "next/server";
+import {JsDisclosuresDTO, JsDisclosuresPostDTO} from "@/data/dtos/JobSeekerProfileCreationDTOs";
+
+const prisma: PrismaClient = getPrismaClient();
+
+export async function POST(request: Request) {
+    try {
+        const body: JsDisclosuresPostDTO = await request.json();
+        const {
+            userId,
+            jobseekerId,
+            isVeteran,
+            hasDisability,
+            gender,
+            race,
+            hasReadTerms
+        } = body;
+
+        if (!userId) {
+            return NextResponse.json({success: false, error: `A userId must be provided.`})
+        }
+
+        const contact = await prisma.contacts.update({
+            where: {
+                user_id: userId
+            },
+            data: {
+                gender: gender,
+                race: race,
+                has_agreed_terms: hasReadTerms,
+                jobseekers: {
+                    update: {
+                        where: {
+                            jobseeker_id: jobseekerId
+                        },
+                        data: {
+                            jobseekers_private_data: {
+                                update: {
+                                    where: {
+                                        jobseeker_id: jobseekerId
+                                    },
+                                    data: {
+                                        is_veteran: isVeteran,
+                                        has_disability: hasDisability,
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            },
+            select: {
+                jobseekers: {
+                    select: {
+                        jobseeker_id: true,
+                        jobseekers_private_data: {
+                            select: {
+                                is_veteran: true,
+                                has_disability: true,
+                            }
+                        }
+                    }
+                },
+                gender: true,
+                has_agreed_terms: true,
+                race: true,
+            }
+        });
+
+        const {jobseekers, gender: contactGender, has_agreed_terms, race: contactRace} = contact;
+        const jobseekerDetails = jobseekers?.[0] || {};
+        const privateDetails = jobseekerDetails.jobseekers_private_data?.[0] || {};
+
+        const result: JsDisclosuresDTO = {
+            jobseekerId: jobseekerDetails.jobseeker_id || null,
+            gender: contactGender || null,
+            race: contactRace || null,
+            hasReadTerms: has_agreed_terms || false,
+            isVeteran: privateDetails.is_veteran || null,
+            hasDisability: privateDetails.has_disability || null
+        };
+
+        return NextResponse.json({success: true, result}, {status: 200})
+
+    } catch
+        (e: any) {
+        return NextResponse.json({error: `Failed to upsert disclosures: ${e.message}`}, {status: 500});
+
+    } finally {
+        await prisma.$disconnect()
+    }
+}
