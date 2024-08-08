d61938e793885675848219cde6e701b3ee857db5
theGaryLarson <gary.larson@seattlecolleges.edu>
2024-08-08 16:36:25 -0700

    Rename and refactor education record delete route.
    The education record delete route has been renamed to a more RESTful path.

--- app/api/jobseekers/education/remove/[eduId]/route.ts
+++ app/api/jobseekers/education/remove/[eduId]/route.ts
@@ -1,24 +1,22 @@
-import { NextResponse } from 'next/server';
+import {NextResponse} from 'next/server';
 import {jobseekers_education, PrismaClient} from '@prisma/client';
 import getPrismaClient from "@/app/lib/prismaClient.mjs";
 
 const prisma: PrismaClient = getPrismaClient();
 
-export async function DELETE(request: Request) {
-    let jsEduId = null;
+export async function DELETE(request: Request, {params}: {params: {eduId: string}}) {
+    let eduId = null;
     try {
-        const body = await request.json();
-        const { jobseekerEdId } = body;
-        jsEduId = jobseekerEdId;
+        eduId = params.eduId;
         const deletedEntry: jobseekers_education = await prisma.jobseekers_education.delete({
             where: {
-                jobseekerEdId: jsEduId,
+                jobseekerEdId: eduId,
             }
         });
         return NextResponse.json({ success: true, result: deletedEntry });
     } catch (e: any) {
         console.log(e.message);
-        const msg = jsEduId ? `(No Jobseeker Education record with id ${jsEduId})` : `unknown id`;
+        const msg = eduId ? `(No Jobseeker Education record with id ${eduId})` : `unknown id`;
         return NextResponse.json({ error: `Failed to delete jobseeker education with id: ${msg}` });
     } finally {
         await prisma.$disconnect();
