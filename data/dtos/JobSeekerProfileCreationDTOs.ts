e7f251dfeb5e20a8cd239ef21a5c33203490b57d
theGaryLarson <gary.larson@seattlecolleges.edu>
2024-08-09 17:50:47 -0700

    Remove completed TODO comments from JobSeeker DTO.

--- data/dtos/JobSeekerProfileCreationDTOs.ts
+++ data/dtos/JobSeekerProfileCreationDTOs.ts
@@ -131,7 +131,7 @@ type workExperienceDTO = {
 export type JsWorkExpDTO = {
     userId: string,
     yearsWorkExperience: string,
-    monthsInternshipExperience?: string | null, // TODO: add field to the database
+    monthsInternshipExperience?: string | null,
     isAuthorizedToWorkUsa: boolean, // TODO: encrypt
     requiresSponsorship: boolean, // TODO: encrypt
     workExperiences?: WorkExperience[]
