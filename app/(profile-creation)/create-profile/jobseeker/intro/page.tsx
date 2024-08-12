0d43f78ec500861896ab343d52c027d17553decf
theGaryLarson <gary.larson@seattlecolleges.edu>
2024-08-12 08:44:07 -0700

    Update import paths in jobseeker intro page
    Simplify the import path for `RootState` by using the alias `@/lib/store`. No functionality was affected by this change.

--- app/(profile-creation)/create-profile/jobseeker/intro/page.tsx
+++ app/(profile-creation)/create-profile/jobseeker/intro/page.tsx
@@ -2,7 +2,7 @@
 
 import React, { ChangeEvent, FormEvent, useState } from 'react';
 import { useRouter } from 'next/navigation';
-import type { RootState } from '../../../../../lib/store';
+import type { RootState } from '@/lib/store';
 import { useSelector, useDispatch } from 'react-redux';
 import { addField, updateField, submitForm, submitFormSuccess, submitFormFailure, FormState } from '../../../../../lib/features/profileCreation/formSlice';
 import InputTextWithLabel from '@/app/ui/components/InputTextWithLabel';
