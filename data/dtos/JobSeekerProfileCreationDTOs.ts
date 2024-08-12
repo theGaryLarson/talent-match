dbe0756676afea7cd27d5bb22371b4884dd65a40
theGaryLarson <gary.larson@seattlecolleges.edu>
2024-08-12 09:12:14 -0700

    Allow nullable names in JobSeekerProfileCreationDTOs
    Changed firstName and lastName to be optional and nullable. This update prevents application crashes when these properties are undefined or null in user profiles.

--- data/dtos/JobSeekerProfileCreationDTOs.ts
+++ data/dtos/JobSeekerProfileCreationDTOs.ts
@@ -4,8 +4,8 @@ import {WorkExperience} from "@prisma/client";
 export type JsIntroDTO = {
     userId: string,
     photoUrl?: string | null;
-    firstName: string;
-    lastName: string;
+    firstName?: string | null;
+    lastName?: string | null;
     birthDate?: string | Date | null;
     phoneCountryCode?: string | null;
     phone?: string | null;
