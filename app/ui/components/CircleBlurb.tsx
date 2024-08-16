70867d98558513068faef8363113d5212b188cd2
not-damien <93546644+not-damien@users.noreply.github.com>
2024-08-16 13:08:59 -0700

    replaced placeholder images with text and added borders

--- app/ui/components/CircleBlurb.tsx
+++ app/ui/components/CircleBlurb.tsx
@@ -35,13 +35,17 @@ export default function CircleBlurb(){
             </div>
         </div>
     </div>
-    <img className="w-[174.25px] h-[42.92px] left-[447.05px] top-[301.44px] absolute rounded-[7.07px] shadow" src="https://via.placeholder.com/174x43" />
     <img className="w-[377.05px] h-[377.05px] left-[132.85px] top-0 absolute rounded-full" src="https://via.placeholder.com/377x377" />
+    <span className="w-[156.23px] h-[42.92px] left-[16.33px] top-[232.38px] absolute rounded-[7.07px] shadow flex items-center justify-center bg-white border border-black">ADD PROJECTS</span>
+    <span className="w-[174.25px] h-[42.92px] left-[447.05px] top-[301.44px] absolute rounded-[7.07px] shadow flex items-center justify-center bg-white border border-black">ADD CERTIFICATES</span>
+    
     <div className="w-[78.67px] h-[78.67px] left-[431.23px] top-[37px] absolute">
         <div className="w-[78.67px] h-[78.67px] left-0 top-0 absolute bg-[#047089] rounded-full" />
     </div>
-    <img className="w-[156.23px] h-[42.92px] left-[16.33px] top-[232.38px] absolute rounded-[7.07px] shadow" src="https://via.placeholder.com/156x43" />
-    <img className="w-[221.62px] h-[42.92px] left-[320.42px] top-[356.92px] absolute rounded-[7.07px] shadow" src="https://via.placeholder.com/222x43" />
+
+   
+    <span className="w-[221.62px] h-[42.92px] left-[320.42px] top-[356.92px] absolute rounded-[7.07px] shadow flex items-center justify-center bg-white border border-black">ADD WORK EXPERIENCE</span>
+    
     <div className="w-64 h-[87.61px] p-[18.81px] left-[39.28px] top-[297px] absolute bg-white rounded-[18.81px] shadow flex-col justify-start items-start gap-[22.57px] inline-flex">
         <div className="flex-col justify-start items-start gap-[7.52px] flex">
             <div className="flex-col justify-start items-start flex">
