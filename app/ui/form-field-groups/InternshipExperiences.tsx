1f6e3965b135445a56fe6edb7d730999cee14df6
theGaryLarson <gary.larson@seattlecolleges.edu>
2024-08-17 22:49:01 -0700

    Change uid to string using UUID
    Switched uid from number to string using UUID for more robust and unique identification. Updated all relevant type annotations and methods including onRemove handler to reflect this change.

--- app/ui/form-field-groups/InternshipExperiences.tsx
+++ app/ui/form-field-groups/InternshipExperiences.tsx
@@ -4,6 +4,7 @@ import { Checkbox } from '@mui/material';
 import { MdClose } from "react-icons/md";
 import InputTextWithLabel from '../components/InputTextWithLabel';
 import TextareaWithLabel from '../components/TextareaWithLabel';
+import { v4 as uuidv4 } from 'uuid';
 
 const classNamePrefix = "profile-creation-internship-experience-group-";
 const classCompany = "company";
@@ -14,7 +15,7 @@ const classCurrent = "current";
 const classExperience = "experience";
 
 export interface InternshipExperienceData {
-  uid: number,
+  uid: string,
   [classCompany]: string,
   [classTitle]: string,
   [classStarts]: string,
@@ -26,7 +27,7 @@ export interface InternshipExperienceData {
 let uniqueListID = 0;
 export function defaultInternshipExperienceData() {
   return {
-    uid: uniqueListID++,
+    uid: uuidv4(),
     [classCompany]: "",
     [classTitle]: "",
     [classStarts]: "",
@@ -38,7 +39,7 @@ export function defaultInternshipExperienceData() {
 
 interface Props {
   data: InternshipExperienceData[],
-  onRemove: (uid:number) => void,
+  onRemove: (uid:string) => void,
   onUpdate: (key: string, value: any) => void,
 }
 
