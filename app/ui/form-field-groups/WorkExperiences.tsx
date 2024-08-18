15efe2d9aac97cecbbe2279be9cfdff3f8101324
theGaryLarson <gary.larson@seattlecolleges.edu>
2024-08-17 22:49:24 -0700

    Change uid to string using UUID
    Switched uid from number to string using UUID for more robust and unique identification. Updated all relevant type annotations and methods including onRemove handler to reflect this change.

--- app/ui/form-field-groups/WorkExperiences.tsx
+++ app/ui/form-field-groups/WorkExperiences.tsx
@@ -3,6 +3,7 @@ import { Button, Checkbox, Label } from 'flowbite-react';
 import { MdClose } from "react-icons/md";
 import InputTextWithLabel from '../components/InputTextWithLabel';
 import TextareaWithLabel from '../components/TextareaWithLabel';
+import {v4 as uuidv4} from 'uuid';
 
 const classNamePrefix = "profile-creation-work-experience-group-";
 const classCompany = "company";
@@ -13,7 +14,7 @@ const classCurrent = "current";
 const classExperience = "experience";
 
 export interface WorkExperienceData {
-  uid: number,
+  uid: string,
   [classCompany]: string,
   [classTitle]: string,
   [classStarts]: string,
@@ -25,7 +26,7 @@ export interface WorkExperienceData {
 let uniqueListID = 0;
 export function defaultWorkExperienceData() : WorkExperienceData {
   return {
-    uid: uniqueListID++,
+    uid: uuidv4(),
     [classCompany]: "",
     [classTitle]: "",
     [classStarts]: "",
@@ -37,7 +38,7 @@ export function defaultWorkExperienceData() : WorkExperienceData {
 
 interface Props {
   data: WorkExperienceData[],
-  onRemove: (uid:number) => void,
+  onRemove: (uid:string) => void,
   onUpdate: (key: string, value: any) => void,
 }
 
