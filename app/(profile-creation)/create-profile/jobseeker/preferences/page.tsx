83d00d73556f3df47087d27d92cc768e5bb190fc
theGaryLarson <gary.larson@seattlecolleges.edu>
2024-08-19 00:15:38 -0700

    Add state and form submission for preferences page
    Implemented useState hooks for employment type and pathway. Added form submission handler to save preferences to the server and navigate on success.

--- app/(profile-creation)/create-profile/jobseeker/preferences/page.tsx
+++ app/(profile-creation)/create-profile/jobseeker/preferences/page.tsx
@@ -1,60 +1,175 @@
 'use client';
 
-import React from 'react';
+import React, { useState } from 'react';
 import ProgressBarFlat from '@/app/ui/components/ProgressBarFlat';
 
-import type { RootState } from '../../../../../lib/store';
-import { useSelector, useDispatch } from 'react-redux';
-import { FormControl, FormControlLabel, FormLabel, Radio, RadioGroup } from '@mui/material';
-import { Button } from "flowbite-react";
+// REVIEW: testing redux
+import type { RootState } from '@/lib/store';
+// import { useSelector, useDispatch } from 'react-redux';
+// import { addField, updateField } from '@/lib/features/profileCreation/formSlice';
+import {
+  Button,
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  Radio,
+  RadioGroup,
+} from '@mui/material';
+import { useRouter } from 'next/navigation';
+import { JsPreferencesDTO } from '@/data/dtos/JobSeekerProfileCreationDTOs';
 
+export default function CreateJobseekerProfilePreferencesPage() {
+  // const { fields } = useSelector((state: RootState) => state.form);
+  // const dispatch = useDispatch();
+  const [employmentType, setEmploymentType] = useState('');
+  const [pathway, setPathway] = useState('');
 
-export default function CreateJobseekerProfilePreferencesPage(){
-  const { fields } = useSelector((state: RootState) => state.form);
-  const dispatch = useDispatch();
-  return(
+  const router = useRouter();
+
+  async function handleSubmit(event: React.FormEvent) {
+    event.preventDefault();
+    const formData: JsPreferencesDTO = {
+      userId: '87E52D83-CC98-46AF-B62A-58124ABEBBDC', // TODO: grab user.id from nextauth session
+      targetedPathwayId: null,
+      targetedPathway: pathway,
+      preferredEmploymentType: employmentType,
+    };
+
+    try {
+      const response = await fetch(
+        '/api/jobseekers/account/preferences/upsert',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(formData),
+        },
+      );
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const result = await response.json();
+      console.log(JSON.stringify(result, null, 2));
+      router.push('/create-profile/jobseeker/preferences');
+    } catch (e: any) {
+      //error handling
+    }
+  }
+  return (
     <main className="flex">
-      <aside className="profile-form-aside">
-      </aside>
+      <aside className="profile-form-aside"></aside>
       <section className="profile-form-section">
         {/* TODO: Comment/Uncomment test script below for viewing */}
         {/* <h1>Data on Another Page</h1>
         <pre>{JSON.stringify(fields, null, 2)}</pre> */}
-        <ProgressBarFlat progress={5/6 * 100} size="sm" className="xl:hidden"/>
+        <ProgressBarFlat
+          progress={(5 / 6) * 100}
+          size="sm"
+          className="xl:hidden"
+        />
         <p>Step 5/6</p>
         <h1>Your preferences</h1>
-        <p className='subtitle'>* Indicates a required field</p>
-        <form>
+        <p>* Indicates a required field</p>
+        <form onSubmit={handleSubmit}>
           <fieldset>
             <div>
               <fieldset>
                 <legend>What are you looking for?</legend>
                 {/* TODO: Pills need function to select */}
-                <div className="container">
-                  <Button pill className="custom-outline-btn inline-block m-2">Full-time job</Button>
-                  <Button pill className="custom-outline-btn inline-block m-2">Part-time job</Button>
-                  <Button pill className="custom-outline-btn inline-block m-2">Internship</Button>
-                  <Button pill className="custom-outline-btn inline-block m-2">On-campus job</Button>
+                <div className="flex">
+                  <Button
+                    variant="outlined"
+                    onClick={(e) => {
+                      setEmploymentType('Full-time job');
+                    }}
+                  >
+                    Full-time job
+                  </Button>
+                  <Button
+                    variant="outlined"
+                    onClick={(e) => {
+                      setEmploymentType('Part-time job');
+                    }}
+                  >
+                    Part-time job
+                  </Button>
+                  <Button
+                    variant="outlined"
+                    onClick={(e) => {
+                      setEmploymentType('Internship');
+                    }}
+                  >
+                    Internship
+                  </Button>
+                  <Button
+                    variant="outlined"
+                    onClick={(e) => {
+                      setEmploymentType('On-campus job');
+                    }}
+                  >
+                    On-campus job
+                  </Button>
                 </div>
               </fieldset>
               <FormControl component="fieldset">
-                <FormLabel id="profile-creation-preferences-require-role" className="mt-7" component="legend" sx={{color:"#000000ff"}}>What is your tech role/pathway targeted?</FormLabel>
+                <FormLabel
+                  id="profile-creation-preferences-require-role"
+                  component="legend"
+                  sx={{ color: '#000000ff' }}
+                >
+                  What is your tech role/targeted pathway?
+                </FormLabel>
                 <RadioGroup
                   aria-labelledby="profile-creation-preferences-require-role"
                   defaultValue="female"
                   name="profile-creation-preferences-require-role"
+                  value={pathway}
+                  onChange={(e) => {
+                    setPathway(e.target.value);
+                  }}
                 >
-                  <FormControlLabel value="Software Development" control={<Radio />} label="Software Development" />
-                  <FormControlLabel value="Cloud Computing" control={<Radio />} label="Cloud Computing" />
-                  <FormControlLabel value="Data analytics" control={<Radio />} label="Data analytics" />
+                  <FormControlLabel
+                    value="Software Developer"
+                    control={<Radio />}
+                    label="Software Developer"
+                  />
+                  <FormControlLabel
+                    value="Web Developer"
+                    control={<Radio />}
+                    label="Web Developer"
+                  />
+                  <FormControlLabel
+                    value="Software Quality Assurance Analyst and Tester"
+                    control={<Radio />}
+                    label="Software Quality Assurance Analyst and Tester"
+                  />
+                  <FormControlLabel
+                    value="Network and Computer Systems Administrator"
+                    control={<Radio />}
+                    label="Network and Computer Systems Administrator"
+                  />
+                  <FormControlLabel
+                    value="Computer User Support Specialist"
+                    control={<Radio />}
+                    label="Computer User Support Specialist"
+                  />
+                  <FormControlLabel
+                    value="Graphic Designer"
+                    control={<Radio />}
+                    label="Graphic Designer"
+                  />
                 </RadioGroup>
               </FormControl>
             </div>
           </fieldset>
 
-          <div className="profile-form-progress-btn-group">
-            <Button pill className="custom-outline-btn">Previous</Button>
-            <Button pill>Save and continue</Button>
+          <div className="flex">
+            <Button variant="outlined">Previous</Button>
+            <Button variant="contained" type="submit">
+              Save and continue
+            </Button>
           </div>
         </form>
       </section>
