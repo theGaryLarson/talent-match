e244c677aad77be729f0623f557c9002560f205a
not-damien <93546644+not-damien@users.noreply.github.com>
2024-08-21 18:46:48 -0700

    added a todo

--- app/(main)/services/jobseekers/[id]/page.tsx
+++ app/(main)/services/jobseekers/[id]/page.tsx
@@ -33,6 +33,9 @@ export default async function page({ params }: { params: { id: string } }) {
         </div>
     </div>
     </div> */}
+    {
+    //TODO add years work expernce "2YRS" 
+    }
         <div className="flex h-[200px] items-center rounded-md border bg-white">
           <div className="flex items-center gap-5 p-4">
             <Avatar imgsrc={jobseeker?.users.photo_url} scale={3}></Avatar>
