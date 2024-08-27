f09e0640c910a1b06e323743705408ba446c1b7f
not-damien <93546644+not-damien@users.noreply.github.com>
2024-08-27 11:14:21 -0700

    addded major to getjobseekeremployerview

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -438,6 +438,7 @@ export async function getJobSeekerEmployerView(jobSeekerId: string) {
                         }
                     },
                     minor: true,
+                    major: true,
                     description: true,
                 }
             },
