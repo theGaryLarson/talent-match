311a2732ba75bf0a7f8d8e9fb66b134d69654304
Quincy Pham <quincytpham@gmail.com>
2024-08-30 15:25:23 -0400

    changed from adjusting padding to height instead

--- app/(profile-creation)/create-profile/employer/company/page.tsx
+++ app/(profile-creation)/create-profile/employer/company/page.tsx
@@ -233,13 +233,13 @@ export default function CreateJobseekerProfileIntroPage(){
                 id="profile-creation-company-size"
                 onChange={handleFieldChange}
                 options={[
-                  // {label:"1-10", value:"1-10"},
-                  // {label:"11-50", value:"11-50"},
-                  // {label:"51-200", value:"51-200"},
-                  // {label:"201-500", value:"201-500"},
-                  // {label:"501-1000", value:"501-1000"},
-                  // {label:"1001-5000", value:"1001-5000"},
-                  // {label:"5000+", value:"5000+"},
+                  {label:"1-10", value:"1-10"},
+                  {label:"11-50", value:"11-50"},
+                  {label:"51-200", value:"51-200"},
+                  {label:"201-500", value:"201-500"},
+                  {label:"501-1000", value:"501-1000"},
+                  {label:"1001-5000", value:"1001-5000"},
+                  {label:"5000+", value:"5000+"},
                 ]}
                 placeholder="Please select"
                 value={fields.find(f => f.id === 'profile-creation-company-size')?.value}
