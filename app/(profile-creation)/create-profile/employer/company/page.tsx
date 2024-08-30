c4a2fc9c01ffb4766868b8f09f764bbab78b6e69
Quincy Pham <quincytpham@gmail.com>
2024-08-30 15:28:47 -0400

    adjusted label positioning to match other components, and adjusted grid

--- app/(profile-creation)/create-profile/employer/company/page.tsx
+++ app/(profile-creation)/create-profile/employer/company/page.tsx
@@ -225,9 +225,7 @@ export default function CreateJobseekerProfileIntroPage(){
             <InputTextWithLabel type="email" id="profile-creation-company-email" placeholder="hello@company.com" onChange={handleFieldChange} value={fields.find(f => f.id === 'profile-creation-company-email')?.value || ''} required>Company Email *</InputTextWithLabel>
             <InputTextWithLabel type="tel" id="profile-creation-company-phone" onChange={handleFieldChange} placeholder="(555) 123-4567" value={fields.find(f => f.id === 'profile-creation-company-phone')?.value || ''} required>Company Phone Number *</InputTextWithLabel>
             <DatePicker label={"Year Founded *"} views={['year']} onChange={setYearFounded} className="year-picker" />
-          </div>
 
-          <div className="profile-form-grid tablet:grid-cols-2">
             {/* <InputTextWithLabel id="profile-creation-company-size" placeholder="5,000+" onChange={handleFieldChange} value={fields.find(f => f.id === 'profile-creation-company-size')?.value || ''} required>Company Size *</InputTextWithLabel> */}
             <SelectOptionsWithLabel
                 id="profile-creation-company-size"
