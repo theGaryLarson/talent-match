f10219999c61bb7a74cccc179fe6020a83519fec
theGaryLarson <gary.larson@seattlecolleges.edu>
2024-09-02 13:21:16 -0700

    Refactor Azure Blob Service for Specific Containers and SAS Tokens
    Refactor upload and retrieval methods to use specific containers for images and resumes. Added support for embedding SAS tokens in resume URLs and created utility functions for token generation with more precise timing. Expanded upload methods to conditionally include SAS tokens based on file type.

--- app/lib/services/azureBlobService.ts
+++ app/lib/services/azureBlobService.ts
@@ -1,14 +1,17 @@
 import {
+    BlobSASPermissions,
     BlobServiceClient,
     BlockBlobClient, ContainerSASPermissions,
-    generateBlobSASQueryParameters, SASProtocol,
+    generateBlobSASQueryParameters,
+    SASProtocol,
     StorageSharedKeyCredential
 } from "@azure/storage-blob";
 
 const connectionString = process.env.AZURE_STORAGE_CONNECTION_STRING!;
 const blobServiceClient = BlobServiceClient.fromConnectionString(connectionString);
 
-const containerName = "career-services";
+const imageContainerName = "image-storage";
+const resumeContainerName = "resume-storage";
 const docFileExtensionsAllowed = ['.pdf', '.doc', '.docx', '.txt', '.rtf'];
 const imagFileExtensionsAllowed = ['.svg', '.png', '.jpg', '.jpeg', '.gif', '.webp'];
 
@@ -18,7 +21,7 @@ enum FilePrefix {
     Resume = 'resume',
 }
 
-// needed to map the correct content-type property based on file extension
+// Needed to map the correct content-type property based on file extension
 const contentTypeMap: { [key: string]: string } = {
     // doc content-types
     '.pdf': 'application/pdf',
@@ -35,31 +38,29 @@ const contentTypeMap: { [key: string]: string } = {
     '.webp': 'image/webp',
 };
 
-// Refactored uploadDocument method using the generalized uploadFile function
+// Upload resume method using the generalized uploadFile function with SAS token
 export async function uploadResume(file: Buffer, fileName: string, userId: string): Promise<string> {
-    return await uploadFile(file, fileName, userId, FilePrefix.Resume, docFileExtensionsAllowed);
+    return await uploadFile(file, fileName, userId, FilePrefix.Resume, docFileExtensionsAllowed, resumeContainerName, true);
 }
 
-// Refactored method to get a link to the resume, accepting any allowed file extension
+// Method to get a link to the resume with a SAS token
 export async function getResumeUrl(userId: string): Promise<string | null> {
     const blobPrefix = `${userId}/${FilePrefix.Resume}`;  // Common prefix for resumes
-    return await getBlobUrlWithSas(containerName, blobPrefix);
+    return await getBlobUrlWithSas(resumeContainerName, blobPrefix);
 }
 
-// Refactored uploadAvatar method using the generalized uploadFile function
+// Upload avatar method using the generalized uploadFile function without SAS token
 export async function uploadAvatar(file: Buffer, fileName: string, userId: string): Promise<string> {
-    return await uploadFile(file, fileName, userId, FilePrefix.Avatar, imagFileExtensionsAllowed);
+    return await uploadFile(file, fileName, userId, FilePrefix.Avatar, imagFileExtensionsAllowed, imageContainerName, false);
 }
 
-// Refactored method to get a link to the avatar image
+// Method to get a link to the avatar image without SAS token
 export async function getAvatarUrl(userId: string): Promise<string | null> {
     const blobPrefix = `${userId}/${FilePrefix.Avatar}`;  // Common prefix for avatars
-    return await getBlobUrlWithSas(containerName, blobPrefix);
+    return await getBlobUrl(imageContainerName, blobPrefix);
 }
 
-// this will delete existing blobs with the same prefix to avoid storing multiple files with different file extensions.
-// Reasoning: keep stored files limited to two per user. 1)avatar image 2) resume
-// If we do want to include other types of files we can create a prefix for them.
+// Delete existing blobs with the same prefix to avoid storing multiple files with different file extensions.
 async function deleteExistingBlobs(containerName: string, prefix: string) {
     const containerClient = blobServiceClient.getContainerClient(containerName);
 
@@ -71,7 +72,15 @@ async function deleteExistingBlobs(containerName: string, prefix: string) {
 }
 
 // Generalized Method to upload a file to Azure Blob Storage
-async function uploadFile(file: Buffer, fileName: string, userId: string, prefix: FilePrefix, allowedExtensions: string[]): Promise<string> {
+async function uploadFile(
+    file: Buffer,
+    fileName: string,
+    userId: string,
+    prefix: FilePrefix,
+    allowedExtensions: string[],
+    containerName: string,
+    attachSasToken: boolean
+): Promise<string> {
     const fileExtension = fileName.substring(fileName.lastIndexOf('.')).toLowerCase();
 
     // Check if the file extension is allowed
@@ -95,7 +104,14 @@ async function uploadFile(file: Buffer, fileName: string, userId: string, prefix
                 blobContentType: contentType,  // Set the content type for the file
             },
         });
-        return blockBlobClient.url;  // Return the URL of the uploaded file
+
+        // Attach SAS token for resume URLs
+        if (attachSasToken) {
+            const sasToken = generateBlobSasToken(containerName, blobName);
+            return `${blockBlobClient.url}?${sasToken}`;  // Return the URL of the uploaded file with SAS token
+        } else {
+            return blockBlobClient.url;  // Return the URL of the uploaded file without SAS token
+        }
     } catch (error) {
         console.error("Error uploading file:", error);
         throw new Error("Failed to upload file");
@@ -108,9 +124,8 @@ function getBlockBlobClient(containerName: string, blobName: string): BlockBlobC
     return containerClient.getBlockBlobClient(blobName);
 }
 
-// Generalized Method to get the URL of a blob with a specific prefix
+// Generalized Method to get the URL of a blob with a SAS token
 async function getBlobUrlWithSas(containerName: string, blobPrefix: string): Promise<string | null> {
-    const blobServiceClient = BlobServiceClient.fromConnectionString(process.env.AZURE_STORAGE_CONNECTION_STRING!);
     const containerClient = blobServiceClient.getContainerClient(containerName);
 
     // List blobs in the container and filter by the expected prefix
@@ -118,8 +133,10 @@ async function getBlobUrlWithSas(containerName: string, blobPrefix: string): Pro
         for await (const blob of containerClient.listBlobsFlat({ prefix: blobPrefix })) {
             // If a matching blob is found, return the URL
             const blockBlobClient = containerClient.getBlockBlobClient(blob.name);
-            const sasToken = generateSasToken(containerName, blob.name);
-            return `${blockBlobClient.url}${sasToken}`;  // Return the full URL of the blob with SAS token
+            const sasToken = generateBlobSasToken(containerName, blob.name);
+            // Check if the blockBlobClient.url already contains a '?' and append accordingly
+            const separator = blockBlobClient.url.includes('?') ? '&' : '?';
+            return `${blockBlobClient.url}${separator}${sasToken}`;  // Return the full URL of the blob with SAS token
         }
 
         // If no matching blob is found, return null or handle accordingly
@@ -131,23 +148,62 @@ async function getBlobUrlWithSas(containerName: string, blobPrefix: string): Pro
     }
 }
 
+// Generalized Method to get the URL of a blob without a SAS token
+async function getBlobUrl(containerName: string, blobPrefix: string): Promise<string | null> {
+    const containerClient = blobServiceClient.getContainerClient(containerName);
+
+    // List blobs in the container and filter by the expected prefix
+    try {
+        for await (const blob of containerClient.listBlobsFlat({ prefix: blobPrefix })) {
+            // If a matching blob is found, return the URL
+            const blockBlobClient = containerClient.getBlockBlobClient(blob.name);
+            return blockBlobClient.url;  // Return the URL of the blob without SAS token
+        }
+
+        // If no matching blob is found, return null or handle accordingly
+        return null;
+    } catch (error) {
+        console.error("Error retrieving blob link:", error);
+        throw new Error("Failed to retrieve blob link");
+    }
+}
+
+// Function to convert current time to UTC formatted string for Azure SAS token
+function formatDateToUTC(date: Date): string {
+    return date.toISOString().replace(/\.\d{3}Z$/, 'Z'); // Format to remove milliseconds
+}
 // Generate SAS Token for a specific blob
-function generateSasToken(containerName: string, blobName: string): string {
+function generateDateWithoutMilliseconds(date: Date): Date {
+    const formattedDate = new Date(date);
+    formattedDate.setMilliseconds(0);  // Remove milliseconds
+    return formattedDate;
+}
+
+function generateBlobSasToken(containerName: string, blobName: string): string {
     const accountName = process.env.AZURE_STORAGE_ACCOUNT_NAME!;
     const accountKey = process.env.AZURE_STORAGE_ACCOUNT_KEY!;
     const sharedKeyCredential = new StorageSharedKeyCredential(accountName, accountKey);
 
+    const startsOn = new Date(new Date(Date.now() - 15 * 60 * 1000).toISOString().split('.')[0] + 'Z');
+    const expiresOn = new Date(new Date(Date.now() + 15 * 60 * 1000).toISOString().split('.')[0] + 'Z');
+    console.log('startsOn', startsOn);
+    console.log('expiresOn', expiresOn)
+
     const sasOptions = {
         containerName,
         blobName,
-        permissions: ContainerSASPermissions.parse("r"), // Read-only permissions
-        expiresOn: new Date(new Date().valueOf() + 1800 * 1000), // Expires in 30 minutes
-        protocol: SASProtocol.Https,
+        permissions: BlobSASPermissions.parse("r"), // Read-only permissions for a blob
+        startsOn: new Date(new Date(Date.now() - 15 * 60 * 1000).toISOString().split('.')[0] + 'Z'), // Start time is 5 minutes before now, without milliseconds
+        expiresOn: new Date(new Date(Date.now() + 15 * 60 * 1000).toISOString().split('.')[0] + 'Z'), // Expires 10 minutes from now without milliseconds
+        protocol: SASProtocol.Https, // HTTPS only
+        resource: "b",
+        version: "2022-11-02", // Set the service version to match Azure
     };
 
     // Generate SAS token
     const sasToken = generateBlobSASQueryParameters(sasOptions, sharedKeyCredential).toString();
 
+    console.log("Generated SAS Token:", sasToken);
     return sasToken;
 }
 
