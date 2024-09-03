7226f4e67308d335b1623c25a89f5c2eb35577e4
theGaryLarson <gary.larson@seattlecolleges.edu>
2024-09-02 22:22:52 -0700

    Remove unused imports and comment out techEdMajors data
    Cleaned up unused import of csv-parser and commented out techEdMajors array. Also reduced the number of seeded users from 500 to 250, and added a log message to indicate the seeding process is complete.

--- prisma/seed.mjs
+++ prisma/seed.mjs
@@ -4,7 +4,6 @@ import {faker} from "@faker-js/faker";
 import {users,} from '../app/lib/placeholder-data.mjs';
 import getPrismaClient from '../app/lib/prismaClient.mjs'
 import fs from 'fs';
-import csv from 'csv-parser';
 import { fileURLToPath } from 'url';
 import path from "node:path";
 
@@ -115,61 +114,61 @@ const subcategoriesData = [
     {skill_category: 'Wireless Technologies'},
 ]; // Lightcast IT subcategories data
 
-// replaced with real data from Dynamics (programs table)
-const techEdMajors = [
-    {name: "None", program_id: uuidv4()},
-    {name: "Computer Science", program_id: uuidv4()},
-    {name: "Information Technology", program_id: uuidv4()},
-    {name: "Software Engineering", program_id: uuidv4()},
-    {name: "Cybersecurity", program_id: uuidv4()},
-    {name: "Data Science", program_id: uuidv4()},
-    {name: "Artificial Intelligence", program_id: uuidv4()},
-    {name: "Network Administration", program_id: uuidv4()},
-    {name: "Cloud Computing", program_id: uuidv4()},
-    {name: "Mobile Application Development", program_id: uuidv4()},
-    {name: "Web Development", program_id: uuidv4()},
-    {name: "Database Management", program_id: uuidv4()},
-    {name: "Game Development", program_id: uuidv4()},
-    {name: "Digital Forensics", program_id: uuidv4()},
-    {name: "IT Project Management", program_id: uuidv4()},
-    {name: "Systems Analysis", program_id: uuidv4()},
-    {name: "Computer Engineering", program_id: uuidv4()},
-    {name: "Robotics", program_id: uuidv4()},
-    {name: "Embedded Systems", program_id: uuidv4()},
-    {name: "Computer Graphics", program_id: uuidv4()},
-    {name: "Information Systems", program_id: uuidv4()},
-    {name: "Business Information Technology", program_id: uuidv4()},
-    {name: "Health Informatics", program_id: uuidv4()},
-    {name: "Human-Computer Interaction", program_id: uuidv4()},
-    {name: "Augmented Reality Development", program_id: uuidv4()},
-    {name: "Virtual Reality Development", program_id: uuidv4()},
-    {name: "Blockchain Technology", program_id: uuidv4()},
-    {name: "Machine Learning", program_id: uuidv4()},
-    {name: "Big Data Analytics", program_id: uuidv4()},
-    {name: "IT Support Specialist", program_id: uuidv4()},
-    {name: "DevOps Engineering", program_id: uuidv4()},
-    {name: "IT Networking", program_id: uuidv4()},
-    {name: "Software Quality Assurance", program_id: uuidv4()},
-    {name: "Ethical Hacking", program_id: uuidv4()},
-    {name: "Programming Languages", program_id: uuidv4()},
-    {name: "Technical Writing", program_id: uuidv4()},
-    {name: "IT Entrepreneurship", program_id: uuidv4()},
-    {name: "IT Consulting", program_id: uuidv4()},
-    {name: "Geographic Information Systems (GIS)", program_id: uuidv4()},
-    {name: "Bioinformatics", program_id: uuidv4()},
-    {name: "Quantum Computing", program_id: uuidv4()},
-    {name: "Computer and Network Security", program_id: uuidv4()},
-    {name: "Multimedia Technology", program_id: uuidv4()},
-    {name: "Internet of Things (IoT)", program_id: uuidv4()},
-    {name: "Artificial Intelligence and Machine Learning", program_id: uuidv4()},
-    {name: "IT Service Management", program_id: uuidv4()},
-    {name: "Information Assurance", program_id: uuidv4()},
-    {name: "Software Architecture", program_id: uuidv4()},
-    {name: "Mobile and Web Design", program_id: uuidv4()},
-    {name: "Technology Management", program_id: uuidv4()},
-    {name: "Computer Systems Technology", program_id: uuidv4()},
-    {name: "Technical Studies in IT", program_id: uuidv4()}
-];
+// // replaced with real data from Dynamics (programs table)
+// const techEdMajors = [
+//     {name: "None", program_id: uuidv4()},
+//     {name: "Computer Science", program_id: uuidv4()},
+//     {name: "Information Technology", program_id: uuidv4()},
+//     {name: "Software Engineering", program_id: uuidv4()},
+//     {name: "Cybersecurity", program_id: uuidv4()},
+//     {name: "Data Science", program_id: uuidv4()},
+//     {name: "Artificial Intelligence", program_id: uuidv4()},
+//     {name: "Network Administration", program_id: uuidv4()},
+//     {name: "Cloud Computing", program_id: uuidv4()},
+//     {name: "Mobile Application Development", program_id: uuidv4()},
+//     {name: "Web Development", program_id: uuidv4()},
+//     {name: "Database Management", program_id: uuidv4()},
+//     {name: "Game Development", program_id: uuidv4()},
+//     {name: "Digital Forensics", program_id: uuidv4()},
+//     {name: "IT Project Management", program_id: uuidv4()},
+//     {name: "Systems Analysis", program_id: uuidv4()},
+//     {name: "Computer Engineering", program_id: uuidv4()},
+//     {name: "Robotics", program_id: uuidv4()},
+//     {name: "Embedded Systems", program_id: uuidv4()},
+//     {name: "Computer Graphics", program_id: uuidv4()},
+//     {name: "Information Systems", program_id: uuidv4()},
+//     {name: "Business Information Technology", program_id: uuidv4()},
+//     {name: "Health Informatics", program_id: uuidv4()},
+//     {name: "Human-Computer Interaction", program_id: uuidv4()},
+//     {name: "Augmented Reality Development", program_id: uuidv4()},
+//     {name: "Virtual Reality Development", program_id: uuidv4()},
+//     {name: "Blockchain Technology", program_id: uuidv4()},
+//     {name: "Machine Learning", program_id: uuidv4()},
+//     {name: "Big Data Analytics", program_id: uuidv4()},
+//     {name: "IT Support Specialist", program_id: uuidv4()},
+//     {name: "DevOps Engineering", program_id: uuidv4()},
+//     {name: "IT Networking", program_id: uuidv4()},
+//     {name: "Software Quality Assurance", program_id: uuidv4()},
+//     {name: "Ethical Hacking", program_id: uuidv4()},
+//     {name: "Programming Languages", program_id: uuidv4()},
+//     {name: "Technical Writing", program_id: uuidv4()},
+//     {name: "IT Entrepreneurship", program_id: uuidv4()},
+//     {name: "IT Consulting", program_id: uuidv4()},
+//     {name: "Geographic Information Systems (GIS)", program_id: uuidv4()},
+//     {name: "Bioinformatics", program_id: uuidv4()},
+//     {name: "Quantum Computing", program_id: uuidv4()},
+//     {name: "Computer and Network Security", program_id: uuidv4()},
+//     {name: "Multimedia Technology", program_id: uuidv4()},
+//     {name: "Internet of Things (IoT)", program_id: uuidv4()},
+//     {name: "Artificial Intelligence and Machine Learning", program_id: uuidv4()},
+//     {name: "IT Service Management", program_id: uuidv4()},
+//     {name: "Information Assurance", program_id: uuidv4()},
+//     {name: "Software Architecture", program_id: uuidv4()},
+//     {name: "Mobile and Web Design", program_id: uuidv4()},
+//     {name: "Technology Management", program_id: uuidv4()},
+//     {name: "Computer Systems Technology", program_id: uuidv4()},
+//     {name: "Technical Studies in IT", program_id: uuidv4()}
+// ];
 
 const highSchools = [
     "Alan T. Sugiyama High School",
@@ -1824,7 +1823,7 @@ async function main() {
     await seedSubcategories(); // use in production
     await seedSkills(); // use in production
     await seedSocialMediaPlatforms(); // use in production
-    await seedUsers(500);
+    await seedUsers(250);
     await seedUserAddresses();
     await seedPrograms(); // use in production
     await seedEduProviders(); // use in production
@@ -1849,6 +1848,7 @@ async function main() {
     await seedCompanySocialLinks();
     await seedJobPostings();
     await seedPostalGeoData("../data/postal_geo_data.json");
+    console.log("Finished seeding.\n")
 
 }
 
