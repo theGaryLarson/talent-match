2c72a093806db19cbddd7a31a0c29acc9d5449db
theGaryLarson <gary.larson@seattlecolleges.edu>
2024-09-05 01:33:12 -0700

    Add county and city search API routes
    Implemented new API routes for searching county and city postal geo data based on user-provided terms. Updated the `searchPostalGeoData` function to handle different search fields (zip, county, city).

--- app/api/postal-geo-data/city/search/[terms]/route.ts
+++ app/api/postal-geo-data/city/search/[terms]/route.ts
@@ -2,7 +2,7 @@ import { searchPostalGeoData } from '@/app/lib/prisma';
 
 export async function GET(req: Request, { params }: { params: { terms: string } }) {
     const terms = decodeURIComponent(params.terms);
-    const searchResults = await searchPostalGeoData(terms, 'zip');
+    const searchResults = await searchPostalGeoData(terms, 'city');
 
     return Response.json(
         searchResults,
