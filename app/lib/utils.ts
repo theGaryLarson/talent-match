6019d0e7994f1c9fae808be736ff1777b525449c
theGaryLarson <gary.larson@seattlecolleges.edu>
2024-09-06 17:17:12 -0700

    Refactor and document utility functions
    Added JSDoc comments to utility functions for better documentation. Introduced the `devLog` function for enhanced dev environment logging. Improved function structure and formatting for readability and maintainability.

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -54,12 +54,27 @@ export const generatePagination = (currentPage: number, totalPages: number) => {
   ];
 };
 
+/**
+ * Maps a value to an enum if it exists in the enum values.
+ * If the value is null, returns null.
+ * If the value does not exist in the enum values, returns null.
+ *
+ * @param {string | null} value - The value to map to the enum.
+ * @param {any} enumType - The enum type to map the value to.
+ * @returns {any} - The mapped value or null.
+ */
 export const mapToEnum = (value: string | null, enumType: any): any => {
   if (value == null) return null;
   const enumValues = Object.values(enumType);
   return enumValues.includes(value) ? value : null;
 };
 
+/**
+ * Normalizes a date string to the ISO 8601 format.
+ *
+ * @param {string} date - The date string to be normalized.
+ * @returns {string} - The normalized date string in the ISO 8601 format.
+ */
 export const normalizeDate = (date: string): string => {
   const d = new Date(date);
   d.setUTCDate(1);
@@ -67,9 +82,12 @@ export const normalizeDate = (date: string): string => {
   return d.toISOString();
 };
 
-// Here the country code is being extracted from how its setup on the frontend.
-// We can also just store the country code on the frontend and use libphonenumber-js
-// to extract the code with getCountryCode = getCountryCallingCode(countryCode);
+/**
+ * Formats a phone number in E.164 format.
+ * @param {string|null} phoneCountryCode - The country code of the phone number.
+ * @param {string|null} phone - The phone number to format.
+ * @returns {string|null} - The formatted phone number in E.164 format, or null if the phone number is invalid.
+ */
 export const formatPhoneE164 = (
   phoneCountryCode?: string | null,
   phone?: string | null,
@@ -92,13 +110,17 @@ export const formatPhoneE164 = (
 };
 
 /**
- * Logs the provided arguments to the console in development mode.
+ * Logs messages or objects to the console during development environment.
  *
- * @param {string} [label] - An optional label for the log message.
- * @param {...any} [args] - Arguments to be logged.
+ * @param {string | Record<string, any> | null | undefined} labelOrObject - The label or object to log.
+ * @param {any} [objOrMessage] - The object or message to log.
+ *
  * @returns {void}
  */
-export const devLog = (labelOrObject: string | Record<string, any> | null | undefined, objOrMessage?: any): void => {
+export const devLog = (
+  labelOrObject: string | Record<string, any> | null | undefined,
+  objOrMessage?: any,
+): void => {
   if (process.env.NODE_ENV === 'development') {
     if (typeof labelOrObject === 'string') {
       // Case 2: `devLog('label', object)` or `devLog('label', string)`
@@ -110,7 +132,10 @@ export const devLog = (labelOrObject: string | Record<string, any> | null | unde
         console.log(labelOrObject, 'undefined');
       } else if (typeof objOrMessage === 'object') {
         // Logging an object with a label
-        console.log(labelOrObject, JSON.stringify(objOrMessage, getCircularReplacer(), 2));
+        console.log(
+          labelOrObject,
+          JSON.stringify(objOrMessage, getCircularReplacer(), 2),
+        );
       } else {
         // Logging a string with a label
         console.log(labelOrObject, objOrMessage);
