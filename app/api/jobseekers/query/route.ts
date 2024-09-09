cdbf6301df03370b296dee147ae446999f385775
theGaryLarson <gary.larson@seattlecolleges.edu>
2024-09-09 10:51:57 -0700

    Refactor jobseeker query endpoint for pagination and sorting
    Implement pagination and sorting in the jobseeker query endpoint. Adjusted logic to skip records based on page and limit results.

--- app/api/jobseekers/query/route.ts
+++ app/api/jobseekers/query/route.ts
@@ -1,115 +1,130 @@
 import { NextResponse } from 'next/server';
 import { PrismaClient } from '@prisma/client';
-import {jobSeekerCardViewSelect, jobseekerQueryTestSelect} from "@/app/lib/prisma";
-import {educationRank} from "@/data/dtos/JobSeekerProfileCreationDTOs";
-import {HighestDegreeType} from "@/data/dtos/JobSeekerProfileCreationDTOs";
+import {
+  jobSeekerCardViewSelect,
+  jobseekerQueryTestSelect,
+} from '@/app/lib/prisma';
+import { educationRank } from '@/data/dtos/JobSeekerProfileCreationDTOs';
+import { HighestDegreeType } from '@/data/dtos/JobSeekerProfileCreationDTOs';
 
 const prisma = new PrismaClient();
 
 export async function POST(request: Request) {
-    const {
-        skills = [],
-        industrySector  = [],
-        educationLevel = undefined,
-        yearsWorkExp = 0,
-        zipCode = undefined,
-        sortBy = "newest",
-        maxResults = 50,
-        page = 1,
-    } = await request.json();
+  const {
+    skills = [],
+    industrySector = [],
+    educationLevel = undefined,
+    yearsWorkExp = 0,
+    zipCode = undefined,
+    sortBy = 'newest',
+    maxResults = 50,
+    page = 1,
+  } = await request.json();
 
-    const normalizedSkills: string[] = skills.filter((skill: string) => skill && skill.trim() !== '');
+  const normalizedSkills: string[] = skills.filter(
+    (skill: string) => skill && skill.trim() !== '',
+  );
 
-    const andConditions: any[] = [];
-    andConditions.push({is_marked_deletion: null})
+  const andConditions: any[] = [];
+  andConditions.push({ is_marked_deletion: null });
 
-    if (normalizedSkills.length > 0) {
-        // Here we are checking if the skills are highlighted in projects or listed as their top five
-        // Discuss with team how we want this to be implemented.
-        // Q: Do we want to look in both places as project skills may not be that great?
-        // Or should we allow the employer to choose 1) Top Skills 2) Any Skill 3) Or All?
-        const orConditions = [
-            {
-                jobseeker_has_skills: {
-                    some: {
-                        skills: {
-                            skill_name: {
-                                in: normalizedSkills,
-                            },
-                        },
-                    },
-                },
+  if (normalizedSkills.length > 0) {
+    // Here we are checking if the skills are highlighted in projects or listed as their top five
+    // Discuss with team how we want this to be implemented.
+    // Q: Do we want to look in both places as project skills may not be that great?
+    // Or should we allow the employer to choose 1) Top Skills 2) Any Skill 3) Or All?
+    const orConditions = [
+      {
+        jobseeker_has_skills: {
+          some: {
+            skills: {
+              skill_name: {
+                in: normalizedSkills,
+              },
             },
-            {
-                project_experiences: {
-                    some: {
-                        project_has_skills: {
-                            some: {
-                                skills: {
-                                    skill_name: {
-                                        in: normalizedSkills,
-                                    }
-                                },
-                            },
-                        },
-                    },
-                }
-            }
-        ];
-        andConditions.push({ OR: orConditions });
-    }
+          },
+        },
+      },
+      {
+        project_experiences: {
+          some: {
+            project_has_skills: {
+              some: {
+                skills: {
+                  skill_name: {
+                    in: normalizedSkills,
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    ];
+    andConditions.push({ OR: orConditions });
+  }
 
+  andConditions.push({
+    years_work_exp: {
+      gte: yearsWorkExp,
+    },
+  });
+
+  // Industry Sector Filtering
+  if (industrySector.length > 0) {
     andConditions.push({
-        years_work_exp: {
-            gte: yearsWorkExp,
-        }
+      work_experiences: {
+        some: {
+          industrySector: {
+            sector_title: industrySector,
+          },
+        },
+      },
     });
+  }
 
-    // Industry Sector Filtering
-    if (industrySector.length > 0) {
-        andConditions.push({
-            work_experiences: {
-                some: {
-                    industrySector: {
-                        sector_title: industrySector,
-                    },
-                },
-            },
-        });
-    }
-
-    // Education Level Filtering
-    if (educationLevel) {
-        const minRank: number = educationRank[educationLevel as HighestDegreeType];
-        andConditions.push({
-            highest_level_of_study_completed: {
-                in: Object.keys(educationRank).filter(
-                    (level) => educationRank[level as HighestDegreeType] >= minRank
-                ),
-            },
-        });
-    }
-
-    // Zip Code Filtering
-    if (zipCode) {
-        andConditions.push({
-            users: {
-                user_addresses: {
-                    some: {
-                        zip: {
-                            startsWith: zipCode
-                        },
-                    },
-                },
-            },
-        });
-    }
-
-    // TODO: implement pagination and sorting
-    const filteredJobSeekers = await prisma.jobseekers.findMany({
-        where: andConditions.length > 0 ? { AND: andConditions } : undefined,
-        select:  jobSeekerCardViewSelect // for testing queries in Postman use jobseekerQueryTestSelect //website use: jobSeekerCardViewSelect
+  // Education Level Filtering
+  if (educationLevel) {
+    const minRank: number = educationRank[educationLevel as HighestDegreeType];
+    andConditions.push({
+      highest_level_of_study_completed: {
+        in: Object.keys(educationRank).filter(
+          (level) => educationRank[level as HighestDegreeType] >= minRank,
+        ),
+      },
     });
+  }
 
-    return NextResponse.json(filteredJobSeekers);
+  // Zip Code Filtering
+  if (zipCode) {
+    andConditions.push({
+      users: {
+        user_addresses: {
+          some: {
+            zip: {
+              startsWith: zipCode,
+            },
+          },
+        },
+      },
+    });
+  }
+
+  // Implement sorting based on 'sortBy' (e.g., "newest")
+  const orderBy =
+    sortBy === 'newest' ? [{ createdAt: 'desc' as const }] : undefined;
+
+  // Determine the number of results to skip based on the page number and maxResults
+  const skip = (page - 1) * maxResults;
+
+  // TODO: implement pagination and sorting
+  const filteredJobSeekers = await prisma.jobseekers.findMany({
+    where: andConditions.length > 0 ? { AND: andConditions } : undefined,
+    select: jobseekerQueryTestSelect, // for testing queries in Postman use jobseekerQueryTestSelect //website use: jobSeekerCardViewSelect
+    take: maxResults,
+    skip: skip,
+    orderBy: orderBy,
+  });
+
+  return NextResponse.json(filteredJobSeekers);
 }
