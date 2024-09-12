31f9ddcced5a8014e3894d0f86d55ead88530c75
theGaryLarson <gary.larson@seattlecolleges.edu>
2024-09-12 14:34:54 -0700

    Remove gender and race from upsert logic

--- app/api/employers/account/personal-info/upsert/route.ts
+++ app/api/employers/account/personal-info/upsert/route.ts
@@ -1,77 +1,84 @@
-import {NextResponse} from "next/server";
-import getPrismaClient from "@/app/lib/prismaClient.mjs";
-import {PrismaClient} from "@prisma/client";
-import {PostEmployerPersonalDTO, ReadEmployerPersonalDTO} from "@/data/dtos/EmployerProfileCreationDTOs";
-import parsePhoneNumberFromString from "libphonenumber-js";
-import {formatPhoneE164} from "@/app/lib/utils";
+import { NextResponse } from 'next/server';
+import getPrismaClient from '@/app/lib/prismaClient.mjs';
+import { PrismaClient } from '@prisma/client';
+import {
+  PostEmployerPersonalDTO,
+  ReadEmployerPersonalDTO,
+} from '@/data/dtos/EmployerProfileCreationDTOs';
+import parsePhoneNumberFromString from 'libphonenumber-js';
+import { formatPhoneE164 } from '@/app/lib/utils';
 
 const prisma: PrismaClient = getPrismaClient();
 
 export async function POST(request: Request) {
-    try{
-        const body: PostEmployerPersonalDTO = await request.json();
-        const {
-            userId,
-            firstName,
-            lastName,
-            birthDate,
-            email,
-            phoneCountryCode,
-            phone,
-            gender,
-            race,
-            photoUrl,
-        } = body;
+  try {
+    const body: PostEmployerPersonalDTO = await request.json();
+    const {
+      userId,
+      firstName,
+      lastName,
+      birthDate,
+      email,
+      phoneCountryCode,
+      phone,
+      photoUrl,
+    } = body;
 
-        const formattedPhone = formatPhoneE164(phoneCountryCode, phone)
-        const upsertedUser = await prisma.user.upsert({
-            where: {
-                id: userId
-            },
-            update: {
-                first_name: firstName,
-                last_name: lastName,
-                birthdate: new Date(birthDate).toISOString(),
-                email: email,
-                phone: formattedPhone,
-                gender: gender,
-                race: race,
-                photo_url: photoUrl,
-                updatedAt: new Date(),
-            },
-            create: {
-                id: userId,
-                role: 'EMPLOYER',
-                first_name: firstName,
-                last_name: lastName,
-                birthdate: new Date(birthDate).toISOString(),
-                email: email,
-                phone: formattedPhone,
-                gender: gender,
-                race: race,
-                photo_url: photoUrl,
-                createdAt: new Date(),
-            }
-        })
+    const formattedPhone = formatPhoneE164(phoneCountryCode, phone);
+    const upsertedUser = await prisma.user.upsert({
+      where: {
+        id: userId,
+      },
+      update: {
+        first_name: firstName,
+        last_name: lastName,
+        birthdate: new Date(birthDate).toISOString(),
+        email: email,
+        phone: formattedPhone,
+        photo_url: photoUrl,
+        updatedAt: new Date(),
+      },
+      create: {
+        id: userId,
+        role: 'EMPLOYER',
+        first_name: firstName,
+        last_name: lastName,
+        birthdate: new Date(birthDate).toISOString(),
+        email: email,
+        phone: formattedPhone,
+        gender: undefined,
+        race: undefined,
+        photo_url: photoUrl,
+        createdAt: new Date(),
+      },
+    });
 
-        const result: ReadEmployerPersonalDTO = {
-            userId: upsertedUser.id,
-            firstName: upsertedUser.first_name,
-            lastName: upsertedUser.last_name,
-            birthDate: upsertedUser?.birthdate?.toISOString(),
-            email: upsertedUser.email,
-            phoneCountryCode: upsertedUser?.phone ? parsePhoneNumberFromString(upsertedUser.phone)?.countryCallingCode : null,
-            phone: upsertedUser?.phone ? parsePhoneNumberFromString(upsertedUser.phone)?.number : null,
-            gender: upsertedUser.gender,
-            race: upsertedUser.race,
-            photoUrl: upsertedUser.photo_url,
-        }
-        return NextResponse.json({success:true, result}, {status: 200})
-
-    } catch(e: any) {
-        console.error('Error upserting employer:', e.message);
-        return NextResponse.json({error: `Failed to upsert employer personal information.\n${e.message}`}, {status: 500});
-    } finally {
-        await prisma.$disconnect();
-    }
+    const result: ReadEmployerPersonalDTO = {
+      userId: upsertedUser.id,
+      firstName: upsertedUser.first_name,
+      lastName: upsertedUser.last_name,
+      birthDate: upsertedUser?.birthdate?.toISOString(),
+      email: upsertedUser.email,
+      phoneCountryCode: upsertedUser?.phone
+        ? parsePhoneNumberFromString(upsertedUser.phone)?.countryCallingCode
+        : null,
+      phone: upsertedUser?.phone
+        ? parsePhoneNumberFromString(upsertedUser.phone)?.number
+        : null,
+      gender: upsertedUser.gender,
+      race: upsertedUser.race,
+      photoUrl: upsertedUser.photo_url,
+    };
+    return NextResponse.json({ success: true, result }, { status: 200 });
+  } catch (e: any) {
+    console.error('Error upserting employer:', e.message);
+    return NextResponse.json(
+      {
+        error: `Failed to upsert employer personal information.\n${e.message}`,
+      },
+      { status: 500 },
+    );
+  } finally {
+    await prisma.$disconnect();
+  }
 }
