33d496410ab845ec8b2b66c02d5c2b78b3ecc3dc
Quincy Pham <quincytpham@gmail.com>
2024-09-17 17:34:06 -0400

    updated employer personal page to use redux, all field ids adjusted to match dto, appears in redux dev tools, however other pages need to have form slice removed, will work on removing next

--- app/(profile-creation)/create-profile/employer/company/page.tsx
+++ app/(profile-creation)/create-profile/employer/company/page.tsx
@@ -2,18 +2,9 @@
 
 import React, { ChangeEvent, FormEvent, useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
-import type { RootState } from '@/lib/store';
+import type { RootState } from '@/lib/employerStore';
 import { v4 as uuidv4 } from 'uuid'; // Import uuidv4
 import { useSelector, useDispatch } from 'react-redux';
-import {
-  addField,
-  updateField,
-  submitForm,
-  submitFormSuccess,
-  submitFormFailure,
-  FormState,
-  FormField,
-} from '@/lib/features/profileCreation/formSlice';
 import InputTextWithLabel from '@/app/ui/components/InputTextWithLabel';
 import SelectOptionsWithLabel from '@/app/ui/components/SelectOptionsWithLabel';
 import ProgressBarFlat from '@/app/ui/components/ProgressBarFlat';
