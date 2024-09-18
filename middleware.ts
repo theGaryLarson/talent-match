478cedbc89c7daad562565c4b79dc7d6f19cbf04
Rory.Hayes <rorosaurus@gmail.com>
2024-09-18 16:07:37 -0700

    change public to guest, restructure auth routing

--- middleware.ts
+++ middleware.ts
@@ -11,26 +11,36 @@ import { Role } from "./data/dtos/UserInfoDTO";
 export default auth((req) => {
   const pathname = req.nextUrl.pathname;
   const userRoles = req.auth?.user?.roles || [];
+  const userId = req.auth?.user?.id;
 
-  const jobseekersOnly = [
+    // Routes for logged in users with jobseeker role
+    const guestRoutes = [
+      "/signup",
+      "/signout",
+    ];
+
+  // Routes for logged in users with jobseeker role
+  const jobseekerRoutes = [
+    "/signup/jobseeker",
+    
     "/create-profile/jobseeker/complete",
     "/create-profile/jobseeker/disclosures",
     "/create-profile/jobseeker/education",
-    "/create-profile/jobseeker/intro",
+    "/create-profile/jobseeker/introduction",
     "/create-profile/jobseeker/preferences",
     "/create-profile/jobseeker/showcase",
     "/create-profile/jobseeker/work-experience",
-  ]
 
-  const jobseekerRoutes = [
-    "/services/joblistings/[id]",
-    "/services/jobseekers/[id]",
+    // "/services/joblistings/[id]", // out of scope for MVP
     "/services/jobseekers/dashboard",
+
     "/signout",
   ];
 
+  // Routes for logged in users with employer role
   const employerRoutes = [
-    "/services/employers/dashboard",
+    "/signup/employer",
+
     "/create-profile/employer/personal",
     "/create-profile/employer/company-info",
     "/create-profile/employer/professional-info",
@@ -39,28 +49,32 @@ export default auth((req) => {
     "/create-profile/employer/mission",
     "/create-profile/employer/video",
     "/create-profile/employer/congratulations",
+
+    "/services/employers/dashboard",
+    "/services/employers/dashboard/listview",
+    // "/services/joblistings/[id]", // out of scope for MVP
+    "/services/jobseekers/[id]",
+
+    "/signout",
   ];
 
+  // Routes for any public, non-logged in user
   const publicRoutes = [
     "/signin",
-    "/signup",
-    "/signup/employer",
-    "/signup/jobseeker",
     "/",
     "/pre-apprenticeship",
     "/services",
     "/services/employers",
-    "/services/employers/dashboard/listview",
     "/services/employers/faq",
-    "/services/joblistings",
+    // "/services/joblistings", // out of scope for MVP
     "/services/jobseekers",
-    "/create-profile/employer",
     "/cfa_images/",
   ];
   
-  const allowedRolesForJobseekersOnly = [Role.ADMIN, Role.JOBSEEKER];
-  const allowedRolesForJobseekerRoutes = [Role.ADMIN, Role.EMPLOYER, Role.JOBSEEKER];
-  const allowedRolesForEmployerRoutes = [Role.ADMIN, Role.EMPLOYER];
+  const rolesForGuest = [/*Role.ADMIN,*/ Role.GUEST]; // disable admin routing for now
+  const rolesForJobseeker = [/*Role.ADMIN,*/ Role.JOBSEEKER]; // disable admin routing for now
+  const rolesForEmployer = [/*Role.ADMIN,*/ Role.EMPLOYER]; // disable admin routing for now
+
   const homeUrl = new URL("/", req.nextUrl.origin);
 
   // Explicitly allow public routes
@@ -68,15 +82,16 @@ export default auth((req) => {
     if (publicRoutes.includes(pathname)) {
       return NextResponse.next();
     } else {
+      console.log("Access denied: This page is not public - " + pathname);
       return NextResponse.redirect(homeUrl);
     }
   }
 
   // If you're signed in and haven't picked a role, you gotta
-  if (req.auth && userRoles.includes(Role.PUBLIC) && pathname !== "/signup") {
+  if (req.auth && userRoles.includes(Role.GUEST) && pathname !== "/signup") {
     const signUpUrl = new URL("/signup", req.nextUrl.origin);
     return NextResponse.redirect(signUpUrl);
-  }
+  } // TODO: maybe allow guest signout? low priority for now
 
   // If you're at signin and logged in, reroute to the main page
   if (req.auth && pathname === "/signin") {
@@ -88,25 +103,29 @@ export default auth((req) => {
     return NextResponse.redirect(homeUrl);
   }
 
-  if (jobseekersOnly.some((route) => pathname.includes(route))) {
-    if (!allowedRolesForJobseekersOnly.some((role) => userRoles.includes(role))) {
-      console.log("Access denied: User does not have permission for jobseeker route");
-      return NextResponse.redirect(homeUrl);
-    }
-    return NextResponse.next();
-  }
-
+  // Route checking for jobseeker routes
   if (jobseekerRoutes.some((route) => pathname.includes(route))) {
-    if (!allowedRolesForJobseekerRoutes.some((role) => userRoles.includes(role))) {
+    // TODO: only allow jobseekers to view their own profile
+    if (!rolesForJobseeker.some((role) => userRoles.includes(role))) {
       console.log("Access denied: User does not have permission for jobseeker route");
       return NextResponse.redirect(homeUrl);
     }
     return NextResponse.next();
   }
 
+  // Route checking for employer routes
   if (employerRoutes.some((route) => pathname.includes(route))) {
-    if (!allowedRolesForEmployerRoutes.some((role) => userRoles.includes(role))) {
+    if (!rolesForEmployer.some((role) => userRoles.includes(role))) {
       console.log("Access denied: User does not have permission for employer route");
+      return NextResponse.redirect(homeUrl);
+    }
+    return NextResponse.next();
+  }
+
+  // Route checking for guest routes
+  if (guestRoutes.some((route) => pathname.includes(route))) {
+    if (!rolesForGuest.some((role) => userRoles.includes(role))) {
+      console.log("Access denied: User does not have permission for guest route");
       return NextResponse.redirect(homeUrl);
     }
     return NextResponse.next();
