73301a6637b7fc91483113547629800486dc5f60
theGaryLarson <gary.larson@seattlecolleges.edu>
2024-09-18 21:58:38 -0700

    Add example.com to image domains

--- next.config.js
+++ next.config.js
@@ -21,6 +21,10 @@ const nextConfig = {
             protocol: 'https',
             hostname: 'randomuser.me'
           },
+          {
+              protocol: 'http',
+              hostname: 'example.com'
+          }
         ],
       }
 };
