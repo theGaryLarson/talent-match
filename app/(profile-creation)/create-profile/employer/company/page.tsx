2a0b6318ced7ecef0a7666dd23adbe83b0882547
theGaryLarson <gary.larson@seattlecolleges.edu>
2024-09-20 14:08:37 -0700

    Refactor company page in employer profile creation
    Remove redundant function and use imported getFieldValue for consistency. Implement UUID for company ID generation, and make minor adjustments to variable naming and comments for clarity.

--- app/(profile-creation)/create-profile/employer/company/page.tsx
+++ app/(profile-creation)/create-profile/employer/company/page.tsx
@@ -29,6 +29,8 @@ import { IndustrySectorDropdownDTO } from '@/data/dtos/IndustrySectorDropdownDTO
 import { useSession } from 'next-auth/react';
 import { useUpdateSession } from '@/app/lib/auth/useUpdateSession';
 import { PostCompanyInfoDTO } from '@/data/dtos/EmployerProfileCreationDTOs';
+import { v4 as uuidv4 } from 'uuid';
+import {getFieldValue} from "@/app/lib/utils";
 
 export default function CreateEmployerCompanyInfoPage() {
   const { fields, isSubmitting, error }: FormState = useSelector(
@@ -99,6 +101,8 @@ export default function CreateEmployerCompanyInfoPage() {
           : null,
       );
       setLogoUrl(companyObject.companyLogoUrl);
+    } else {
+      setCompanyId(uuidv4())
     }
   }, [companyObject, dispatch]);
 
@@ -124,19 +128,7 @@ export default function CreateEmployerCompanyInfoPage() {
     }
   };
 
-  function getFieldValue<T = string>(id: string, defaultValue: T): T {
-    const fieldValue = fields.find((f: FormField) => f.id === id)?.value;
 
-    if (fieldValue === undefined || fieldValue === null) {
-      return defaultValue; // Return default value if fieldValue is undefined or null
-    }
-
-    if (typeof fieldValue === typeof defaultValue) {
-      return fieldValue as T; // Cast fieldValue to the type of defaultValue
-    }
-
-    return defaultValue;
-  }
 
   const handleImageUpload = (url: string) => {
     // Update the local state with the uploaded image URL
@@ -176,7 +168,7 @@ export default function CreateEmployerCompanyInfoPage() {
       };
       // update employer session if the company already exists
       await updateSessionProperties({
-        companyId,
+        companyId: companyId,
         companyIsApproved: companyObject.approvedCompany,
       });
     } else {
@@ -184,22 +176,23 @@ export default function CreateEmployerCompanyInfoPage() {
       // and set the employer session data on the following page.
       formData = {
         userId: session?.user.id!,
-        employerId: undefined, // TODO: fetched on
-        companyId: undefined, // TODO: fetch this on the following page with updateSessionProperties custom hook.
+        employerId: undefined, // TODO: fetch this on the following page with updateSessionProperties custom hook.
+        companyId: companyId!, // TODO: fetch this on the following page with updateSessionProperties custom hook.
         industrySectorId: industry ? industry.industry_sector_id : null,
         industrySectorTitle: industry ? industry.sector_title : null,
         companyName: companyObject || '',
         logoUrl: logoUrl,
         companyEmail: getFieldValue<string>(
+              fields,
           'profile-creation-company-email',
           '',
         ),
         yearFounded: year_founded?.toISOString()!,
-        websiteUrl: getFieldValue('profile-creation-company-website', ''),
+        websiteUrl: getFieldValue(fields,'profile-creation-company-website', ''),
         phoneCountryCode: 'United States +1',
-        companyPhone: getFieldValue('profile-creation-company-phone', ''),
-        size: getFieldValue<string>('profile-creation-company-size', ''),
-        estimatedAnnualHires: getFieldValue(
+        companyPhone: getFieldValue(fields,'profile-creation-company-phone', ''),
+        size: getFieldValue<string>(fields,'profile-creation-company-size', ''),
+        estimatedAnnualHires: getFieldValue(fields,
           'profile-creation-company-annual-hire',
           '',
         ),
@@ -286,7 +279,7 @@ export default function CreateEmployerCompanyInfoPage() {
               fieldLabel="Company Name *"
               id="profile-creation-company-name"
               searchingText="Searching..."
-              noResultsText="No companies found..."
+              noResultsText="No company found..."
               value={companyObject ?? ''}
               onChange={(e, val) => setCompanyObject(val ?? '')}
               searchPlaceholder="Company name"
@@ -355,7 +348,7 @@ export default function CreateEmployerCompanyInfoPage() {
                 value={
                   typeof companyObject === 'object' && companyObject !== null
                     ? companyObject.companyWebsite || ''
-                    : getFieldValue('profile-creation-company-website', '')
+                    : getFieldValue(fields,'profile-creation-company-website', '')
                 }
                 disabled={typeof companyObject === 'object'}
                 required
@@ -370,7 +363,7 @@ export default function CreateEmployerCompanyInfoPage() {
                 value={
                   typeof companyObject === 'object' && companyObject !== null
                     ? companyObject.companyEmail || ''
-                    : getFieldValue('profile-creation-company-email', '')
+                    : getFieldValue(fields,'profile-creation-company-email', '')
                 }
                 disabled={typeof companyObject === 'object'}
                 required
@@ -385,7 +378,7 @@ export default function CreateEmployerCompanyInfoPage() {
                 value={
                   typeof companyObject === 'object' && companyObject !== null
                     ? companyObject.companyPhone || ''
-                    : getFieldValue('profile-creation-company-phone', '')
+                    : getFieldValue(fields,'profile-creation-company-phone', '')
                 }
                 disabled={typeof companyObject === 'object'}
                 required
@@ -427,7 +420,7 @@ export default function CreateEmployerCompanyInfoPage() {
                   companyObject !== null &&
                   companyObject.companySize
                     ? companyObject.companySize
-                    : getFieldValue('profile-creation-company-size', '')
+                    : getFieldValue(fields,'profile-creation-company-size', '')
                 }
                 disabled={typeof companyObject === 'object'}
               >
@@ -442,7 +435,7 @@ export default function CreateEmployerCompanyInfoPage() {
                   companyObject !== null &&
                   companyObject.predictedHires
                     ? companyObject.predictedHires
-                    : getFieldValue('profile-creation-company-annual-hire', '')
+                    : getFieldValue(fields,'profile-creation-company-annual-hire', '')
                 }
                 required
                 disabled={typeof companyObject === 'object'}
