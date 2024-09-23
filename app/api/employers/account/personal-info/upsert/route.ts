158ea356fc0a86e2b9af4f69347ce532fde97dea
theGaryLarson <gary.larson@seattlecolleges.edu>
2024-09-23 14:00:53 -0700

    Add is_marked_deletion timestamp field to upsert data
    Introduced the `is_marked_deletion` timestamp field to the upsert object in the personal-info API route. This field indicates when the data is marked for deletion, set to 30 days from the current date. This addition enhances data lifecycle management and allows for automatic clean-up of outdated records.

--- app/api/employers/account/personal-info/upsert/route.ts
+++ app/api/employers/account/personal-info/upsert/route.ts
@@ -51,6 +51,7 @@ export async function POST(request: Request) {
         race: undefined,
         photo_url: photoUrl,
         createdAt: new Date(),
+        is_marked_deletion: new Date().setDate(Date.now()+30).toString(),
       },
     });
 
