bf1f71d51b6d43e918208f745d3d971408882fc6
theGaryLarson <gary.larson@seattlecolleges.edu>
2024-09-25 12:18:43 -0700

    Refactor form data initialization and handling.

--- app/(profile-creation)/create-profile/employer/about/page.tsx
+++ app/(profile-creation)/create-profile/employer/about/page.tsx
@@ -13,25 +13,24 @@ import { Button, Progress } from 'flowbite-react';
 import TextareaWithLabel from '@/app/ui/components/TextareaWithLabel';
 import { useSession } from 'next-auth/react';
 import { useUpdateSession } from '@/app/lib/auth/useUpdateSession';
-import { getFieldValue } from '@/app/lib/utils';
 
 export default function CreateEmployerCompanyInfoAboutPage() {
-  const { fields, isSubmitting, error }: FormState = useSelector(
-    (state: RootState) => state.form,
-  );
+  // const { fields, isSubmitting, error }: FormState = useSelector(
+  //   (state: RootState) => state.form,
+  // );
   const dispatch = useDispatch();
   const router = useRouter();
 
   const [newFieldId, setNewFieldId] = useState('');
   const [newFieldLabel, setNewFieldLabel] = useState('');
   const [newFieldType, setNewFieldType] = useState<
-    'text' | 'email' | 'number' | 'select' | 'radio'
+      'text' | 'email' | 'number' | 'select' | 'radio'
   >('text');
   const [newFieldValue, setNewFieldValue] = useState('');
   const [newFieldOptions, setNewFieldOptions] = useState<
-    { value: string | number; label: string }[]
+      { value: string | number; label: string }[]
   >([]);
-  const { data: session, update, status } = useSession();
+  const {data: session, update, status} = useSession();
   const updateSessionProperties = useUpdateSession();
 
   useEffect(() => {
@@ -40,20 +39,20 @@ export default function CreateEmployerCompanyInfoAboutPage() {
 
       try {
         const response = await fetch(
-          `/api/employers/account/professional-info/get/${session.user.id}`,
-          {
-            method: 'GET',
-            headers: {
-              'Content-Type': 'application/json',
+            `/api/employers/account/professional-info/get/${session.user.id}`,
+            {
+              method: 'GET',
+              headers: {
+                'Content-Type': 'application/json',
+              },
             },
-          },
         );
 
         if (!response.ok) {
           throw new Error(`Error: ${response.status} ${response.statusText}`);
         }
 
-        const { success, result } = await response.json(); // Destructure response
+        const {success, result} = await response.json(); // Destructure response
 
         if (success && result) {
           const {
@@ -81,122 +80,123 @@ export default function CreateEmployerCompanyInfoAboutPage() {
     }
   }, [session?.user?.id]);
   const handleFieldChange = (
-    e: ChangeEvent<HTMLInputElement | HTMLSelectElement>,
+      e: ChangeEvent<HTMLInputElement | HTMLSelectElement>,
   ) => {
-    const { name, value } = e.target;
+    const {name, value} = e.target;
     console.log(name, value);
-    const field = fields.find((field) => field.id === name);
-    if (field) {
-      const parsedValue = field.type === 'number' ? parseInt(value, 10) : value;
-      dispatch(updateField({ id: field.id, value: parsedValue }));
-    } else {
-      dispatch(
-        addField({
-          id: e.target.id,
-          label: newFieldLabel,
-          value: e.target.value,
-          type: newFieldType,
-          options: newFieldOptions,
-        }),
-      );
-    }
-  };
+    // const field = fields.find((field) => field.id === name);
+    //   if (field) {
+    //     const parsedValue = field.type === 'number' ? parseInt(value, 10) : value;
+    //     dispatch(updateField({ id: field.id, value: parsedValue }));
+    //   } else {
+    //     dispatch(
+    //       addField({
+    //         id: e.target.id,
+    //         label: newFieldLabel,
+    //         value: e.target.value,
+    //         type: newFieldType,
+    //         options: newFieldOptions,
+    //       }),
+    //     );
+    //   }
+    // };
 
-  const handleAddField = () => {
-    if (newFieldLabel) {
-      dispatch(
-        addField({
-          id: newFieldId,
-          label: newFieldLabel,
-          value: newFieldValue,
-          type: newFieldType,
-          options:
-            newFieldType === 'select' || newFieldType === 'radio'
-              ? newFieldOptions
-              : undefined,
-        }),
-      );
-      setNewFieldId('');
-      setNewFieldLabel('');
-      setNewFieldType('text');
-      setNewFieldValue('');
-      setNewFieldOptions([]);
-    }
-  };
+    // const handleAddField = () => {
+    //   if (newFieldLabel) {
+    //     dispatch(
+    //       addField({
+    //         id: newFieldId,
+    //         label: newFieldLabel,
+    //         value: newFieldValue,
+    //         type: newFieldType,
+    //         options:
+    //           newFieldType === 'select' || newFieldType === 'radio'
+    //             ? newFieldOptions
+    //             : undefined,
+    //       }),
+    //     );
+    //     setNewFieldId('');
+    //     setNewFieldLabel('');
+    //     setNewFieldType('text');
+    //     setNewFieldValue('');
+    //     setNewFieldOptions([]);
+    //   }
+    // };
 
-  const handleSubmit = async (e: FormEvent) => {
-    e.preventDefault();
-    dispatch(submitForm());
+    // const handleSubmit = async (e: FormEvent) => {
+    //   e.preventDefault();
+    //   dispatch(submitForm());
+    //
+    //   const formData = {
+    //     companyId: session?.user.companyId,
+    //     aboutUs: getFieldValue(fields, 'profile-creation-company-about', ''),
+    //   };
+    //
+    //   try {
+    //     const response = await fetch('/api/companies/about/update/', {
+    //       method: 'PATCH',
+    //       headers: {
+    //         'Content-Type': 'application/json',
+    //       },
+    //       body: JSON.stringify(formData),
+    //     });
+    //
+    //     if (response.ok) {
+    //       const result = await response.json();
+    //       dispatch(submitFormSuccess());
+    //       router.push('/create-profile/employer/mission');
+    //     } else {
+    //       const errorData = await response.json();
+    //       dispatch(
+    //         submitFormFailure(errorData.error || 'Failed to submit the form'),
+    //       );
+    //     }
+    //   } catch (error) {
+    //     dispatch(submitFormFailure('Failed to submit the form'));
+    //   }
+    // };
 
-    const formData = {
-      companyId: session?.user.companyId,
-      aboutUs: getFieldValue(fields, 'profile-creation-company-about', ''),
-    };
+    return (
+        <main className="flex justify-center">
+          <aside className="profile-form-aside"></aside>
+          <section className="profile-form-section">
+            <ProgressBarFlat progress={(3 / 6) * 100} size="sm"/>
+            <p>Step 3/6</p>
+            <h1>Company Info</h1>
+            <p className="subtitle">* Indicates a required field</p>
 
-    try {
-      const response = await fetch('/api/companies/about/update/', {
-        method: 'PATCH',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(formData),
-      });
-
-      if (response.ok) {
-        const result = await response.json();
-        dispatch(submitFormSuccess());
-        router.push('/create-profile/employer/mission');
-      } else {
-        const errorData = await response.json();
-        dispatch(
-          submitFormFailure(errorData.error || 'Failed to submit the form'),
-        );
-      }
-    } catch (error) {
-      dispatch(submitFormFailure('Failed to submit the form'));
-    }
-  };
-
-  return (
-    <main className="flex justify-center">
-      <aside className="profile-form-aside"></aside>
-      <section className="profile-form-section">
-        <ProgressBarFlat progress={(3 / 6) * 100} size="sm" />
-        <p>Step 3/6</p>
-        <h1>Company Info</h1>
-        <p className="subtitle">* Indicates a required field</p>
-
-        <h2>About</h2>
-        <form onSubmit={handleSubmit}>
-          Tell us about your company *
-          <div className="profile-form-grid">
-            <fieldset>
-              <TextareaWithLabel
-                id="profile-creation-company-about"
-                placeholder="About your company"
-                rows="16"
-                onChange={handleFieldChange}
-                required
-                defaultValue={getFieldValue(
-                  fields,
-                  'profile-creation-company-about',
-                  '',
-                )}
-              >
-                {/* Tell us about your company * */}
-              </TextareaWithLabel>
-            </fieldset>
-          </div>
-          <div className="profile-form-progress-btn-group">
-            <Button pill className="custom-outline-btn">
-              Cancel
-            </Button>
-            <Button pill type="submit">
-              Save and continue
-            </Button>
-          </div>
-        </form>
-      </section>
-    </main>
-  );
+            <h2>About</h2>
+            <form >
+              Tell us about your company *
+              <div className="profile-form-grid">
+                <fieldset>
+                  <TextareaWithLabel
+                      id="profile-creation-company-about"
+                      placeholder="About your company"
+                      rows="16"
+                      onChange={handleFieldChange}
+                      required
+                      // defaultValue={getFieldValue(
+                      //   fields,
+                      //   'profile-creation-company-about',
+                      //   '',
+                      // )}
+                  >
+                    {/* Tell us about your company * */}
+                  </TextareaWithLabel>
+                </fieldset>
+              </div>
+              <div className="profile-form-progress-btn-group">
+                <Button pill className="custom-outline-btn">
+                  Cancel
+                </Button>
+                <Button pill type="submit">
+                  Save and continue
+                </Button>
+              </div>
+            </form>
+          </section>
+        </main>
+    );
+  }
 }
