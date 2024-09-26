2b518c6b505443006d7f60ff07c7276252aafdf5
Quincy Pham <quincytpham@gmail.com>
2024-09-26 15:27:59 -0400

    added mission get route, and completed mission page redux updates

---
+++ app/api/companies/mission/get/[companyId]/route.ts
@@ -0,0 +1,51 @@
+import { NextResponse } from 'next/server';
+import getPrismaClient from '@/app/lib/prismaClient.mjs';
+import { PrismaClient } from '@prisma/client';
+import { ReadEmployerMissionDTO } from '@/data/dtos/EmployerProfileCreationDTOs';
+const prisma: PrismaClient = getPrismaClient();
+
+export async function GET(
+  request: Request,
+  { params }: { params: { companyId: string } },
+) {
+  try {
+    const companyId = params.companyId;
+
+    if (!companyId) {
+      return NextResponse.json(
+        { success: false, error: `A uuidv4 companyId is required.` },
+        { status: 400 },
+      );
+    }
+    const companyInfo = await prisma.companies.findUnique({
+      where: {
+        company_id: companyId,
+      },
+      select: {
+        company_id: true,
+        company_mission: true,
+      },
+    });
+
+    if (!companyInfo) {
+      return NextResponse.json({
+        success: true,
+        error: `No entry exists for companyId: ${companyId}`,
+      });
+    }
+
+    const result: ReadEmployerMissionDTO = {
+      companyId: companyInfo.company_id,
+      mission: companyInfo.company_mission,
+    };
+    return NextResponse.json({ success: true, result }, { status: 200 });
+  } catch (e: any) {
+    console.error('Error retrieving company record.', e.message);
+    return NextResponse.json(
+      { error: `Error retrieving company record.\n${e.message}` },
+      { status: 500 },
+    );
+  } finally {
+    await prisma.$disconnect();
+  }
+}
