28074dbb332183b682ace685ccc134aa9b4536d4
theGaryLarson <gary.larson@seattlecolleges.edu>
2024-09-27 15:21:07 -0700

    Fix import path for ProfileCreationHeader
    Corrected the import path from CFAProfileCreationHeader component in the jobseeker profile creation layout.

--- app/(profile-creation)/create-profile/jobseeker/layout.tsx
+++ app/(profile-creation)/create-profile/jobseeker/layout.tsx
@@ -5,7 +5,7 @@ import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
 import '@/app/ui/global.css';
 import '@/app/ui/profile-creation.css';
 import { inter } from '@/app/ui/fonts';
-import CFAProfileCreationHeader from '@/app/ui/CFAProfileCreationHeader';
+import CFAProfileCreationHeader from '@/app/ui/ProfileCreationHeader';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 
 // REVIEW: You can locate the store in the layout component if all the routes using that layout need the store. 
