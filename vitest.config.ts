a60f25bd2ca11570ae485d1d590b4a651b9e9d57
Gage Berz <gageberz@computingforall.org>
2024-09-30 18:58:39 +0000

    Merged PR 39: pipelines: Testing and coverage added

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,6 +6,9 @@ export default defineConfig({
     plugins: [react()],
     test: {
         environment: 'jsdom',
+        coverage: {
+            reporter: ['cobertura', 'text'],
+        },
     },
     resolve: {
         alias: {
