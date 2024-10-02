9d05be96cc4eaaebe709c22ee2e4d56504fd04ad
not-damien <93546644+not-damien@users.noreply.github.com>
2024-10-02 12:09:20 -0700

    small obtimizations on landing page

--- app/ui/components/TCPortalFunctionsFold.tsx
+++ app/ui/components/TCPortalFunctionsFold.tsx
@@ -1,23 +1,27 @@
-'use client'
+'use client';
 import Image from 'next/image';
 import { useEffect, useState } from 'react';
 export default function TCPortalFunctionsFold() {
-  const [index, setIndex] = useState<number>(0);  
+  const [index, setIndex] = useState<number>(0);
   const tabs = [
     { src: '/images/SearchableDB.svg', text: 'Searchable Database', index: 0 },
-    { src: '/images/SkillGapAnalysis.svg', text: 'Skills Gap Analysis', index: 1 },
-    { src: '/images/DLTicon.svg', text: 'Diverse Local Talent', index: 2 } // Change the text here
-  ]
+    {
+      src: '/images/SkillGapAnalysis.svg',
+      text: 'Skills Gap Analysis',
+      index: 1,
+    },
+    { src: '/images/DLTicon.svg', text: 'Diverse Local Talent', index: 2 }
+  ];
   const Subtitle = [
     'Our searchable database quickly connects you with qualified candidates who match your specific needs.',
     'Uncover hidden tech talent, assess their fit, and quickly find the perfect match for your team—all in one place.',
-    'Discover a diverse pool of talented candidates from various backgrounds and experiences, ready to contribute unique perspectives to your team'
-  ]
+    'Discover a diverse pool of talented candidates from various backgrounds and experiences, ready to contribute unique perspectives to your team',
+  ];
   const Content = [
-  "/images/CandidateSearchTabContent.png",
-    "/images/SkillsGapContent.png",
-    "/images/DiverseLocalContent.png"
-  ]
+    '/images/CandidateSearchTabContent.png',
+    '/images/SkillsGapContent.png',
+    '/images/DiverseLocalContent.png',
+  ];
   // Automatically switch tabs every 4 seconds
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -30,7 +34,7 @@ export default function TCPortalFunctionsFold() {
   //TODO: add transition on tab content
   //TODO: Replace content images with higher quality images from figma & crop so they have the same aspect raito
   return (
-    <div className='flex flex-col items-center gap-[42px]'>
+    <div className="flex flex-col items-center gap-[42px]">
       <div className="text-center">
         <span className="font-['Roboto'] text-[88px] font-normal capitalize leading-[123.20px] text-[#014260]">
           Talent & Career{' '}
@@ -39,48 +43,60 @@ export default function TCPortalFunctionsFold() {
           Portal
         </span>
       </div>
-      
+
       <div className="text-center font-['Roboto'] text-[22px] font-normal leading-[30.80px] text-[#0f1728]">
-       {Subtitle.filter((text, i)=>i==index)}
+      {Subtitle[index]}
       </div>
-      <div className='flex items center gap-4'>
-      {tabs.map((tab, i) => (
-      <TabButton
-        key={i}
-        src={tab.src}
-        text={tab.text}
-        isSelected={index === tab.index}
-        onClick={() => setIndex(tab.index)}
-      />
-    ))}
-      
+      <div className="items center flex gap-4">
+        {tabs.map((tab, i) => (
+          <TabButton
+            key={i}
+            src={tab.src}
+            text={tab.text}
+            isSelected={index === tab.index}
+            onClick={() => setIndex(tab.index)}
+          />
+        ))}
       </div>
-      {
-        Content.map((content, i)=> {
-          if(i == index){
-            return <Image key={i} src={content} width={588} height={432} alt={''}/>
-          }else{
-            return <Image key={i} className="hidden" src={content} width={588} height={432} alt={''}/>
-          }
-        })
-      }
+      {Content.map((content, i) => (
+          <Image
+            key={i}
+            className={i === index ? '' : 'hidden'}
+            src={content}
+            width={588}
+            height={432}
+            alt=''
+          />
+        ))}
     </div>
   );
 }
 
-function TabButton(props:{src:string, text:string, isSelected:boolean, onClick: () => void }) {
+function TabButton(props: {
+  src: string;
+  text: string;
+  isSelected: boolean;
+  onClick: () => void;
+}) {
   return (
-    <div className="inline-flex h-[120px] w-[120px] flex-col items-center justify-center gap-2 rounded-2xl bg-[#f3f3f3] p-4 shadow" onClick={props.onClick}>
-      <TabImage src={props.src} isSelected={props.isSelected}/>
-      <div className="text-center font-['Roboto'] text-sm ">
-      {props.text}
-      </div>
+    <div
+      className="inline-flex h-[120px] w-[120px] flex-col items-center justify-center gap-2 rounded-2xl bg-[#f3f3f3] p-4 shadow"
+      onClick={props.onClick}
+    >
+      <TabImage src={props.src} isSelected={props.isSelected} />
+      <div className="text-center font-['Roboto'] text-sm ">{props.text}</div>
     </div>
   );
 }
 
-function TabImage(props:{src:string, isSelected:boolean}){
-    return(
-        <Image className={props.isSelected?'':'grayscale'} src={props.src} alt={''} width={48} height={48}/>
-    );
+function TabImage(props: { src: string; isSelected: boolean }) {
+  return (
+    <Image
+      className={props.isSelected ? '' : 'grayscale'}
+      src={props.src}
+      alt={''}
+      width={48}
+      height={48}
+    />
+  );
 }
