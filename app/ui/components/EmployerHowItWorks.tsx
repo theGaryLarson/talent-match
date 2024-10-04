f4d897df289d1f9a471077ec6c299480dcc08e7f
not-damien <93546644+not-damien@users.noreply.github.com>
2024-10-04 11:00:55 -0700

    added bottom of emplyer landing page

--- app/ui/components/EmployerHowItWorks.tsx
+++ app/ui/components/EmployerHowItWorks.tsx
@@ -1,5 +1,5 @@
 import { ArrowRightIcon, ArrowUpRightIcon } from "@heroicons/react/24/outline";
-import { ArrowRight } from "@mui/icons-material";
+import Image from "next/image";
 import Link from "next/link";
 
 export default function EmployerHowItWorks(){
@@ -10,11 +10,15 @@ return(
 <Step buttonText={"Post a Job"} title={"Post a Job"} article={"Create a job post, outline the specific skills you need, and publish it with a few clicks. You'll easily manage and track all your postings from a centralized dashboard, keeping you organized and informed throughout the hiring process."} link={"/underconstruction"}/>
 <Step buttonText={"Search Local Candidates"} title={"Discover Local Talent"} article={"Connect with top tech talent right in your area. Simply define your desired search radius, and we'll present you with a curated list of local candidates. Through our advanced filters, you can search for the exact skills and experience you need, ensuring a perfect match for your needs."} link={"/services/employers/dashboard/listview"}/>
 <Step buttonText={"Start Hiring"} title={"Connect With EaseConnect With Ease"} article={"Once you've found the perfect candidate, our Portal makes connecting effortless. Directly initiate contact through our messaging system, streamlining communication and ensuring a smooth hiring process."} link={"/underconstruction"}/>
+<div className="space-y-6">
+
+<div className="text-center text-[#014260] text-[66px] font-normal font-['Roboto'] capitalize leading-[66px]">Discover diverse, qualified talent in your community</div>
+<Image src={"/images/employerBottomPic.png"} alt={"Discover diverse, qualified talent in your community"} width={3480} height={1791}/>
+</div>
 </div>
 </div>
 );
 }
-
 
 
 
