fac6a4af6114bcc9c04295e7619a7cdcfa26f0ff
theGaryLarson <gary.larson@seattlecolleges.edu>
2024-10-07 13:28:35 -0700

    Update skills endpoint in middleware
    Changed the skills endpoint to `/api/skills/search/` for both jobseekers and employers to ensure the correct API paths are used in the TextFieldAutoComplete component. This adjustment resolves access issues and aligns with the intended autocomplete functionality.

--- middleware.ts
+++ middleware.ts
@@ -29,7 +29,7 @@ export default auth((req) => {
     "/api/jobseekers/",
 
     "/api/edu-providers/", //fixme: had to add so jobseeker can see list of colleges in TextFieldAutoComplete component
-    "/api/skills/",  //fixme: had to add so jobseeker can see list of skills in TextFieldAutoComplete component
+    "/api/skills/search/",  //fixme: had to add so jobseeker can see list of skills in TextFieldAutoComplete component
     "/api/employers/technology-areas", //fixme: had to add so jobseeker can see list of tech-areas in TextFieldAutoComplete component
     "/services/jobseekers"  // fixme: had to add to view own profile. Need to validate that id is theirs to view the page.
   ];
@@ -51,7 +51,7 @@ export default auth((req) => {
 
     "/api/companies",  //fixme: had to add so employer can see list of existing companies in TextFieldAutoComplete component
     "/api/users/avatar/upload", //fixme: had to add so employer can upload an image
-    "/api/skills/", // fixme: had to add so employer can search based on skills
+    "/api/skills/search/", // fixme: had to add so employer can search based on skills
   ];
 
   const publicRoutes = [ // Routes for anyone, logged in or not
