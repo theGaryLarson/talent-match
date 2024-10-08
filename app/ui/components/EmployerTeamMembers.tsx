68d13332a0d4524e37af4cd61fd0faf47722f3d7
not-damien <93546644+not-damien@users.noreply.github.com>
2024-10-08 11:48:37 -0700

    prettier config needed file extintion change from .js to .cjs, ran prettier on some files

--- app/ui/components/EmployerTeamMembers.tsx
+++ app/ui/components/EmployerTeamMembers.tsx
@@ -1,27 +1,35 @@
-'use server'
-import { getEmployersByCompanyId } from "@/app/lib/prisma";
-import { auth } from "@/auth";
-import Avatar from "./Avatar";
+'use server';
+import { getEmployersByCompanyId } from '@/app/lib/prisma';
+import { auth } from '@/auth';
+import Avatar from './Avatar';
 
-export default async function EmployerTeamMembers(){
-    let session = await auth();
-    let teamates = await getEmployersByCompanyId(session?.user.companyId??'')
-    console.log("Team Mates: ",teamates)
-    return(
-<div>
-<div className="text-black/90 text-xl font-medium leading-relaxed">My team</div>
-<div className="flex">
-<div className="flex items-center">
-<Avatar imgsrc={"/images/plusIcon.png"} scale={.75}/>
-                <div className="text-sm font-semibold tracking-tight">Invite Team</div></div>
-    {teamates.map((t)=>{
-        return (
+export default async function EmployerTeamMembers() {
+  let session = await auth();
+  let teamates = await getEmployersByCompanyId(session?.user.companyId ?? '');
+  console.log('Team Mates: ', teamates);
+  return (
+    <div>
+      <div className="text-xl font-medium leading-relaxed text-black/90">
+        My team
+      </div>
+      <div className="flex">
+        <div className="flex items-center">
+          <Avatar imgsrc={'/images/plusIcon.png'} scale={0.75} />
+          <div className="text-sm font-semibold tracking-tight">
+            Invite Team
+          </div>
+        </div>
+        {teamates.map((t) => {
+          return (
             <div className="flex items-center" key={t.employer_id}>
-                <Avatar imgsrc={t.users.photo_url??undefined} scale={.75}/>
-                <div className="text-sm font-semibold tracking-tight">{t.users.first_name} {t.users.last_name}</div>
+              <Avatar imgsrc={t.users.photo_url ?? undefined} scale={0.75} />
+              <div className="text-sm font-semibold tracking-tight">
+                {t.users.first_name} {t.users.last_name}
+              </div>
             </div>
-        )
-    })}</div>
-</div>
-    );
+          );
+        })}
+      </div>
+    </div>
+  );
 }
