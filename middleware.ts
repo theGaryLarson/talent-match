283cb1fbe6facf8fc5bf17af15c9e31f5a720187
theGaryLarson <gary.larson@seattlecolleges.edu>
2024-10-07 21:48:41 -0700

    Add jobseeker avatar upload route
    Added route "/api/users/avatar/upload" to allow jobseekers to update their avatar image. This change ensures jobseekers can manage their profile pictures within the application.

--- middleware.ts
+++ middleware.ts
@@ -31,7 +31,8 @@ export default auth((req) => {
     "/api/edu-providers/", //fixme: had to add so jobseeker can see list of colleges in TextFieldAutoComplete component
     "/api/skills/search/",  //fixme: had to add so jobseeker can see list of skills in TextFieldAutoComplete component
     "/api/employers/technology-areas", //fixme: had to add so jobseeker can see list of tech-areas in TextFieldAutoComplete component
-    "/services/jobseekers"  // fixme: had to add to view own profile. Need to validate that id is theirs to view the page.
+    "/services/jobseekers",  // fixme: had to add to view own profile. Need to validate that id is theirs to view the page.
+    "/api/users/avatar/upload" // fixme: had to add so jobseeker can update their avatar image.
   ];
 
   const employerRoutes = [ // Routes for logged in users with EMPLOYER role
