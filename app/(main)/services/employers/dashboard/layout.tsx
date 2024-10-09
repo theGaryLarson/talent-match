abc66fc1b6838a7d0ba6922c358ac64719380e0e
not-damien <93546644+not-damien@users.noreply.github.com>
2024-10-09 11:45:22 -0700

    conditional rendering for sidebar on dashboard

--- app/(main)/services/employers/dashboard/layout.tsx
+++ app/(main)/services/employers/dashboard/layout.tsx
@@ -3,7 +3,11 @@ import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 import FolderOutlinedIcon from '@mui/icons-material/FolderOutlined';
 import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
-export default function layout({ children }: { children: React.ReactNode }) {
+import { auth } from '@/auth';
+export default async function layout({ children }: { children: React.ReactNode }) {
+  let session = await auth()
+  console.log(session)
+  if(session?.user.employerId){
   return (
     <div className="flex">
       <div className="flex w-[230px] flex-col text-[#047f9c]">
@@ -37,8 +41,17 @@ export default function layout({ children }: { children: React.ReactNode }) {
           <EmailOutlinedIcon />
           Inbox
         </Link>
+        <Link
+          href="/services/employers/dashboard/postjob"
+          className="inline-flex items-center justify-start gap-[5px]  p-2"
+        >
+          <EmailOutlinedIcon />
+          Post a job
+        </Link>
       </div>
       {children}
     </div>
-  );
+  );}else{
+    return <>{children}</>
+  }
 }
