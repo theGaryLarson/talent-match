2949852b708aec714717ab00aa3aeef7556befdb
theGaryLarson <gary.larson@seattlecolleges.edu>
2024-10-10 17:16:01 -0700

    Update cookie domain assignment based on environment
    Modified the cookie domain assignment to be conditional, applying the domain 'www.watechwfcoalition.org' only in production environments. This change ensures that the domain is not set for non-production deployments, improving flexibility in development and testing environments.

--- auth.ts
+++ auth.ts
@@ -147,7 +147,7 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
         sameSite: 'lax',
         path: '/',
         secure: true,
-        domain: 'www.watechwfcoalition.org',
+        ...(process.env.NODE_ENV === 'production' && { domain: 'www.watechwfcoalition.org' }),
       },
     },
   },
