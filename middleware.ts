43e55fc89ab0ee6be7b0cf7a3375c3945832757b
theGaryLarson <gary.larson@seattlecolleges.edu>
2024-10-14 20:22:40 -0700

    Fix typo in route path and update matcher configuration
    Corrected the typo in the postal geo-data search route for accuracy. Updated the matcher configuration to include API routes, ensuring proper routing for all necessary endpoints.

--- middleware.ts
+++ middleware.ts
@@ -57,7 +57,7 @@ export default auth((req) => {
     "/api/companies",  //fixme: had to add so employer can see list of existing companies in TextFieldAutoComplete component
     "/api/users/avatar/upload", //fixme: had to add so employer can upload an image
     "/api/skills/search/", // fixme: had to add so employer can search based on skills
-    "/api/post-geo-data/zip/search/", // fixme: had to add so employer can select work location.
+    "/api/postal-geo-data/zip/search/", // fixme: had to add so employer can select work location.
   ];
 
   const publicRoutes = [ // Routes for anyone, logged in or not
