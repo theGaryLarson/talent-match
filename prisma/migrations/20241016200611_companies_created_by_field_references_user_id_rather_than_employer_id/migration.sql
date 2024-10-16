4d02b643aa5fc3a77c76acfb15dc06e1adacf040
theGaryLarson <gary.larson@seattlecolleges.edu>
2024-10-16 13:48:26 -0700

    Refactor foreign key constraints and default value handling
    Modified the companies table to reference user IDs via createdBy field, rather than employerId. Having two references to the same table requires a lot of renaming and potential breaking changes across routes. Switching to userId was the quickest fix.

---
+++ prisma/migrations/20241016200611_companies_created_by_field_references_user_id_rather_than_employer_id/migration.sql
@@ -0,0 +1,23 @@
+BEGIN TRY
+
+BEGIN TRAN;
+
+-- AlterTable
+ALTER TABLE [dbo].[skill_subcategories] DROP CONSTRAINT [skill_subcategories_skill_subcategory_id_df];
+ALTER TABLE [dbo].[skill_subcategories] ADD CONSTRAINT [skill_subcategories_skill_subcategory_id_df] DEFAULT (newid()) FOR [skill_subcategory_id];
+
+-- AddForeignKey
+ALTER TABLE [dbo].[companies] ADD CONSTRAINT [companies_createdBy_fkey] FOREIGN KEY ([createdBy]) REFERENCES [dbo].[users]([id]) ON DELETE NO ACTION ON UPDATE CASCADE;
+
+COMMIT TRAN;
+
+END TRY
+BEGIN CATCH
+
+IF @@TRANCOUNT > 0
+BEGIN
+    ROLLBACK TRAN;
+END;
+THROW
+
+END CATCH
