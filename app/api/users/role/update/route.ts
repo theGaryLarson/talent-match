530cc56c10820516e957b59850befc343ad1985b
theGaryLarson <gary.larson@seattlecolleges.edu>
2024-10-21 18:14:25 -0700

    Refactor role update logic to handle multiple roles
    Enhanced the role update functionality by checking if the user exists and fetching the current roles. Ensured proper handling of role additions and removals, especially for the `Role.GUEST`. This change improves the robustness and flexibility of role management.

--- app/api/users/role/update/route.ts
+++ app/api/users/role/update/route.ts
@@ -19,15 +19,41 @@ export async function PATCH(request: Request) {
     const { role } = body;
 
     const result = await prisma.$transaction(async (prisma) => {
-      // Upsert user
-      const user = await prisma.user.update({
+      // Fetch the current user roles
+      const user = await prisma.user.findUnique({
         where: { id: userId },
+        select: { role: true }, // Only fetching the role field
+      });
+
+      if (!user) {
+        throw new Error('User not found');
+      }
+
+      // Split the current roles
+      let currentRoles: Role[] = user.role.split(',').map(r => r.trim().toUpperCase() as Role);
+      // Ensure the role is valid using mapToEnumOrThrow
+      const newRole = mapToEnumOrThrow(role, Role);
+      // Remove Role.GUEST if the new role is not Role.GUEST
+      if (newRole !== Role.GUEST) {
+        currentRoles = currentRoles.filter((r: Role) => r !== Role.GUEST);
+      }
+
+// Add the new role if it's not already in the roles array
+      if (!currentRoles.includes(newRole)) {
+        currentRoles.push(newRole);
+      }
+
+      // and rejoin them
+      const updatedRoles = currentRoles.join(',');
+
+      // Upsert user
+      return prisma.user.update({
+        where: {id: userId},
         data: {
-          role: mapToEnumOrThrow(role, Role),
+          role: updatedRoles,
         },
       });
 
-      return user;
     });
     devLog('ss-result', result);
     return NextResponse.json({ success: true, result }, { status: 200 });
