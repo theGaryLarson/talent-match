26ba7987ba7fde46c410c4bb782fd12ac15e3972
not-damien <93546644+not-damien@users.noreply.github.com>
2024-10-23 17:11:39 -0700

    improve joblisting page

--- app/(main)/services/joblistings/[id]/page.tsx
+++ app/(main)/services/joblistings/[id]/page.tsx
@@ -9,8 +9,11 @@ export default async function page({params}:{params: {id:string}}){
     console.log(jobListing)
     return(
         <main className="space-y-3 py-8 mx-4 tablet:mx-[150px] laptop:mx-[200px] font-['Roboto']">
-            <h1 className="text-3xl">{jobListing?.job_title} <Bookmark bookmarked={(jobListing?.jobseekersThatBookMarked.some((e)=>e.jobseeker_id == session?.user.jobseekerId))??false} addUrl={`/api/joblistings/bookmark/add/${jobListing?.job_posting_id}`} removeUrl={`/api/joblistings/bookmark/remove/${jobListing?.job_posting_id}`}/></h1>
-            <p>{jobListing?.job_description}</p>
+            <div className="">
+            <h1 className="text-4xl flex items-center ">{jobListing?.job_title} @ {jobListing?.companies.company_name} {session?.user.jobseekerId?<Bookmark bookmarked={(jobListing?.jobseekersThatBookMarked.some((e)=>e.jobseeker_id == session?.user.jobseekerId))??false} addUrl={`/api/joblistings/bookmark/add/${jobListing?.job_posting_id}`} removeUrl={`/api/joblistings/bookmark/remove/${jobListing?.job_posting_id}`}/>:''}</h1>
+            <p className="text-sm">Posted: {jobListing?.publish_date.toDateString()} Open Until: {jobListing?.unpublish_date.toDateString()}</p>
+            </div>
+            <p>Job Description: {jobListing?.job_description}</p>
             <h1 className="text-xl REPLACE-BEFORE-RELEASE">NOTE THIS IS UNSTYLED AND JUST HAS ALL OF THE INFO OF THE Job Listing in question</h1>
             {JSON.stringify(jobListing).split(',').map((e)=><p key={e}>{e}</p>)}
             {session?.user.jobseekerId?
