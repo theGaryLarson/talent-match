38f392d1bf569cea00ff6ee66d07843517bc9a86
not-damien <93546644+not-damien@users.noreply.github.com>
2024-10-30 15:26:12 -0700

    bookmarking now refeashes when you unbookmark

--- app/(main)/services/(dash)/employers/dashboard/(overview)/myjobposts/page.tsx
+++ app/(main)/services/(dash)/employers/dashboard/(overview)/myjobposts/page.tsx
@@ -4,7 +4,7 @@ import JobPostingCard from "@/app/ui/components/jobPostings/JobPostingCard";
 
 export default function Page(){
     return(
-<main className="w-full">
+<main className="w-full m-2 mb-0 w-full space-y-4 pt-8 phone:m-4 phone:p-6 sm-tablet:m-6 laptop:px-[200px]">
 <EmployerRecentJobPosts/>
 </main>
     );
