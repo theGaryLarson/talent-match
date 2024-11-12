6c2ab49657d2575f27a42963d1de5db1d2d42213
theGaryLarson <gary.larson@seattlecolleges.edu>
2024-11-12 01:34:24 -0800

    Add career prep student detail endpoint
    Implemented a new API endpoint to fetch detailed information for a specific jobseeker's career preparation status. It validates the presence of `jobseekerId` and fetches the relevant data using `getCareerPrepStudentDetailView`.

---
+++ app/api/admin/career-prep/get-student-detail/[jobseekerId]/route.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from "next/server";
+import {
+    CareerPrepStatus,
+    getCareerPrepStudentDetailView,
+    updateCareerPrepStatusCardView
+} from "@/app/lib/admin/careerPrep";
+
+export async function GET(request: Request, { params }: { params: { jobseekerId: string } }) {
+    const  jobseekerId = params.jobseekerId;
+    if (!jobseekerId) {
+        return NextResponse.json({ error: 'jobseekerId is required.' }, { status: 400 });
+    }
+
+    const data = await getCareerPrepStudentDetailView(jobseekerId);
+    return NextResponse.json(data);
+}
