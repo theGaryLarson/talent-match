7bf742c2e1a577b785ca53025e1b3c83ad9184c6
theGaryLarson <gary.larson@seattlecolleges.edu>
2024-11-12 01:34:47 -0800

    Refactor type handling and error message in career prep API
    Explicitly define the type for fetched data using `CareerPrepJobseekerCardViewDTO[]` and improve error message clarity. This helps in maintaining type safety and makes the error messages more descriptive.

--- app/api/admin/career-prep/get-students/route.ts
+++ app/api/admin/career-prep/get-students/route.ts
@@ -1,12 +1,12 @@
 import { NextResponse } from 'next/server';
-import {getCareerPrepStudentsCardView} from "@/app/lib/admin/careerPrep";
+import {CareerPrepJobseekerCardViewDTO, getCareerPrepStudentsCardView} from "@/app/lib/admin/careerPrep";
 
 export async function GET() {
-    const data = await getCareerPrepStudentsCardView();
+    const data: CareerPrepJobseekerCardViewDTO[] | null = await getCareerPrepStudentsCardView();
 
     if (data === null) {
         // Return a 500 error response if data is null due to an error
-        return NextResponse.json({ error: 'Failed to fetch data' }, { status: 500 });
+        return NextResponse.json({ error: 'Failed to Career Prep Jobseeker Card view data' }, { status: 500 });
     }
 
     return NextResponse.json(data);
