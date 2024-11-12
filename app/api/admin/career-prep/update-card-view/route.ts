a559fa16ca0daf2ddd34898b8db46689cc844b41
theGaryLarson <gary.larson@seattlecolleges.edu>
2024-11-11 19:35:04 -0800

    Add detailed career prep assessment ratings
    Extended `CareerPrepAssessment` with specific skill rating models in Cybersecurity, Data Analytics, IT Cloud, Software Development, Durable Skills, and Branding. Included `companyId` reference in `JobPlacement` model to link with `companies` table, enhancing relational integrity.

---
+++ app/api/admin/career-prep/update-card-view/route.ts
@@ -0,0 +1,15 @@
+import { NextResponse } from "next/server";
+import { bookmarkJobseeker } from "@/app/lib/prisma";
+import {CareerPrepStatus, updateCareerPrepStatusCardView} from "@/app/lib/admin/careerPrep";
+
+export async function PATCH(request: Request, {params}: {params: {jobseekerId: string, status?: CareerPrepStatus, expectedEndDate?: Date}}) {
+    const { jobseekerId, status, expectedEndDate } = params;
+    if (!jobseekerId) {
+        return NextResponse.json({ error: 'jobseekerId is required.' }, { status: 400 });
+    }
+    if (!status) {
+        return NextResponse.json({ error: 'status entry required.' }, { status: 400 });
+    }
+    const data = await updateCareerPrepStatusCardView(jobseekerId, status, expectedEndDate);
+    return NextResponse.json(data);
+}
