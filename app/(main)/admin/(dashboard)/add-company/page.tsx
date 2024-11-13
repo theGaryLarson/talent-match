777a68c72923d8530f3f0326042d737554d88d58
not-damien <93546644+not-damien@users.noreply.github.com>
2024-11-12 18:17:52 -0800

    basic company creation is complete

--- app/(main)/admin/(dashboard)/add-company/page.tsx
+++ app/(main)/admin/(dashboard)/add-company/page.tsx
@@ -1,20 +1,9 @@
 'use client';
-import { FormEvent, useEffect, useState } from 'react';
-import { useRouter } from 'next/navigation';
-import TagsWithAutocomplete from '@/app/ui/components/mui/TagsWithAutocomplete';
-import { SkillDTO } from '@/data/dtos/SkillDTO';
-import { companies, industry_sectors, technology_areas } from '@prisma/client';
-import { JobPostCreationDTO } from '@/data/dtos/JobListingDTO';
+import { FormEvent } from 'react';
+import { CompanyAdminCreationDTO } from '@/data/dtos/CompanyAdminCreationDTO';
 
 
 export default function Page() {
-  const router = useRouter();
-  const [skills, setSkills] = useState<SkillDTO[]>();
-  const [fetchLoadedTags, setFetchLoadedTags] = useState<SkillDTO[]>([]);
-  const [companies, setCompanies] = useState<companies[]>();
-  const [techAres, setTechAreas] = useState<technology_areas[]>();
-  const [industrySectors, setIndustrySectors] = useState<industry_sectors[]>();
- 
   async function onSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
     const formData = new FormData(event.currentTarget);
@@ -22,12 +11,16 @@ export default function Page() {
       'button[type="submit"]',
     ) as HTMLButtonElement;
     if (submitButton) submitButton.disabled = true;
-    const companyData = {
-      company_name: formData.get('company_name') as string,
-
+    const companyData:CompanyAdminCreationDTO = {
+        companyName: formData.get('company_name') as string,
+        aboutUs: formData.get('about_company') as string,
+        companyEmail: formData.get('company_email') as string,
+        yearFounded:  parseInt(formData.get('year_founded') as string, 10),
+        size: formData.get('size') as string,
+        isApproved: true
     };
     try {
-      const response = await fetch('/api/joblistings/add', {
+      const response = await fetch('/api/companies/create', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -49,26 +42,6 @@ export default function Page() {
       console.error('Error creating job listing:', error);
     }
   }
-  useEffect(()=>{
-    fetch('/api/companies/getall').then((res)=>{
-      return res.json();
-    }).then((jsonData)=>{
-      setCompanies(jsonData);
-    });
-
-    fetch('/api/joblistings/sectors').then((res)=>{
-      return res.json();
-    }).then((jsonData)=>{
-      setIndustrySectors(jsonData)
-    });
-    
-    fetch('/api/joblistings/techarea').then((res)=>{
-      return res.json();
-    }).then((jsonData)=>{
-      setTechAreas(jsonData);
-    });
-
-  },[])
   return (
     <form onSubmit={onSubmit} className='space-y-3'>
       
@@ -79,159 +52,34 @@ export default function Page() {
       </div>
 
 
-{/**workline */}
-
-
-
-
-
       {/* Job Description */}
       <div className="grid grid-cols-1">
-        <label htmlFor="job_description">Job Description</label>
-        <textarea name="job_description" required />
+        <label htmlFor="about_company">About Company</label>
+        <textarea name="about_company" required />
       </div>
 
 
-      {/*tech Sector*/}
+
+      {/*company email */}
       <div className="grid grid-cols-1">
-        <label htmlFor="sector">What Tech Sector does this job fall under?</label>
-        <select name='sector' id='sector' required>
-          <option value={''}>--Please Select a Sector--</option>
-          { 
-            industrySectors?.map((sector)=> <option value={sector.industry_sector_id}>{sector.sector_title}</option>)
-          } 
-        </select>
+        <label htmlFor="company_email">Company Email</label>
+        <textarea name="company_email" required />
       </div>
-      {/*Tech Area*/}
+
+      {/* year founded */}
       <div className="grid grid-cols-1">
-        <label htmlFor="area">What Tech Area Best Discribes This Job?</label>
-        <select name='area' id='area' required>
-          <option value={''}>--Please Select an Area--</option>
-          { 
-            techAres?.map((area)=> <option value={area.id}>{area.title}</option>)
-          } 
-        </select>
+        <label htmlFor="year_founded">Year Founded</label>
+        <input type="number" name="year_founded"/>
       </div>
 
-      {/* Internship */}
-      <div>
-        <label>Is this an internship?</label>
-        <div>
-          <label>
-            <input type="radio" name="is_internship" value="yes" required />
-            Yes
-          </label>
-          <label>
-            <input type="radio" name="is_internship" value="no" required />
-            No
-          </label>
-        </div>
-      </div>
-
-      {/* Paid */}
-      <div>
-        <label>Is this a paid position?</label>
-        <div>
-          <label>
-            <input type="radio" name="is_paid" value="yes" required />
-            Yes
-          </label>
-          <label>
-            <input type="radio" name="is_paid" value="no" required />
-            No
-          </label>
-        </div>
-      </div>
-
-      {/* Employment Type */}
-      <div>
-        <label htmlFor="employment_type">Employment Type</label>
-        <select name="employment_type" defaultValue="full-time">
-          <option value="full-time">Full-Time</option>
-          <option value="part-time">Part-Time</option>
-          <option value="contract">Contract</option>
-        </select>
-      </div>
-
-      {/* Location */}
-      <div>
-        <label>Location</label>
-        <div>
-          <label>
-            <input type="radio" name="location" value="remote" required />
-            Remote
-          </label>
-        </div>
-        <div>
-          <label>
-            <input type="radio" name="location" value="on-site" />
-            On-Site
-          </label>
-        </div>
-        <div>
-          <label>
-            <input type="radio" name="location" value="hybrid" />
-            Hybrid
-          </label>
-        </div>
-      </div>
-
-      {/* Salary Range */}
       <div className="grid grid-cols-1">
-        <label htmlFor="salary_range">Salary Range</label>
-        <input type="text" name="salary_range" required />
+        <label htmlFor="size">Number Of Employes</label>
+        <input type="number" name="size"/>
       </div>
 
-      {/* County */}
-
-      {/* ZIP Code */}
-      <div className="grid grid-cols-1">
-        <label htmlFor="zip" >ZIP Code</label>
-        <input type="text" name="zip" required />
-      </div>
-      {/* Unpublish Date */}
-      <div className="grid grid-cols-1">
-        <label htmlFor="unpublish_date">Unpublish Date</label>
-        <input type="date" name="unpublish_date" min={new Date().toISOString().split("T")[0]}/>
-      </div>
-
-      {/* Job Post URL */}
-      <div className="grid grid-cols-1">
-        <label htmlFor="job_post_url">Job Post URL</label>
-        <input type="text" name="job_post_url" />
-      </div>
-
-      {/* Assessment URL */}
-      <div >
-        <label htmlFor="assessment_url">Assessment URL</label>
-        <input type="text" name="assessment_url" />
-      </div>
-
-      {/* Skills */}
-      <div>
-        <label htmlFor="job-listing-skills">What skills are needed for this role?</label>
-      <TagsWithAutocomplete
-        apiSearchRoute="/api/skills/search/"
-        fieldLabel="Select the top 5 skills"
-        id="job-listing-skills"
-        maxTags={5}
-        searchingText="Searching..."
-        noResultsText="No skills found..."
-        onChange={function (ev, val) {
-          if (val.every((skill) => typeof skill !== 'string')) {
-            setSkills(val as SkillDTO[]);
-          }
-        }}
-        searchPlaceholder="Skill (ex: Java)"
-        addNewTags={fetchLoadedTags}
-        getTagLabel={(option: SkillDTO) => option.skill_name}
-        getTagLink={(option: SkillDTO) => option.skill_info_url}
-      />
-      <p>Select your top 5 skills from your skills list</p>
-</div>
       {/* Submit Button */}
       <div>
-        <button type="submit">Create Job Listing</button>
+        <button type="submit">Create Company</button>
       </div>
     </form>
   );
