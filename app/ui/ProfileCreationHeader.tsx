03024b625a64578631a851289590c7264490f082
Quincy Pham <quincytpham@gmail.com>
2024-11-14 17:34:07 -0500

    updated skip button for profile creation/edit pages, new component SkipButton made with logic applicable to both employer and jobseeker. Was previously static button in layout.tsx profilecreationheader.tsx.

--- app/ui/ProfileCreationHeader.tsx
+++ app/ui/ProfileCreationHeader.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image';
 import { useSession } from 'next-auth/react';
 import { Role } from '@/data/dtos/UserInfoDTO';
 import { SessionProvider } from 'next-auth/react';
+import SkipButton from '@/app/ui/components/SkipButton';
 
 export default function ProfileCreationHeader() {
   const { data: session } = useSession();
@@ -12,33 +13,27 @@ export default function ProfileCreationHeader() {
     : '/services/jobseekers/dashboard';
 
   return (
-      <header className="bg-white">
-        <nav
-          className="mx-auto flex items-center justify-between p-6 laptop:px-8"
-          aria-label="Global"
-        >
-          <div className="flex laptop:flex-1">
-            <Link href="/">
-              <span className="sr-only">Tech Workforce Coalition</span>
-              <Image
-                src="/images/TWC_75x50_2024.svg"
-                alt="Tech Workforce Coalition"
-                width={75}
-                height={31.8}
-              />
-            </Link>
-          </div>
+    <header className="bg-white">
+      <nav
+        className="mx-auto flex items-center justify-between p-6 laptop:px-8"
+        aria-label="Global"
+      >
+        <div className="flex laptop:flex-1">
+          <Link href="/">
+            <span className="sr-only">Tech Workforce Coalition</span>
+            <Image
+              src="/images/TWC_75x50_2024.svg"
+              alt="Tech Workforce Coalition"
+              width={75}
+              height={31.8}
+            />
+          </Link>
+        </div>
 
-          <div className="flex flex-1 justify-end">
-            <Link
-              className="mt-1 inline-block h-fit rounded-full bg-cyan-700 px-4 py-2 text-white hover:bg-red-800"
-              href={dashboardLink}
-              target="_self"
-            >
-              Skip
-            </Link>
-          </div>
-        </nav>
-      </header>
+        <div className="flex flex-1 justify-end">
+          <SkipButton />
+        </div>
+      </nav>
+    </header>
   );
 }
---
+++ app/ui/components/SkipButton.tsx
@@ -0,0 +1,48 @@
+'use client';
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+function SkipButton() {
+  const currentPath = usePathname(); //returns full path ie: /edit-profile/jobseeker/introduction
+  let pageSequence: string[] = [];
+  let userPrefix: string = '';
+  if (currentPath.includes('/edit-profile/jobseeker')) {
+    pageSequence = [
+      'introduction',
+      'education',
+      'work-experience',
+      'showcase',
+      'preferences',
+      'disclosures',
+    ];
+    userPrefix = 'jobseeker';
+  } else if (currentPath.includes('/edit-profile/employer')) {
+    pageSequence = [
+      'personal',
+      'company',
+      'about',
+      'mission',
+      'video',
+      'disclosures',
+    ];
+    userPrefix = 'employer';
+  }
+  const lastSegment = currentPath.split('/').pop() || ''; //returns last segment ie: introduction
+  const currentIndex = pageSequence.indexOf(lastSegment);
+
+  const nextPage =
+    currentIndex >= 0 && currentIndex < pageSequence.length - 1
+      ? `/edit-profile/${userPrefix}/${pageSequence[currentIndex + 1]}`
+      : null;
+  return nextPage ? (
+    <Link
+      href={nextPage}
+      className="mt-1 inline-block h-fit rounded-full bg-cyan-700 px-4 py-2 text-white hover:bg-red-800"
+    >
+      Skip
+    </Link>
+  ) : null;
+}
+
+export default SkipButton;
