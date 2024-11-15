42675cc0dc6f35a40ce9117f2fb3eeb65d5abd1f
Quincy Pham <quincytpham@gmail.com>
2024-11-15 14:32:12 -0500

    updated test for ProfileCreationHeader as skip button no longer needed on all pages

--- tests/ui/ProfileCreationHeader.test.tsx
+++ tests/ui/ProfileCreationHeader.test.tsx
@@ -6,49 +6,54 @@ import { Role } from '@/data/dtos/UserInfoDTO';
 import { Session } from 'next-auth';
 
 vi.mock('next-auth/react', () => ({
-    useSession: vi.fn(),
-    SessionProvider: ({ children }: { children: React.ReactNode }) => <div>{children}</div>,
+  useSession: vi.fn(),
+  SessionProvider: ({ children }: { children: React.ReactNode }) => (
+    <div>{children}</div>
+  ),
 }));
 
 const renderWithProviders = (ui: React.ReactElement, role: Role) => {
-    vi.mocked(useSession).mockImplementation(() => ({
-        update: async (): Promise<Session | null> => null, // Update the return type of the update function
-        data: {
-            expires: "1",
-            user: {
-                email: "bagel@bagel.com", name: "Bagel", image: "/images/loveBagels.jpg", roles: [role],
-                firstName: "bagel",
-                lastName: "test",
-                createdAt: new Date(),
-                updatedAt: new Date(),
-                companyIsApproved: false,
-                employeeIsApproved: false
-            },
-        },
-        status: "authenticated",
-    }));
-    return render(<SessionProvider>{ui}</SessionProvider>);
+  vi.mocked(useSession).mockImplementation(() => ({
+    update: async (): Promise<Session | null> => null, // Update the return type of the update function
+    data: {
+      expires: '1',
+      user: {
+        email: 'bagel@bagel.com',
+        name: 'Bagel',
+        image: '/images/loveBagels.jpg',
+        roles: [role],
+        firstName: 'bagel',
+        lastName: 'test',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        companyIsApproved: false,
+        employeeIsApproved: false,
+      },
+    },
+    status: 'authenticated',
+  }));
+  return render(<SessionProvider>{ui}</SessionProvider>);
 };
 
 describe('ProfileCreationHeader', () => {
-    afterEach(() => {
-        cleanup();
-    });
+  afterEach(() => {
+    cleanup();
+  });
 
-    it('renders correctly', () => {
-        renderWithProviders(<ProfileCreationHeader />, Role.JOBSEEKER);
-        expect(screen.getByRole('banner')).toBeDefined();
-        expect(screen.getByAltText('Tech Workforce Coalition')).toBeDefined();
-        expect(screen.getByText('Skip')).toBeDefined();
-    });
+  it('renders correctly', () => {
+    renderWithProviders(<ProfileCreationHeader />, Role.JOBSEEKER);
+    expect(screen.getByRole('banner')).toBeDefined();
+    expect(screen.getByAltText('Tech Workforce Coalition')).toBeDefined();
+    expect(screen.getByText('Skip')).toBeDefined();
+  });
 
-    it('links to the correct dashboard for employers', () => {
-        renderWithProviders(<ProfileCreationHeader />, Role.EMPLOYER);
-        expect(screen.getByText('Skip').closest('a')).toHaveProperty('pathname', '/services/employers/dashboard');
-    });
+  // it('links to the correct dashboard for employers', () => {
+  //     renderWithProviders(<ProfileCreationHeader />, Role.EMPLOYER);
+  //     expect(screen.getByText('Skip').closest('a')).toHaveProperty('pathname', '/services/employers/dashboard');
+  // });
 
-    it('links to the correct dashboard for jobseekers', () => {
-        renderWithProviders(<ProfileCreationHeader />, Role.JOBSEEKER);
-        expect(screen.getByText('Skip').closest('a')).toHaveProperty('pathname', '/services/jobseekers/dashboard');
-    });
+  // it('links to the correct dashboard for jobseekers', () => {
+  //     renderWithProviders(<ProfileCreationHeader />, Role.JOBSEEKER);
+  //     expect(screen.getByText('Skip').closest('a')).toHaveProperty('pathname', '/services/jobseekers/dashboard');
+  // });
 });
