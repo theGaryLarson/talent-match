b28deef44384de046797b934d5125cda86048bda
theGaryLarson <gary.larson@seattlecolleges.edu>
2024-11-16 19:43:49 -0800

    Fix: update middleware matcher to include api/auth route
    accidentally merged an edit so I could postman without OAuth

--- middleware.ts
+++ middleware.ts
@@ -209,6 +209,6 @@ export default auth((req) => {
  */
 export const config = {
   matcher: [
-    '/((?!api|_next/static|_next/image|images|favicon.ico|ess).*)',
+    '/((?!api/auth|_next/static|_next/image|images|favicon.ico|ess).*)',
   ],
 };
