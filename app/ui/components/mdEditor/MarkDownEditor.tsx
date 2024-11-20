6577db0b27a0ac263854432e0b1da0b1ab30d745
not-damien <93546644+not-damien@users.noreply.github.com>
2024-11-20 11:41:04 -0800

    explict sying of md reader

--- app/ui/components/mdEditor/MarkDownEditor.tsx
+++ app/ui/components/mdEditor/MarkDownEditor.tsx
@@ -10,7 +10,39 @@ import 'quill/dist/quill.snow.css'; // Add css for snow theme
 import { CreateNoteDTO, NoteType } from '@/app/lib/admin/careerPrep';
 // or import 'quill/dist/quill.bubble.css'; // Add css for bubble theme
 export default function MarkDownEditor (props:{title:string, noteType:NoteType, jobseekerId:string, noteid?:string}){
-    const { quill, quillRef } = useQuill();
+  const theme = 'snow';
+  // const theme = 'bubble';
+
+  const modules = {
+    toolbar: [
+      ['bold', 'italic', 'underline', 'strike'],
+      [{ align: [] }],
+  
+      [{ list: 'ordered'}, { list: 'bullet' }],
+      [{ indent: '-1'}, { indent: '+1' }],
+  
+      [{ size: ['small', false, 'large', 'huge'] }],
+      [{ header: [1, 2, 3, 4, 5, 6, false] }],
+      ['link', 'image', 'video'],
+      [{ color: [] }, { background: [] }],
+  
+      ['clean'],
+    ],
+    clipboard: {
+      matchVisual: false,
+    },
+  };
+  const placeholder = 'Compose an epic...';
+
+  const formats = [
+    'bold', 'italic', 'underline', 'strike',
+    'align', 'list', 'indent',
+    'size', 'header',
+    'link', 'image', 'video',
+    'color', 'background',
+    'clean',
+  ];
+    const { quill, quillRef } = useQuill({ theme, modules, formats, placeholder });;
     const router = useRouter();
     React.useEffect(() => {
       if (quill) {
