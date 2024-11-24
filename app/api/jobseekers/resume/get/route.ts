ce577d1d09545bbb46d59ac558543c6ac3e388bb
theGaryLarson <gary.larson@seattlecolleges.edu>
2024-11-24 01:25:39 -0800

    Update resume retrieval with auth and enhanced response
    Integrated user authentication to securely fetch the resume and revised the response structure to include a success status. This ensures both secure access and a clearer indication of the retrieval outcome for clients.

--- app/api/jobseekers/resume/get/route.ts
+++ app/api/jobseekers/resume/get/route.ts
@@ -2,22 +2,26 @@ import { NextResponse } from 'next/server';
 import getPrismaClient from '@/app/lib/prismaClient.mjs';
 import { PrismaClient } from '@prisma/client';
 import { getResumeUrl } from '@/app/lib/services/azureBlobService';
+import { auth } from "@/auth";
 
 const prisma: PrismaClient = getPrismaClient();
 
-export async function GET(
-  request: Request,
-  { params }: { params: { userId: string } },
-) {
+export async function GET() {
   try {
-    const userId = params.userId;
+    const session = await auth();
+    const userId = session?.user.id;
+
+    if (!userId) {
+      return NextResponse.json({ success: false, url: null, error: 'No user ID in session storage.' }, { status: 400 });
+    }
+
     const url = await getResumeUrl(userId);
 
     if (!url) {
-      return NextResponse.json({ error: 'Resume not found' }, { status: 404 });
+      return NextResponse.json({ success: false, url: null, error: 'No saved resume.' }, { status: 404 });
     }
 
-    return NextResponse.json(url);
+    return NextResponse.json({ success: true, url: url}, { status: 200 });
   } catch (e: any) {
     console.error('Error retrieving resume from Blob storage:', e.message);
     return NextResponse.json(
