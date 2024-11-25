7cd831a0cac38705221b9b3b71e6247f2c004dfe
not-damien <93546644+not-damien@users.noreply.github.com>
2024-11-25 13:19:29 -0800

    bolded fields

--- app/(main)/career-prep/(dashboard)/[id]/page.tsx
+++ app/(main)/career-prep/(dashboard)/[id]/page.tsx
@@ -1,9 +1,6 @@
-import { CategorizedNotes, getCareerPrepStudentDetailView, getCareerPrepStudentNotes, NoteDTO, NoteType } from "@/app/lib/admin/careerPrep";
-import NoteCard from "@/app/ui/components/careerPrep/NoteCard";
-import MarkDownEditor from "@/app/ui/components/mdEditor/MarkDownEditor";
+import {getCareerPrepStudentDetailView, getCareerPrepStudentNotes, NoteType } from "@/app/lib/admin/careerPrep";
 import NoteContainer from "@/app/ui/components/careerPrep/NoteContainer";
 import Tabs from "@/app/ui/components/Tabs";
-import { ReactNode } from "react";
 
 export default async function page({ params }: { params: { id: string } }){
     const client = await getCareerPrepStudentDetailView(params.id);
@@ -11,17 +8,14 @@ export default async function page({ params }: { params: { id: string } }){
     return(
         <main className="space-y-3 pr-[100px] w-full">
             <h1 className="text-2xl">{client.data?.firstName} {client.data?.lastName} ({client.data?.pronouns})</h1>
-            <h2>Status: {client.data?.prepEnrollmentStatus}</h2>
-            <h2>Assessment Date:{client.data?.assessmentDate}</h2>
-            <h2>Highest Level of Education: {client.data?.education}</h2>
-            <h2>Email address: {client.data?.emailAddress}</h2>
-            <h2>Pool: {client.data?.poolAssignment}</h2>
-            <h2>Linkedin:{client.data?.linkedin}</h2>
-            <h2>Pathway: {client.data?.pathway}</h2>
-            <h2>Technical Certificates: {client.data?.technicalCertificates.map((c)=>`${c.name}, `)}</h2>
-            {
-            //TODO fix bug where if you have a note selected to update and swap tabs it will add it to the new tab
-            }
+            <h2><b>Status:</b> {client.data?.prepEnrollmentStatus}</h2>
+            <h2><b>Assessment Date:</b>{client.data?.assessmentDate}</h2>
+            <h2><b>Highest Level of Education:</b> {client.data?.education}</h2>
+            <h2><b>Email address:</b> {client.data?.emailAddress}</h2>
+            <h2><b>Pool:</b> {client.data?.poolAssignment}</h2>
+            <h2><b>Linkedin:</b><a href={client.data?.linkedin}></a>{client.data?.linkedin}</h2>
+            <h2><b>Pathway:</b> {client.data?.pathway}</h2>
+            <h2><b>Technical Certificates:</b> {client.data?.technicalCertificates.map((c)=>`${c.name}, `)}</h2>
             <div className="">
                 <Tabs tabs={
                 [
