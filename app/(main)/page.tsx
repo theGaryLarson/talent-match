c7dfd911a65e4b80dcea4b9e5974418e221c700c
Rory.Hayes <rorosaurus@gmail.com>
2024-11-25 14:20:42 -0800

    what-we-do mobile complete

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -26,7 +26,7 @@ export default function Page() {
                 </div>
                 {/* Content */}
                 <div className="w-full relative justify-center items-center z-1 bg-[#D6F1F7]">
-                    <div className="mt-12 px-4 laptop:px-24 flex-col justify-start items-center gap-5 inline-flex">
+                    <div className="mt-8 px-4 laptop:px-24 flex-col justify-start items-center gap-5 inline-flex">
                         <div className="p-4 flex-col justify-start items-center flex">
                             {/* About */}
                             <div className="justify-center items-center gap-12 mt-10 mb-20 flex-col laptop:flex-row laptop:inline-flex">
@@ -74,8 +74,9 @@ export default function Page() {
                             <div className="mb-4 self-stretch text-center text-neutral-700 text-5xl font-normal font-['Roboto'] capitalize leading-10"> In Tech roles by 2025</div>
                         </div>
                         {/* What we do */}
-                        <div className="mt-10 self-stretch justify-center items-center gap-12 inline-flex">
+                        <div className="mt-10 mx-4 self-stretch justify-center items-center gap-12 inline-flex">
                             <div className="w-2/3 flex-col justify-center items-start gap-2.5 inline-flex">
+                                <img className="laptop:hidden mb-4 w-full aspect-square rounded-3xl shadow object-cover" src="/images/landing/what-we-do.jpg" />
                                 <div className="self-stretch text-sky-900 text-xl font-semibold font-['Roboto'] capitalize leading-snug tracking-widest">WHAT WE DO</div>
                                 <div className="leading-none self-stretch text-cyan-700 text-6xl font-normal font-['Roboto'] capitalize leading-10">Build A more inclusive tech future</div>
                                 <div className="self-stretch text-neutral-700 text-xl font-normal font-['Roboto'] leading-loose">Our mission is to connect underrepresented candidates with in-demand tech jobs. We offer targeted training programs in IT and cybersecurity, along with personalized career coaching and networking opportunities. By bridging the gap between talent and opportunity, we are working to build a more diverse and innovative tech industry.</div>
@@ -83,7 +84,7 @@ export default function Page() {
                                     <div className="w-px h-7 relative" />
                                 </div>
                             </div>
-                            <img className="w-96 h-96 rounded-3xl shadow object-cover" src="/images/landing/what-we-do.jpg" />
+                            <img className="hidden laptop:block w-96 h-96 rounded-3xl shadow object-cover" src="/images/landing/what-we-do.jpg" />
                         </div>
                     </div>
                 </div>
