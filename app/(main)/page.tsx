7cd94c7c4927b9ddbc5d5082d40c3efe4b41dd99
Rory.Hayes <rorosaurus@gmail.com>
2024-11-25 18:24:36 -0800

    final tweaks to mobile landing page :)

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -52,8 +52,8 @@ export default function Page() {
                                 <div className="w-full laptop:grid laptop:grid-cols-2 justify-center items-start gap-2.5">
                                     <div className="">
                                         <div className="mb-8 self-stretch text-white/70 text-xl font-semibold font-['Roboto'] capitalize leading-snug tracking-widest">LAUNCHING SOON...</div>
-                                        <div className="leading-none mb-12 self-stretch text-neutral-100 text-6xl font-normal font-['Roboto'] capitalize leading-10">Talent Portal</div>
-                                        <div className="mb-8 self-stretch text-sky-200 text-5xl font-normal font-['Roboto'] capitalize leading-10">Hire Qualified Candidates</div>
+                                        <div className="text-4xl sm-tablet:text-6xl leading-none mb-4 sm-tablet:mb-12 self-stretch text-neutral-100 font-normal font-['Roboto'] capitalize leading-10">Talent Portal</div>
+                                        <div className="text-2xl sm-tablet:text-5xl mb-8 self-stretch text-sky-200 font-normal font-['Roboto'] capitalize leading-10">Hire Qualified Candidates</div>
                                         <div className="self-stretch text-neutral-100 text-xl font-normal font-['Roboto'] leading-loose">Members of the Coalition have access to our soon to be launched Talent Finder Portal. Our searchable database will quickly connect you with local talent that meets your specific needs.</div>
                                         <div className="flex-col justify-start items-start flex">
                                             <div className="w-px h-2.5 relative" />
@@ -68,17 +68,17 @@ export default function Page() {
                             </div>
                         </div>
                         {/* Our goal */}
-                        <div className="mt-10 h-72 flex-col justify-center items-center gap-2.5 flex">
+                        <div className="mt-4 laptop:mt-10 h-72 flex-col justify-center items-center gap-2.5 flex">
                             <div className="sm-tablet:mb-8 self-stretch text-center text-neutral-700 text-base sm-tablet:text-xl font-semibold font-['Roboto'] capitalize leading-snug tracking-widest">OUR GOAL</div>
                             <div className="sm-tablet:mb-8 leading-none self-stretch text-center text-cyan-700 text-4xl sm-tablet:text-6xl tablet:text-8xl font-normal font-['Roboto']">1,000 Job Candidates</div>
                             <div className="sm-tablet:mb-4 self-stretch text-center text-neutral-700 text-2xl sm-tablet:text-5xl font-normal font-['Roboto'] capitalize leading-10"> In Tech roles by 2025</div>
                         </div>
                         {/* What we do */}
-                        <div className="mt-10 mx-4 self-stretch justify-center items-center gap-12 inline-flex">
+                        <div className="mt-4 laptop:mt-10 mx-4 self-stretch justify-center items-center gap-12 inline-flex">
                             <div className="w-full laptop:w-2/3 flex-col justify-center items-start gap-2.5 inline-flex">
                                 <img className="laptop:hidden mb-4 w-full aspect-square rounded-3xl shadow object-cover" src="/images/landing/what-we-do.jpg" />
                                 <div className="self-stretch text-sky-900 text-xl font-semibold font-['Roboto'] capitalize leading-snug tracking-widest">WHAT WE DO</div>
-                                <div className="leading-none self-stretch text-cyan-700 text-6xl font-normal font-['Roboto'] capitalize leading-10">Build A more inclusive tech future</div>
+                                <div className="text-4xl sm-tablet:text-6xl leading-none self-stretch text-cyan-700 font-normal font-['Roboto'] capitalize leading-10">Build A more inclusive tech future</div>
                                 <div className="self-stretch text-neutral-700 text-xl font-normal font-['Roboto'] leading-loose">Our mission is to connect underrepresented candidates with in-demand tech jobs. We offer targeted training programs in IT and cybersecurity, along with personalized career coaching and networking opportunities. By bridging the gap between talent and opportunity, we are working to build a more diverse and innovative tech industry.</div>
                                 <div className="flex-col justify-start items-start flex">
                                     <div className="w-px h-7 relative" />
@@ -92,7 +92,7 @@ export default function Page() {
                 <div className="w-full mt-0 h-12 bg-[#D6F1F7] rounded-bl-3xl rounded-br-3xl" />
             </div>
 
-            <div className="w-full mt-40 px-8 laptop:px-24 flex-col justify-start items-center gap-3.5 inline-flex">
+            <div className="w-full mt-20 laptop:mt-40 px-8 laptop:px-24 flex-col justify-start items-center gap-3.5 inline-flex">
                 {/* For employers */}
                 <div className="self-stretch justify-center items-start gap-12 inline-flex">
                     <div className="hidden laptop:block justify-center items-center gap-24 flex">
