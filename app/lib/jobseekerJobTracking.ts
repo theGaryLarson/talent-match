50977f8298a0614c3ffda79ce027f73fd9552760
theGaryLarson <gary.larson@seattlecolleges.edu>
2024-11-26 01:19:30 -0800

    Add JobStatus enum for job tracking
    Introduce a new JobStatus enum to categorize the stages of job applications. This enum enhances code readability and standardizes the status values across the application.

---
+++ app/lib/jobseekerJobTracking.ts
@@ -0,0 +1,13 @@
+
+export enum JobStatus {
+    Bookmarked = "Bookmarked",
+    Applying = "Applying",
+    Applied = "Applied",
+    Interviewing = "Interviewing",
+    Negotiating = "Negotiating",
+    Accepted = "Accepted",
+    IWithdrew = "I Withdrew",
+    NotSelected = "Not Selected",
+    NoResponse = "No Response",
+    Archived = "Archived"
+}
