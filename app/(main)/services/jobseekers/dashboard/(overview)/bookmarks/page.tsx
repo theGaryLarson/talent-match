0b4c7db91b4bcf0095b1f36f2f47ab389fcf523e
not-damien <93546644+not-damien@users.noreply.github.com>
2024-11-26 11:58:13 -0800

    fixing merge issues

--- app/(main)/services/jobseekers/dashboard/(overview)/bookmarks/page.tsx
+++ app/(main)/services/jobseekers/dashboard/(overview)/bookmarks/page.tsx
@@ -3,14 +3,17 @@ import Link from "next/link";
 
 export default async function page(){
     const myBookMarkedJobs = await getJobSeekerBookmarkedJobs();
+    if(myBookMarkedJobs === undefined || myBookMarkedJobs.length == 0){
+      return (
+        <div><p>No Saved Job Posts Found: <Link href={'/services/jobseekers/dashboard/jobsearch'} className='LINK'>Find Job Listings here</Link></p></div>
+      );
+    }
     return(
 <main>
 <h1 className="font-bold text-xl">BookMarked Jobs</h1>
-    
-    
     <div>
       {
-        myBookMarkedJobs?.BookmarkedJobs.map((job)=><div key={job.job_posting_id}><Link href={`/services/joblistings/${job.job_posting_id}`}>{job.job_title}</Link></div>)
+        myBookMarkedJobs.map((job)=><div key={job.job_posting.job_posting_id}><Link href={`/services/joblistings/${job.job_posting.job_posting_id}`}>{job.job_posting.job_title}</Link></div>)
       }
     </div>
 </main>
