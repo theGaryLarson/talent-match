abb9d4738dad000a769b6bf08d5e11b25fbe7fae
theGaryLarson <gary.larson@seattlecolleges.edu>
2024-11-27 13:50:24 -0800

    Refactor upsert operation in employer profile creation
    Updated the upsert operations for users and employers to include necessary fields like UUIDs and role assignments. This refactor ensures that new employer profiles are created with additional identifiers and connected data, improving data integrity and consistency.

--- app/api/employers/account/profile/upsert/route.ts
+++ app/api/employers/account/profile/upsert/route.ts
@@ -1,8 +1,10 @@
 import {NextResponse} from "next/server";
 import getPrismaClient from "@/app/lib/prismaClient.mjs";
 import {PrismaClient} from "@prisma/client";
-import { ReadEmployerProfileDTO } from "@/data/dtos/EmployerProfileCreationDTOs";
 import { auth } from '@/auth';
+import { v4 as uuidv4 } from 'uuid';
+import {Role} from "@/data/dtos/UserInfoDTO";
+
 const prisma: PrismaClient = getPrismaClient();
 
 export async function GET(request: Request) {
@@ -18,51 +20,70 @@ export async function GET(request: Request) {
             return NextResponse.json({success: false, error: `A uuidv4 companyId is required.`}, {status: 400})
         }
 
-        const createdEmployerProfile = await prisma.$transaction( async (prisma) => {
+        prisma.$transaction( async (prisma) => {
 
-            const updatedUser = await prisma.user.upsert({
+            prisma.user.upsert({
                 where: {
                     id: session?.user?.id!
-                },update: {
-
+                },
+                update: {
+                    first_name: firstName,
+                    last_name: lastName,
                 },
                 create: {
+                    id: uuidv4(),
+                    role: Role.EMPLOYER,
                     first_name: firstName,
                     last_name: lastName,
                     email: session?.user.email!,
                     has_agreed_terms: false,
+                    createdAt: new Date(Date.now()),
                 }
             });
 
-            const updatedEmployer = await prisma.employers.upsert({
+            prisma.employers.upsert({
                 where: {
                     user_id: session?.user?.id!
-                },update: {
-
+                },
+                update: {
+                    job_title: currentJobTitle,
+                    linkedin_url: linkedInUrl,
+                    company_addresses: {
+                        connect: {
+                            company_address_id: workAddressId,
+                        }
+                    }
                 },
                 create: {
-
+                    employer_id: session?.user.employerId!,
+                    job_title: currentJobTitle,
+                    linkedin_url: linkedInUrl,
+                    hasAgreedTerms: false,
+                    is_verified_employee: false,
+                    users: {
+                        connect: {
+                            id: session?.user.id!,
+                        }
+                    },
+                    company_addresses: {
+                        connect: {
+                            company_address_id: workAddressId,
+                        }
+                    },
+                    companies: {
+                        connect: {
+                            company_id: companyId,
+                        }
+                    }
                 }
             });
+        }); // end transaction
 
-            const
-        })
-
-
-        const result: ReadEmployerProfileDTO[] = companyTestimonial.map(t => ({
-            testimonyId: t.testimonial_id,
-            companyId: t.company_id,
-            employerId: t.employer_id,
-            text: t.text,
-            author: t.author,
-        }));
-
-
-        return NextResponse.json({success: true, result}, {status: 200})
+        return NextResponse.json({ success: true }, {status: 200})
 
     } catch (e: any) {
-        console.error('Error creating company testimonial:', e.message);
-        return NextResponse.json({error: `Failed to create company testimonial.\n${e.message}`}, {status: 500});
+        console.error('Error creating employer profile:', e.message);
+        return NextResponse.json({error: `Failed to create employer profile.\n${e.message}`}, {status: 500});
     } finally {
         await prisma.$disconnect();
     }
