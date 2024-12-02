f1d80a61d5cbb4eebb5a8f241cc216a6354f8ea6
theGaryLarson <gary.larson@seattlecolleges.edu>
2024-12-02 15:27:52 -0800

    Add fields to EduProvider DTOs for comprehensive data
    Enhanced the DTOs for education providers by adding multiple fields, such as eduLevel, provider details, mission, and cost data. This addition will support broader attributes and improve the handling and display of education provider data across the application. These updates are crucial for upcoming features that rely on detailed provider information.

--- app/lib/eduProviders.ts
+++ app/lib/eduProviders.ts
@@ -1,36 +1,53 @@
 // TODO: CREATE FORM FOR TRAINING PROVIDER DATA
 
-export type ReadEduProviderDTO = {
+import {EducationLevel} from "@/data/dtos/JobSeekerProfileCreationDTOs";
 
+export type ReadEduProviderDTO = {
+    eduProviderId: string, //edu_providers.id
+    eduLevel: EducationLevel, // edu_providers.edu_type
+    providerName: string, // edu_providers.name
+    contactName: string, // edu_providers.contact
+    contactEmail: string, // edu_providers.contact_email
+    url: string, // edu_providers.edu_url
+    mission: string, // edu_providers.mission
+    providerDescription: string, // edu_providers.providerDescription
+    setsApartStatement: string, // edu_providers.setsApartStatement
+    screeningCriteria: string, // edu_providers.screeningCriteria
+    recruitingSources: string, // edu_providers.recruitingSources
+    programCount: string, // edu_providers.programCount
+    cost: string, // edu_providers.cost
+    // isAdminReviewed: boolean, // edu_providers.isAdminReviewed
+    // isCoalitionMember: boolean, // edu_providers.isCoalitionMember
+    // createdBy: string, // edu_providers.userId
 }
 
 
 export type ReadEduProviderProgramDetailDTO = {
     programId: string, // provider_programs.training_program_id
-    // logoUrl: string // will use blobStorage method to retrieve image url. // TODO: create post, get method for this.
-    programName: string,
-    eduProviderName: string,
-    locations: string[], // TODO: add this to program provider table, save as TEXT field in db but separate into list on DTO delimiter (~)
-    about: string, //TODO: use Quill to implement this
+    // logoUrl: string // will use blobStorage ss method to retrieve image url. getEduProviderLogoUrl(eduProviderId)
+    programName: string, // provider_programs join programs on program_id
+    eduProviderName: string, // provider_programs join edu_providers on edu_provider_id
+    locations: string[], // TODO: add provider_programs.locations, save as TEXT field in db but separate into list on DTO delimiter (~)
+    about: string, // TODO: add provider_programs.about use Quill to implement this
     quickInfo: {
-        tuition?: string,
-        fees?: string,
-        additionalCostInfo?: string,
-        locationType: LocationType,
-        getStartedUrl: string,
+        tuition?: string, // TODO: add provider_programs.tuition
+        fees?: string, // TODO: add provider_programs.fees
+        additionalCostInfo?: string, // TODO:provider_programs.cost
+        locationType: LocationType, // TODO: add provider_programs.locationType
+        getStartedUrl: string, // TODO: add provider_programs.getStartedUrl
     },
-    faq: { question: string, answer: string }[],
-    pathways: EduProviderPathways[]
+    faq: { question: string, answer: string }[], // TODO: add to db provider_programs.faq as TEXT field parse response with JSON.parse, JSON.stringify
+    pathways: EduProviderPathways[] // provider_programs.pathways will need converted from string to list and each item cast into EduProviderPathways enum
 }
 
 export type ReadEduProviderProgramCardDTO = {
     programId: string, // provider_programs.training_program_id
-    // logoUrl: string // will use blobStorage method to retrieve image url. // TODO: create post, get method for this.
-    programName: string,
-    eduProviderName: string,
-    edType: EducationType, // TODO: should be added to EduProviderDTO.
-    programLength: string,
-    cost: string, // brief cost
+    // logoUrl: string // will use blobStorage ss method to retrieve image url. getEduProviderLogoUrl(eduProviderId)
+    programName: string, // provider_programs join programs on program_id
+    eduProviderName: string, // provider_programs join edu_providers on edu_provider_id
+    eduLevel: EducationLevel, // TODO: add provider_programs.edLevel
+    programLength: string, // TODO: add provider_programs.programLength
+    costSummary: string, // TODO: remove cost, add provider_programs.costSummary db.TEXT
     pathway: EduProviderPathways[]
 }
 
