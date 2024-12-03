5b9f346e153cfae758c8ee369f71e2ccb678fecb
theGaryLarson <gary.larson@seattlecolleges.edu>
2024-12-03 12:46:12 -0800

    Integrate Prisma client for database access
    Added Prisma client to eduProviders and implemented functions to retrieve provider program card and detail views. The changes include restructuring DTOs for better alignment with new fields. This setup enhances data handling and streamlines interactions with the database.

--- app/lib/eduProviders.ts
+++ app/lib/eduProviders.ts
@@ -1,6 +1,9 @@
 // TODO: CREATE FORM FOR TRAINING PROVIDER DATA
-
+import { PrismaClient } from '@prisma/client';
+import getPrismaClient from '@/app/lib/prismaClient.mjs';
 import {EducationLevel} from "@/data/dtos/JobSeekerProfileCreationDTOs";
+
+const prisma: PrismaClient = getPrismaClient();
 
 export type ReadEduProviderDTO = {
     eduProviderId: string, //edu_providers.id
@@ -22,33 +25,31 @@ export type ReadEduProviderDTO = {
 }
 
 
-export type ReadEduProviderProgramDetailDTO = {
-    programId: string, // provider_programs.training_program_id
-    // logoUrl: string // will use blobStorage ss method to retrieve image url. getEduProviderLogoUrl(eduProviderId)
-    programName: string, // provider_programs join programs on program_id
-    eduProviderName: string, // provider_programs join edu_providers on edu_provider_id
-    locations: string[], // TODO: add provider_programs.locations, save as TEXT field in db but separate into list on DTO delimiter (~)
-    about: string, // TODO: add provider_programs.about use Quill to implement this
-    quickInfo: {
-        tuition?: string, // TODO: add provider_programs.tuition
-        fees?: string, // TODO: add provider_programs.fees
-        additionalCostInfo?: string, // TODO:provider_programs.cost
-        locationType: LocationType, // TODO: add provider_programs.locationType
-        getStartedUrl: string, // TODO: add provider_programs.getStartedUrl
-    },
-    faq: { question: string, answer: string }[], // TODO: add to db provider_programs.faq as TEXT field parse response with JSON.parse, JSON.stringify
-    pathways: EduProviderPathways[] // provider_programs.pathways will need converted from string to list and each item cast into EduProviderPathways enum
-}
-
 export type ReadEduProviderProgramCardDTO = {
     programId: string, // provider_programs.training_program_id
     // logoUrl: string // will use blobStorage ss method to retrieve image url. getEduProviderLogoUrl(eduProviderId)
     programName: string, // provider_programs join programs on program_id
     eduProviderName: string, // provider_programs join edu_providers on edu_provider_id
-    eduLevel: EducationLevel, // TODO: add provider_programs.edLevel
-    programLength: string, // TODO: add provider_programs.programLength
-    costSummary: string, // TODO: remove cost, add provider_programs.costSummary db.TEXT
+    eduLevel: EducationLevel, // provider_programs.eduLevel
+    programLength: string, // provider_programs.programLength
+    costSummary: string, // provider_programs.costSummary db.TEXT
     pathway: EduProviderPathways[]
+}
+
+export type ReadEduProviderProgramDetailDTO = {
+    programId: string, // provider_programs.training_program_id
+    // logoUrl: string // will use blobStorage ss method to retrieve image url. getEduProviderLogoUrl(eduProviderId)
+    programName: string, // provider_programs join programs on program_id
+    eduProviderName: string, // provider_programs join edu_providers on edu_provider_id
+    locations: string[], // provider_programs.locations. Saved as TEXT field in db but separate into list on DTO delimiter (~)
+    about: string, // provider_programs.about Possibly use Quill to implement this
+    tuition?: string, // provider_programs.tuition
+    fees?: string, // provider_programs.fees
+    costSummary?: string, // provider_programs.costSummary
+    locationType: LocationType, // provider_programs.locationType (enum LocationType)
+    getStartedUrl: string, // provider_programs.getStartedUrl
+    faq: { question: string, answer: string }[], // provider_programs.faq as TEXT field parse response with JSON.parse, JSON.stringify
+    pathways: EduProviderPathways[] // provider_programs.pathways will need converted from string to list and each item cast into EduProviderPathways enum
 }
 
 export enum EduProviderPathways {
@@ -69,4 +70,14 @@ export enum EducationType { // TODO: Instead of creating a new enum. Let's use t
 export enum LocationType {
     InPerson = 'In-Person',
     Online = 'Online',
+}
+
+
+export const getProviderProgramCardView = async (pathway: EduProviderPathways) => {
+    const data = await prisma.provider_programs.findMany()
+
+}
+
+export const getProviderProgramDetailView = async (trainingProviderId: string) => {
+
 }
