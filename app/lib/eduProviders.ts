02569ad3606808881a4017f9a668e425d78842f0
theGaryLarson <gary.larson@seattlecolleges.edu>
2024-12-03 15:29:55 -0800

    Export isEnumValue for external module use
    Changed the `isEnumValue` function to be exported, enabling its use across other modules. This modification facilitates enum validation checks outside the current file.

--- app/lib/eduProviders.ts
+++ app/lib/eduProviders.ts
@@ -182,7 +182,7 @@ export const getProviderProgramDetailView = async (
     return dto;
 };
 
-function isEnumValue<T extends { [key: string]: string | number }>(
+export function isEnumValue<T extends { [key: string]: string | number }>(
     enumObj: T,
     value: string | number | null
 ): value is T[keyof T] {
