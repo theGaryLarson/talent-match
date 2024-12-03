c6acf05ede133f6545d4aa07326d6a436275c0a8
Gage Berz <gageberz@computingforall.org>
2024-12-03 11:05:41 -0500

    policies/terms-of-service: Added text provided by Gary

--- app/(main)/policies/terms-of-service/page.tsx
+++ app/(main)/policies/terms-of-service/page.tsx
@@ -1,96 +1,169 @@
 import { Box, Container, Typography, Link, List, ListItem, ListItemText } from '@mui/material';
 
-export default function TermsOfService() {
+export default function page() {
   return (
     <Container maxWidth="md">
       <Box sx={{ my: 4 }}>
-        <Typography variant="h3" component="h1" gutterBottom align="center" sx={{fontWeight: "bold"}}>
+        <Typography variant="h3" component="h1" gutterBottom align="center" sx={{ fontWeight: "bold" }}>
           Terms of Service
         </Typography>
         <Typography align="center">
-          Published: December 2, 2024
+          Published: December 3, 2024
         </Typography>
-        <Typography align="center" sx={{mb: 2}}>
-          <Box component={"span"} sx={{fontWeight: "bold"}}>Effective: </Box>December 2, 2024
+        <Typography align="center" sx={{ mb: 2 }}>
+          <Box component={"span"} sx={{ fontWeight: "bold" }}>Effective: </Box>December 3, 2024
         </Typography>
 
         <Typography>
-          Thank you for using Washington Tech Coalition! By accessing or using our services, you agree to comply with and be bound by
-          the following terms and conditions. Please read them carefully.
+          These Terms and Conditions ("Terms") govern your use of the website located at <Link href="https://www.watechcoalition.org">www.watechcoalition.org</Link> ("Site"),
+          which is owned and operated by the Washington Tech Workforce Coalition ("we", "our", or "us").
+          By using this Site, you acknowledge that you have read, understand, and agree to abide by these Terms.
         </Typography>
 
-        <Typography variant="h5" component="h5" gutterBottom sx={{mt: 4, fontWeight: "bold"}}>
-          1. Acceptance of Terms
+        <Typography variant="h5" component="h5" gutterBottom sx={{ mt: 2, fontWeight: "bold" }}>
+          Intellectual Property
         </Typography>
         <Typography>
-          By using our website, you acknowledge that you have read, understood, and agree to be bound by these terms.
-          If you do not agree to these terms, you may not access or use the services.
+          All content available on our Site, including but not limited to images, text, logos, documents, and downloadable files,
+          is the exclusive property of Washington Tech Workforce Coalition and the Site's creators.
+          Unauthorized use of any content is prohibited.
         </Typography>
 
-        <Typography variant="h5" component="h5" gutterBottom sx={{mt: 4, fontWeight: "bold"}}>
-          2. Changes to Terms
+        <Typography variant="h5" component="h5" gutterBottom sx={{ mt: 2, fontWeight: "bold" }}>
+          User Contributions
         </Typography>
         <Typography>
-          We reserve the right to modify these terms at any time. Changes will be effective immediately upon posting
-          to the website. Your continued use of the service constitutes acceptance of the modified terms.
-        </Typography>
-
-        <Typography variant="h5" component="h5" gutterBottom sx={{mt: 4, fontWeight: "bold"}}>
-          3. User Conduct
-        </Typography>
-        <Typography>
-          You agree not to engage in any prohibited activities, including but not limited to:
+          Users may post content on our Site, such as:
         </Typography>
         <List disablePadding sx={{ listStyleType: 'disc', '& .MuiListItem-root': { display: 'list-item', marginLeft: 3 }, }}>
-          <ListItem disableGutters>
-            <ListItemText primary="Violating any laws or regulations" />
+          <ListItem disablePadding>
+            <ListItemText primary="YouTube video showcasing skills" />
           </ListItem>
-          <ListItem disableGutters>
-            <ListItemText primary="Infringing on the intellectual property rights of others." />
+          <ListItem disablePadding>
+            <ListItemText primary="Resume" />
           </ListItem>
-          <ListItem disableGutters>
-            <ListItemText primary="Attempting to disrupt the website or its security feature." />
+          <ListItem disablePadding>
+            <ListItemText primary="Cover letter" />
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemText primary="Work History" />
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemText primary="Education history" />
+          </ListItem>
+        </List>
+        <Typography>
+          By posting on the Site, you agree not to post anything illegal or that violates these Terms.
+        </Typography>
+
+        <Typography variant="h5" component="h5" gutterBottom sx={{ mt: 2, fontWeight: "bold" }}>
+          Accounts
+        </Typography>
+        <Typography>
+          When creating an account on our Site, you agree to:
+        </Typography>
+        <List disablePadding sx={{ listStyleType: 'decimal', '& .MuiListItem-root': { display: 'list-item', marginLeft: 3 }, }}>
+          <ListItem disablePadding>
+            <ListItemText primary="Be solely responsible for the security and privacy of your account, including all sensitive information associated with OAuth 2.0 authentication. " />
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemText primary="Ensure that the personal information you provide is accurate, truthful, and up-to-date, and update it as needed." />
+          </ListItem>
+        </List>
+        <Typography>We use OAuth 2.0 for account authentication, and support providers such as Google, Microsoft Entra, and GitHub.</Typography>
+        <Typography>We reserve the right to suspend or terminate your account if we believe you are using our Site unlawfully or violating these Terms.</Typography>
+
+        <Typography variant="h5" component="h5" gutterBottom sx={{ mt: 2, fontWeight: "bold" }}>
+          Services
+        </Typography>
+        <Typography>
+          We provide the following services to jobseekers: connecting them to training providers to increase their job-readiness in the IT industry
+          and connecting them with potential employers in IT-related job roles. We do not supply any goods or services directly.
+          If we become aware that a user is violating these Terms, we reserve the right to suspend or prohibit the user from using our Site.
+        </Typography>
+
+        <Typography variant="h5" component="h5" gutterBottom sx={{ mt: 2, fontWeight: "bold" }}>
+          Links to Third-Party Websites
+        </Typography>
+        <Typography>
+          Our Site may contain links to third-party websites or services that we do not control.
+          We are not responsible for the content, policies, or practices of these third-party websites.
+          You are responsible for reviewing the terms and conditions and privacy policies of any third-party websites you use.
+        </Typography>
+
+        <Typography variant="h5" component="h5" gutterBottom sx={{ mt: 2, fontWeight: "bold" }}>
+          Limitation of Liability
+        </Typography>
+        <Typography>
+          Washington Tech Workforce Coalition and our directors, officers, agents, employees, subsidiaries, and affiliates are not liable for
+          any actions, claims, losses, damages, liabilities, or expenses, including legal fees, arising from your use of the Site.
+        </Typography>
+
+        <Typography variant="h5" component="h5" gutterBottom sx={{ mt: 2, fontWeight: "bold" }}>
+          Indemnification
+        </Typography>
+        <Typography>
+          Except where prohibited by law, you agree to indemnify and hold harmless Washington Tech Workforce Coalition,
+          along with our directors, officers, agents, employees, subsidiaries, and affiliates, from any actions, claims, losses,
+          damages, liabilities, or expenses, including legal fees, resulting from your use of our Site or your violation of these Terms.
+        </Typography>
+
+        <Typography variant="h5" component="h5" gutterBottom sx={{ mt: 2, fontWeight: "bold" }}>
+          Governing Law
+        </Typography>
+        <Typography>
+          These Terms are governed by the laws of the State of Washington.
+        </Typography>
+
+        <Typography variant="h5" component="h5" gutterBottom sx={{ mt: 2, fontWeight: "bold" }}>
+          Severability
+        </Typography>
+        <Typography>
+          If any provision of these Terms is found to be invalid or unenforceable under applicable law,
+          such provision will be removed, and the remaining provisions will continue in full effect.
+        </Typography>
+
+        <Typography variant="h5" component="h5" gutterBottom sx={{ mt: 2, fontWeight: "bold" }}>
+          Changes to Terms and Conditions
+        </Typography>
+        <Typography>
+          We may amend these Terms from time to time to ensure compliance with the law or reflect changes in how we operate our Site.
+          Users will be notified of significant changes via email or through a notice posted on our Site.
+        </Typography>
+
+        <Typography variant="h5" component="h5" gutterBottom sx={{ mt: 2, fontWeight: "bold" }}>
+          Data Sharing
+        </Typography>
+        <Typography>
+          By signing this agreement, you agree that we may communicate and validate your education and work history
+          through our training partners and employers. The following data is collected for the sole purpose of Grant reporting
+          and will only be shared with the State of Washington for grant reporting requirements and will not be shared with employers or training providers:
+        </Typography>
+        <List disablePadding sx={{ listStyleType: 'disc', '& .MuiListItem-root': { display: 'list-item', marginLeft: 3 }, }}>
+          <ListItem disablePadding>
+            <ListItemText primary="Gender" />
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemText primary="Veteran Status" />
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemText primary="Ethnicity" />
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemText primary="Race" />
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemText primary="Mental Disability" />
           </ListItem>
         </List>
 
-        <Typography variant="h5" component="h5" gutterBottom sx={{mt: 4, fontWeight: "bold"}}>
-          4. Intellectual Property
+        <Typography variant="h5" component="h5" gutterBottom sx={{ mt: 2, fontWeight: "bold" }}>
+          Contact Information
         </Typography>
-        <Typography>
-          All content, including but not limited to text, images, and software, is the property of Washington Tech Coalition
-          or its licensors. Unauthorized use of the content is prohibited.
-        </Typography>
-
-        <Typography variant="h5" component="h5" gutterBottom sx={{mt: 4, fontWeight: "bold"}}>
-          5. Disclaimer of Warranties
-        </Typography>
-        <Typography>
-          The services are provided "as is" without warranties of any kind, either express or implied. We do not
-          guarantee the accuracy, completeness, or reliability of the services.
-        </Typography>
-
-        <Typography variant="h5" component="h5" gutterBottom sx={{mt: 4, fontWeight: "bold"}}>
-          6. Limitation of Liability
-        </Typography>
-        <Typography>
-          To the fullest extent permitted by law, Washington Tech Coalition shall not be liable for any damages arising from
-          your use of the services.
-        </Typography>
-
-        <Typography variant="h5" component="h5" gutterBottom sx={{mt: 4, fontWeight: "bold"}}>
-          7. Governing Law
-        </Typography>
-        <Typography>
-          These terms are governed by the laws of Potatoland
-        </Typography>
-
-        <Typography variant="h5" component="h5" gutterBottom sx={{mt: 4, fontWeight: "bold"}}>
-          8. Contact Information
-        </Typography>
-        <Typography>
-          If you have any questions about these terms, please contact us at{' '}
-          <Link href="mailto:support@watechcoalition.org">support@watechcoalition.org</Link>.
-        </Typography>
+        <Typography>If you have any questions or concerns, please contact us at:</Typography>
+        <Link href="mailto:Ritu@computingforall.org">Ritu@computingforall.org</Link>
+        <Typography>Computing for All</Typography>
+        <Typography>1311 108th Ave NE, Bellevue, WA 98004</Typography>
       </Box>
     </Container>
   );
