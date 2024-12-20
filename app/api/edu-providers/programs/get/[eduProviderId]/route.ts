58818aa24071970143ba72c48e0f98fb41143139
theGaryLarson <gary.larson@seattlecolleges.edu>
2024-12-19 20:20:46 -0800

    Add endpoint to fetch programs by eduProviderId
    Introduced a GET endpoint to retrieve training programs for a specific education provider based on eduProviderId.

---
+++ app/api/edu-providers/programs/get/[eduProviderId]/route.ts
@@ -0,0 +1,31 @@
+import { NextResponse } from 'next/server'; // Adjust imports based on your framework
+import { getProviderProgramCardView } from "@/app/lib/eduProviders";
+export async function GET(request: Request, { params }: { params: { eduProviderId: string } }) {
+  const  eduProviderId = params.eduProviderId;
+
+  if (!eduProviderId) {
+    return NextResponse.json(
+      { error: 'eduProviderId is required' },
+      { status: 400 }
+    );
+  }
+
+  try {
+    const providerPrograms = await getProviderProgramCardView(eduProviderId);
+
+    if (!providerPrograms) {
+      return NextResponse.json(
+        { error: 'Training provider programs not found.' },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json(providerPrograms, { status: 200 });
+  } catch (error) {
+    console.error('Error fetching training provider programs:', error);
+    return NextResponse.json(
+      { error: 'Internal server error' },
+      { status: 500 }
+    );
+  }
+}
