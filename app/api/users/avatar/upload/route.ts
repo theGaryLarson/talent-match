0b57c26703cebc094062ec661127c4e7aa9843ae
theGaryLarson <gary.larson@seattlecolleges.edu>
2024-12-24 02:31:14 -0800

    Refactor avatar upload to retrieve userId from request body.
    The code now retrieves `userId` directly from the request body instead of relying on the session. This approach aligns with prior usage in `AvatarUpload.tsx` and accommodates cases like admin changes to other users' data.

--- app/api/users/avatar/upload/route.ts
+++ app/api/users/avatar/upload/route.ts
@@ -8,12 +8,20 @@ const prisma: PrismaClient = getPrismaClient();
 
 export async function POST(request: Request) {
     try {
-        // Get essentials from session, not the request
-        let session = await auth();
-        const userId: string = session?.user.id!;
+        // Get essentials from session, not the request.
+        // @Rory, let's discuss this tomorrow please.
+        // In this instance. The component is the control/gatekeeper for
+        // whether we need an userId, companyId or eduProviderId.
+        // We could create three separate components that work
+        // identically, so we know the context and can use session but that still wouldn't
+        // work for a third party. aka
+        // admin changing someone else's data.
+
+        // let session = await auth();
+        // const userId: string = session?.user.id!;
 
         const body = await request.json();
-        const { file, fileName } = body;
+        const { file, fileName, userId } = body; // destructured userId from the body (there from older code in AvatarUpload.tsx).
 
         if (!file || !userId) {
             return NextResponse.json({ success: false, error: "Missing file or userId" }, { status: 400 });
