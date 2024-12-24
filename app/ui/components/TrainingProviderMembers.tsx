f9f6e5b08132855f01d8d8a0d6a14727dc8668d6
Rory.Hayes <rorosaurus@gmail.com>
2024-12-24 15:17:58 -0800

    add key prop to TrainingProviderMembers.tsx

--- app/ui/components/TrainingProviderMembers.tsx
+++ app/ui/components/TrainingProviderMembers.tsx
@@ -13,7 +13,7 @@ export default async function TrainingProviderMembers() {
             </div>
             <div className="w-full pb-8 justify-center items-center gap-10 inline-flex flex-wrap">
                 {trainingProviders != null && trainingProviders.map(function (tp, i) {
-                    return <Link href={"/services/training-providers/" + tp.eduProviderId}>
+                    return <Link href={"/services/training-providers/" + tp.eduProviderId} key={i}>
                         <div className="h-24 px-7 py-4 bg-neutral-100 hover:bg-sky-900 text-cyan-700 hover:text-neutral-100 rounded-3xl flex-col justify-start items-start gap-2.5 inline-flex">
                             <div className="self-stretch h-20 flex-col justify-center items-center gap-2.5 flex">
                                 <div className="self-stretch h-11 justify-center items-center gap-2.5 inline-flex">
