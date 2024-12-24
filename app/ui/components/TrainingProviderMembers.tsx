fc6a53b68e55af7b9314144fd77603b69063da1b
Rory.Hayes <rorosaurus@gmail.com>
2024-12-24 15:10:57 -0800

    add hover behavior for coalition members

--- app/ui/components/TrainingProviderMembers.tsx
+++ app/ui/components/TrainingProviderMembers.tsx
@@ -14,13 +14,13 @@ export default async function TrainingProviderMembers() {
             <div className="w-full pb-8 justify-center items-center gap-10 inline-flex flex-wrap">
                 {trainingProviders != null && trainingProviders.map(function (tp, i) {
                     return <Link href={"/services/training-providers/" + tp.eduProviderId}>
-                        <div className="h-24 px-7 py-4 bg-neutral-100 rounded-3xl flex-col justify-start items-start gap-2.5 inline-flex">
+                        <div className="h-24 px-7 py-4 bg-neutral-100 hover:bg-sky-900 text-cyan-700 hover:text-neutral-100 rounded-3xl flex-col justify-start items-start gap-2.5 inline-flex">
                             <div className="self-stretch h-20 flex-col justify-center items-center gap-2.5 flex">
                                 <div className="self-stretch h-11 justify-center items-center gap-2.5 inline-flex">
                                     <Image width={500} height={500} className="w-32" src={tp.logoUrl} alt={tp.providerName + " logo"} />
                                 </div>
                                 <div className="self-stretch justify-center items-center gap-2.5 inline-flex">
-                                    <div className="text-cyan-700 text-base font-semibold font-['Roboto'] uppercase leading-none tracking-wider">
+                                    <div className="text-base font-semibold font-['Roboto'] uppercase leading-none tracking-wider">
                                         {tp.providerName}
                                     </div>
                                 </div>
