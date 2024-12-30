64de4331047aa7e23f9869b66e051107c21781cf
not-damien <93546644+not-damien@users.noreply.github.com>
2024-12-30 15:04:03 -0800

    email notifications for career prep assessment working

---
+++ emails/career-prep-applicant-email-notification.tsx
@@ -0,0 +1,62 @@
+import {
+    Body,
+    Button,
+    Container,
+    Head,
+    Heading,
+    Hr,
+    Html,
+    Preview,
+    Section,
+    Text
+  } from '@react-email/components';
+  import { Tailwind } from '@react-email/tailwind';
+  
+  //import { AppInfo } from '@/constants/app-info';
+  //import { Routes } from '@/constants/routes';
+  //import { getBaseUrl } from '@/lib/urls/get-base-url';
+  
+  export type CareerPrepNotificationData = {
+    recipient: string;
+    navigatorName: string;
+    applicantName: string;
+  };
+  
+  export const CareerPrepApplicantEmailNotification = ({navigatorName}: CareerPrepNotificationData) => (
+    <Html>
+      <Head />
+      <Preview>New Career Prep Case Available!</Preview>
+      <Tailwind>
+        <Body className="m-auto bg-white px-2 font-sans">
+          <Container className="mx-auto my-[40px] max-w-[465px] rounded border border-solid border-[#eaeaea] p-[20px]">
+            <Heading className="mx-0 my-[30px] p-0 text-center text-[24px] font-normal text-black">
+            New Career Prep Case(s) Available!
+            </Heading>
+            <Text className="text-[14px] leading-[24px] text-black">
+              Hello {navigatorName},
+            </Text>
+            <Text className="text-[14px] leading-[24px] text-black">
+              There has been a new application to the career prep platform that needs your attention. 
+            </Text>
+            <Section className="my-[32px] text-center">
+              <Button
+                className="rounded bg-[#000000] px-5 py-3 text-center text-[12px] font-semibold text-white no-underline"
+                href={`https://www.watechcoalition.org/career-prep/new-cases`}
+              >
+                Claim or View New Cases Here
+              </Button>
+            </Section>
+            <Text className="text-[14px] leading-[24px] text-black">
+              If you have any questions or need assistance, please don't hesitate
+              to reach out to our support team.
+            </Text>
+            <Hr className="mx-0 my-[26px] w-full border border-solid border-[#eaeaea]" />
+            <Text className="text-[12px] leading-[24px] text-[#666666]">
+              You receive this email because you are registered as a Career Navigator on https://www.watechcoalition.org/.
+            </Text>
+          </Container>
+        </Body>
+      </Tailwind>
+    </Html>
+  );
+  
---
+++ lib/smtp/send-career-prep-applicant-email-notification.ts
@@ -0,0 +1,30 @@
+import { CareerPrepApplicantEmailNotification, CareerPrepNotificationData } from '@/emails/career-prep-applicant-email-notification';
+import { render } from '@react-email/render';
+import { sendEmail } from '@/lib/smtp/mailer/send-email';
+import { getAllCareerNavigators } from '@/app/lib/admin/careerPrep';
+
+
+
+export async function sendCareerPrepApplicantEmailNotificationEmail(applicantName:string) {
+    const carrerNavs = await getAllCareerNavigators();
+    if(!carrerNavs) return;
+    carrerNavs.forEach((cn)=>{
+        sendSingleCareerPrepApplicantEmailNotificationEmail({
+            recipient: cn.email,
+            applicantName: applicantName,
+            navigatorName: `${cn.first_name} ${cn.last_name}`
+        })
+    })
+}
+
+async function sendSingleCareerPrepApplicantEmailNotificationEmail(data:CareerPrepNotificationData){
+    const component = CareerPrepApplicantEmailNotification(data);
+    const html = await render(component);
+    const text = await render(component, { plainText: true });
+    await sendEmail({
+        recipient: data.recipient,
+        subject: `New Case Available`,
+        html,
+        text
+      });
+}
