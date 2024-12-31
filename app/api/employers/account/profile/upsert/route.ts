4761e91a3787bfae9627947a997f212342d5164c
theGaryLarson <gary.larson@seattlecolleges.edu>
2024-12-31 14:07:43 -0800

    Remove unused `hasAgreedTerms` field in profile upsert
    Cleaned up unused `hasAgreedTerms` property to reduce redundancy and improve database schema clarity.

--- app/api/employers/account/profile/upsert/route.ts
+++ app/api/employers/account/profile/upsert/route.ts
@@ -88,7 +88,6 @@ export async function PATCH(request: Request) {
           employer_id: session?.user.employerId!,
           job_title: currentJobTitle,
           linkedin_url: linkedInUrl,
-          hasAgreedTerms: false,
           is_verified_employee: false,
           users: {
             connect: {
