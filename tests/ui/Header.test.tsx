284165970bdd020bfb5a9d19af592fcb5f5ba73d
cfa-jonathan <91294137+cfa-jonathan@users.noreply.github.com>
2024-12-30 19:17:07 -0800

    Update the vitest for Header.test.tsx

--- tests/ui/Header.test.tsx
+++ tests/ui/Header.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen, fireEvent, cleanup, within } from '@testing-library/react';
+import { userEvent } from '@testing-library/user-event';
 import { describe, it, expect, vi, afterEach } from 'vitest';
 import Header from '../../app/ui/Header';
 import { usePathname } from 'next/navigation';
@@ -35,14 +36,38 @@ describe('Header', () => {
         cleanup();
     });
 
-    it('renders the logo and navigation links', () => {
+    it('renders the logo and navigation links', async () => {
         vi.mocked(usePathname).mockReturnValue('/');
         renderWithProviders(<Header />);
 
         expect(screen.getByAltText('Tech Workforce Coalition')).toBeDefined();
+
+        const topLinkForEmployers = screen.getByText('For Employers');
+        expect(topLinkForEmployers).toBeDefined();
+        await userEvent.click(topLinkForEmployers);
+        expect(screen.getByText('Landing Page')).toBeDefined();
         expect(screen.getByText('Talent Showcase')).toBeDefined();
-        expect(screen.getByText('For Employers')).toBeDefined();
-        expect(screen.getByText('For Job Seekers')).toBeDefined();
+
+        const topLinkForJobseekers = screen.getByText('For Jobseekers');
+        expect(topLinkForJobseekers).toBeDefined();
+        await userEvent.click(topLinkForJobseekers);
+        expect(screen.getByText('Landing Page')).toBeDefined();
+        expect(screen.getByText('Job Listings')).toBeDefined();
+
+        const topLinkOurCommunity = screen.getByText('Our Community');
+        expect(topLinkOurCommunity).toBeDefined();
+        await userEvent.click(topLinkOurCommunity);
+        expect(screen.getByText('Join Our Community')).toBeDefined();
+        expect(screen.getByText('Careers')).toBeDefined();
+
+        expect(screen.getByText('Events')).toBeDefined();
+
+        const topLinkCoalition = screen.getByText('Coalition');
+        expect(topLinkCoalition).toBeDefined();
+        await userEvent.click(topLinkCoalition);
+        expect(screen.getByText('Training Providers')).toBeDefined();
+
+        expect(screen.getByText('About Us')).toBeDefined();
     });
 
     it('changes logo based on pathname', () => {
