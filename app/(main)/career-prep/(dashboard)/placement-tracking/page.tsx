c103e85585e2c2e75bf9c4a515b01dc1c3553920
not-damien <93546644+not-damien@users.noreply.github.com>
2025-01-02 11:52:06 -0800

    delete unused stuff

--- app/(main)/career-prep/(dashboard)/placement-tracking/page.tsx
+++ app/(main)/career-prep/(dashboard)/placement-tracking/page.tsx
@@ -1,19 +1,9 @@
 import { getAllJobPosts } from "@/app/lib/joblistings";
 import JobTrackingTable from "@/app/ui/components/careerPrep/JobTrackingTable";
-import { auth } from "@/auth";
-import { sendWelcomeEmail } from "@/lib/smtp/send-welcome-email";
 import * as React from "react";
 
 export default async function Page() {
     const jobposts = await getAllJobPosts();
-    console.log(JSON.stringify(jobposts,null,4))
-    const session = await auth();
-    // if(session?.user.email && session.user.name){
-    //     await sendWelcomeEmail({
-    //         name:session.user.name,
-    //         recipient:session.user.email
-    //     });
-    // }
     if(!jobposts){
         return <main><h1 className="text-2xl">No Job Lisings Found</h1></main>
     }
