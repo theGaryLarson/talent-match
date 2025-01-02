a754fe8b4b1e437586832af5873535a70e3c8d97
Rory.Hayes <rorosaurus@gmail.com>
2025-01-02 14:03:36 -0800

    refactor avatar upload to verify against session info

--- app/api/users/avatar/upload/route.ts
+++ app/api/users/avatar/upload/route.ts
@@ -2,29 +2,25 @@ import getPrismaClient from "@/app/lib/prismaClient.mjs";
 import { PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/server";
 import { uploadAvatar } from "@/app/lib/services/azureBlobService";
-// import { auth } from "@/auth";
+import { auth } from "@/auth";
+import { Role } from "@/data/dtos/UserInfoDTO";
 
 const prisma: PrismaClient = getPrismaClient();
 
 export async function POST(request: Request) {
     try {
-        // Get essentials from session, not the request.
-        // @Rory, let's discuss this tomorrow please.
-        // In this instance. The component is the control/gatekeeper for
-        // whether we need an userId, companyId or eduProviderId.
-        // We could create three separate components that work
-        // identically, so we know the context and can use session but that still wouldn't
-        // work for a third party. aka
-        // admin changing someone else's data.
-
-        // let session = await auth();
-        // const userId: string = session?.user.id!;
+        let session = await auth();
+        const sessionUserId: string = session?.user.id!;
 
         const body = await request.json();
-        const { file, fileName, userId } = body; // destructured userId from the body (there from older code in AvatarUpload.tsx).
+        const { file, fileName, userId } = body; // destructured userId from the body
 
         if (!file || !userId) {
             return NextResponse.json({ success: false, error: "Missing file or userId" }, { status: 400 });
+        }
+
+        if (userId !== sessionUserId && !session?.user.roles.includes(Role.ADMIN)) {
+            return NextResponse.json({ success: false, error: "Unauthorized" }, { status: 401 });
         }
 
         // Convert file back to Buffer
