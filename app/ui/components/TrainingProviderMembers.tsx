aee29772bb10dd31c2527c04bfd36abf686346d9
Rory.Hayes <rorosaurus@gmail.com>
2025-01-03 11:35:46 -0800

    clamp tp logo height

--- app/ui/components/TrainingProviderMembers.tsx
+++ app/ui/components/TrainingProviderMembers.tsx
@@ -17,7 +17,7 @@ export default async function TrainingProviderMembers() {
                         <div className="h-24 px-7 py-4 bg-neutral-100 hover:bg-sky-900 text-cyan-700 hover:text-neutral-100 rounded-3xl flex-col justify-start items-start gap-2.5 inline-flex">
                             <div className="self-stretch h-20 flex-col justify-center items-center gap-2.5 flex">
                                 <div className="mt-2 self-stretch h-11 justify-center items-center gap-2.5 inline-flex">
-                                    <Image width={500} height={500} className="w-32" src={tp.logoUrl || "/images/TWC_75x50_2024.svg"} alt={tp.providerName + " logo"} />
+                                    <Image width={500} height={500} className="w-32 h-20 object-contain" src={tp.logoUrl || "/images/TWC_75x50_2024.svg"} alt={tp.providerName + " logo"} />
                                 </div>
                                 <div className="my-2 self-stretch justify-center items-center gap-2.5 inline-flex">
                                     <div className="text-base font-semibold font-['Roboto'] uppercase leading-none tracking-wider">
