e4721f0010f6aaf20235f8cf57b44c79f9834366
Quincy Pham <quincytpham@gmail.com>
2025-01-06 13:41:36 -0500

    resume api route, get updated so jobseeker role cannot get unless is viewing own data

--- app/api/jobseekers/resume/get/[userId]/route.ts
+++ app/api/jobseekers/resume/get/[userId]/route.ts
@@ -2,33 +2,68 @@ import { NextResponse } from 'next/server';
 import getPrismaClient from '@/app/lib/prismaClient.mjs';
 import { PrismaClient } from '@prisma/client';
 import { getResumeUrl } from '@/app/lib/services/azureBlobService';
-import { auth } from "@/auth";
+import { auth } from '@/auth';
+import { Role } from '@/data/dtos/UserInfoDTO';
 
 const prisma: PrismaClient = getPrismaClient();
 
-export async function GET(request: Request, props: { params: Promise<{ userId: string }> }) {
-    const params = await props.params;
-    const userId = params.userId;
-    try {
+export async function GET(
+  request: Request,
+  props: { params: Promise<{ userId: string }> },
+) {
+  const params = await props.params;
+  const userId = params.userId;
+  try {
+    let session = await auth();
 
-        if (!userId) {
-            return NextResponse.json({ success: false, url: null, error: 'No user ID in session storage.' }, { status: 400 });
-        }
-
-        const url = await getResumeUrl(userId);
-
-        if (!url) {
-            return NextResponse.json({ success: false, url: null, error: 'No saved resume.' }, { status: 404 });
-        }
-
-        return NextResponse.json(url);
-    } catch (e: any) {
-        console.error('Error retrieving resume from Blob storage:', e.message);
-        return NextResponse.json(
-            { error: `Failed to retrieve resume for user.\n${e.message}` },
-            { status: 500 },
-        );
-    } finally {
-        await prisma.$disconnect();
+    // Check if the session exists
+    if (!session?.user) {
+      return NextResponse.json({ error: 'Page not found' }, { status: 404 });
     }
+
+    const {
+      roles,
+      employeeIsApproved,
+      jobseekerId: userJobseekerId,
+    } = session.user;
+
+    // Allow access if:
+    // 1. The user is an EMPLOYER and is approved, OR ADMIN, OR...
+    // 2. The user is a JOBSEEKER and their jobseekerId matches the requested jobseekerId
+    const isEmployerApproved =
+      (roles.includes(Role.EMPLOYER) && employeeIsApproved) ||
+      roles.includes(Role.ADMIN);
+    const isJobseekerViewingOwnData =
+      roles.includes(Role.JOBSEEKER) && userJobseekerId === userId;
+
+    if (!isEmployerApproved && !isJobseekerViewingOwnData) {
+      return NextResponse.json({ error: 'Page not found' }, { status: 404 });
+    }
+
+    if (!userId) {
+      return NextResponse.json(
+        { success: false, url: null, error: 'No user ID in session storage.' },
+        { status: 400 },
+      );
+    }
+
+    const url = await getResumeUrl(userId);
+
+    if (!url) {
+      return NextResponse.json(
+        { success: false, url: null, error: 'No saved resume.' },
+        { status: 404 },
+      );
+    }
+
+    return NextResponse.json(url);
+  } catch (e: any) {
+    console.error('Error retrieving resume from Blob storage:', e.message);
+    return NextResponse.json(
+      { error: `Failed to retrieve resume for user.\n${e.message}` },
+      { status: 500 },
+    );
+  } finally {
+    await prisma.$disconnect();
+  }
 }
