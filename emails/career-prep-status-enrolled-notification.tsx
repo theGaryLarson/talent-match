5927e2675f536e511dbf1e2efdc508174667af1d
theGaryLarson <gary.larson@seattlecolleges.edu>
2025-01-07 14:22:59 -0800

    Add email templates for CareerPrepStatus change
    Add templates for CareerPrepStatus change to "Plan Created" and "Enrolled".

---
+++ emails/career-prep-status-enrolled-notification.tsx
@@ -0,0 +1,65 @@
+import {
+  Body,
+  Button,
+  Container,
+  Head,
+  Heading,
+  Hr,
+  Html,
+  Preview,
+  Section,
+  Text,
+} from '@react-email/components';
+import { Tailwind } from '@react-email/tailwind';
+
+//import { AppInfo } from '@/constants/app-info';
+//import { Routes } from '@/constants/routes';
+//import { getBaseUrl } from '@/lib/urls/get-base-url';
+
+export type DevPlanNotificationData = {
+  recipient: string;
+  name: string;
+};
+
+export const CPStatusPlanCreatedNotification = ({
+  name,
+}: DevPlanNotificationData) => (
+  <Html>
+    <Head />
+    <Preview>You’re Officially Enrolled!</Preview>
+    <Tailwind>
+      <Body className="font-sans m-auto bg-white px-2">
+        <Container className="mx-auto my-[40px] max-w-[465px] rounded border border-solid border-[#eaeaea] p-[20px]">
+          <Heading className="mx-0 my-[30px] p-0 text-center text-[24px] font-normal text-black">
+            You’re Officially Enrolled!
+          </Heading>
+          <Text className="text-[14px] leading-[24px] text-black">
+            Hello {name},
+          </Text>
+          <Text className="text-[14px] leading-[24px] text-black">
+            {`We're excited to officially welcome you to Career Prep! Log in to
+              your dashboard to view your personalized development plan and access
+              your Canvas training.`}
+          </Text>
+          <Section className="my-[32px] text-center">
+            <Button
+              className="rounded bg-[#000000] px-5 py-3 text-center text-[12px] font-semibold text-white no-underline"
+              href={`https://watechcoalition.org/services/jobseekers/dashboard`}
+            >
+              Open Dashboard
+            </Button>
+          </Section>
+          <Text className="text-[14px] leading-[24px] text-black">
+            {`If you have any questions or need assistance, please don't hesitate
+              to reach out to our support team.`}
+          </Text>
+          <Hr className="mx-0 my-[26px] w-full border border-solid border-[#eaeaea]" />
+          <Text className="text-[12px] leading-[24px] text-[#666666]">
+            You receive this email because you signed up on
+            https://www.watechcoalition.org/.
+          </Text>
+        </Container>
+      </Body>
+    </Tailwind>
+  </Html>
+);
