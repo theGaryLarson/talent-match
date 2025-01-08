05576c74fe394faeb6b441d39e13df0aa7d77881
Quincy Pham <quincytpham@gmail.com>
2025-01-08 13:39:44 -0500

    showcase page updated to remove flowbite and use only mui

--- app/(profile-creation)/edit-profile/jobseeker/showcase/page.tsx
+++ app/(profile-creation)/edit-profile/jobseeker/showcase/page.tsx
@@ -2,7 +2,8 @@
 
 import React, { useEffect, useRef, useState } from 'react';
 import ProgressBarFlat from '@/app/ui/components/ProgressBarFlat';
-import { Button } from 'flowbite-react';
+// import { Button } from 'flowbite-react';
+import { Button } from '@mui/material';
 import TagsWithAutocomplete from '@/app/ui/components/mui/TagsWithAutocomplete';
 import TextFieldWithSeparatedLabel from '@/app/ui/components/mui/TextFieldWithSeparatedLabel';
 import TextFieldWithNoLabel from '@/app/ui/components/mui/TextFieldWithNoLabel';
@@ -350,17 +351,16 @@ export default function CreateJobseekerProfileShowcasePage() {
           </div>
           <div className="profile-form-progress-btn-group">
             <Button
-              pill
-              className="custom-outline-btn"
+              // pill
+              // className="custom-outline-btn"
+              variant="outlined"
               onClick={() => {
                 router.push('/edit-profile/jobseeker/preferences');
               }}
             >
               Previous
             </Button>
-            <Button pill type="submit">
-              Save and continue
-            </Button>
+            <Button type="submit">Save and continue</Button>
           </div>
         </form>
       </section>
