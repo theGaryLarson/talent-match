4d7cc948f4b38ee34ea2c1854f137c0804eb104e
Quincy Pham <quincytpham@gmail.com>
2025-01-09 17:18:59 -0500

    completed flowbite to mui button refactor from disclosures, Label, List, ListItem still pending

--- app/(profile-creation)/edit-profile/jobseeker/disclosures/page.tsx
+++ app/(profile-creation)/edit-profile/jobseeker/disclosures/page.tsx
@@ -10,7 +10,9 @@ import {
   JsDisclosuresPostDTO,
 } from '@/data/dtos/JobSeekerProfileCreationDTOs';
 import { useRouter } from 'next/navigation';
-import { Button, Label, List, ListItem } from 'flowbite-react';
+import { Label, List, ListItem } from 'flowbite-react';
+import { Button } from '@mui/material';
+
 import {
   FormControl,
   FormControlLabel,
@@ -422,17 +424,14 @@ export default function CreateJobseekerProfileDisclosuresPage() {
           </fieldset>
           <div className="profile-form-progress-btn-group">
             <Button
-              pill
-              className="custom-outline-btn"
+              variant="outlined"
               onClick={() => {
                 router.push('/edit-profile/jobseeker/work-experience');
               }}
             >
               Previous
             </Button>
-            <Button pill type="submit">
-              Submit
-            </Button>
+            <Button type="submit">Submit</Button>
           </div>
         </form>
       </section>
