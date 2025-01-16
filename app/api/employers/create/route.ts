6b322777451d20d64b7e72afa3bd18428f2ef361
not-damien <93546644+not-damien@users.noreply.github.com>
2025-01-15 17:43:17 -0800

    emails now get sent to gary every time an employer is created

--- app/api/employers/create/route.ts
+++ app/api/employers/create/route.ts
@@ -1,16 +1,22 @@
 import { NextResponse } from 'next/server';
 import { createEmployer } from '@/app/lib/employer';
 import { auth } from '@/auth';
+import { sendEmployerOrCompanyNeedsApprovalEmailToGary } from '@/lib/smtp/send-employer-needs-verification-email';
 
 export async function POST() {
   try {
     let session = await auth();
     const userId: string = session?.user.id!;
     const employerData = await createEmployer(userId);
+    sendEmployerOrCompanyNeedsApprovalEmailToGary({
+      recipient: 'gary.larson@computingforall.org',
+      name: 'Gary Larson'
+    })
     return NextResponse.json(
       { success: true, message: 'Employer created successfully.', employerData },
       { status: 200 },
     );
+    
   } catch (error: any) {
     return NextResponse.json(
       { error: 'Failed to create Employer.' },
