d418e3e4aa417c750535b2300f7f0c6f83bb9b59
Quincy Pham <quincytpham@gmail.com>
2025-01-22 15:31:27 -0500

    Updated AvatarUpload to use MUI Avatar instead and sx styling equivalents

--- app/ui/components/AvatarUpload.tsx
+++ app/ui/components/AvatarUpload.tsx
@@ -1,8 +1,7 @@
 'use client';
 
 import { ChangeEvent, useEffect, useState } from 'react';
-import { Avatar, AvatarImageProps } from 'flowbite-react';
-
+import { Avatar } from '@mui/material';
 interface Props {
   id: string;
   fileTypeText: string;
@@ -41,7 +40,6 @@ export default function AvatarUpload({
       if (file.name !== '') {
         setFileSelected(file.name);
       }
-
 
       if (filePath) URL.revokeObjectURL(filePath);
       setFilePath(URL.createObjectURL(file));
@@ -106,27 +104,19 @@ export default function AvatarUpload({
   const fileTypeTextPlusSizeLimit =
     fileTypeText + ' (max. ' + maxSizeMB + ' MB)';
 
-  // it was getting late and playing around. Feel free to implement this however you find best :)
-  const imageProps: AvatarImageProps = {
-    className: 'w-20 h-20 rounded-full object-cover', // Ensure the image is a perfect circle
-    'data-testid': 'avatar-image',
-  };
-
   return (
     <div>
       <label className="flex cursor-pointer rounded-full p-4 hover:bg-slate-50">
         <Avatar
-          rounded
-          // img={filePath}
-          img={(props) => (
-            <img
-              src={filePath || initialImageUrl || undefined}
-              alt="Uploaded Avatar"
-              {...props}
-              {...imageProps}
-            />
-          )}
-          className="h-20 w-20 flex-shrink-0"
+          src={filePath || initialImageUrl || undefined}
+          alt="Uploaded Avatar"
+          sx={{
+            width: '5rem', // Tailwind's w-20
+            height: '5rem', // Tailwind's h-20
+            flexShrink: 0, // Tailwind's flex-shrink-0
+            borderRadius: '50%', // Mimics Flowbite's rounded
+          }}
+          data-testid="avatar-image"
         />
         <input
           type="file"
