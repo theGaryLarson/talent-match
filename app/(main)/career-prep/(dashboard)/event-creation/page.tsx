3c8419ca2aefd97bf7d53b1ab5d26399d8159276
not-damien <93546644+not-damien@users.noreply.github.com>
2025-01-23 11:58:26 -0800

    spacing on event creation page

--- app/(main)/career-prep/(dashboard)/event-creation/page.tsx
+++ app/(main)/career-prep/(dashboard)/event-creation/page.tsx
@@ -3,7 +3,7 @@ import EventUpdateForm from "@/app/ui/components/EventCreation/EventUpdateForm";
 
 export default function page(){
     return(
-        <main className="w-full gap-4 grid tablet:grid-cols-2 ">
+        <main className="w-full gap-4 grid tablet:grid-cols-2 m-4 ">
         <EventCreationForm/>
         <EventUpdateForm/>
         </main>
