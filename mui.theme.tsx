f9480e332fc662943ab5a1d873738d9933010bc0
Gage Berz <gageberz@computingforall.org>
2025-01-24 12:33:11 -0500

    mui.theme: initial palette scheme

--- mui.theme.tsx
+++ mui.theme.tsx
@@ -4,7 +4,9 @@ import { createTheme } from '@mui/material/styles';
 const theme = createTheme({
   cssVariables: true,
   colorSchemes: {
-    light: {},
+    light: {
+      palette: {},
+    },
     dark: false,
   },
   components: {},
