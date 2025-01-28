0b10d4cd6907c974b07febd9ecfd2029f2c30287
not-damien <93546644+not-damien@users.noreply.github.com>
2025-01-28 15:13:06 -0800

    add link to layout

--- app/(main)/career-prep/(dashboard)/layout.tsx
+++ app/(main)/career-prep/(dashboard)/layout.tsx
@@ -4,6 +4,7 @@ import CasesOutlinedIcon from "@mui/icons-material/CasesOutlined";
 import CreateNewFolderOutlinedIcon from "@mui/icons-material/CreateNewFolderOutlined";
 import TableChartOutlinedIcon from "@mui/icons-material/TableChartOutlined";
 import EditCalendarIcon from "@mui/icons-material/EditCalendar";
+import PostAddOutlinedIcon from '@mui/icons-material/PostAddOutlined';
 import { auth } from "@/auth";
 import { BookmarkIcon } from "@heroicons/react/24/outline";
 export default async function layout({
@@ -47,21 +48,19 @@ export default async function layout({
           Application Status
         </Link>
         <Link
+          href="/career-prep/postajob"
+          className="inline-flex items-center justify-start gap-[5px] p-2 hover:bg-gray-200"
+        >
+          <PostAddOutlinedIcon/>
+          Post a Job
+        </Link>
+        <Link
           href="/career-prep/event-creation"
           className="inline-flex items-center justify-start gap-[5px] p-2 hover:bg-gray-200"
         >
           <EditCalendarIcon />
           Events
         </Link>
-
-        {/* 
-        <Link
-          href="/career-prep"
-          className="inline-flex items-center justify-start gap-[5px] p-2 hover:bg-gray-200"
-        >
-          <BookmarkIcon width={24}/>
-          Unassigned Cases
-        </Link> */}
       </div>
       {children}
     </div>
