c22cfea4dc19fce43a91ed4ab860782671c824df
theGaryLarson <gary.larson@seattlecolleges.edu>
2025-01-30 11:07:58 -0800

    feat(events-frontend): refactor layout and indentation in page.tsx (#feature/events-frontend)
    Refactor layout and indentation in page.tsx to improve code readability. The modification does not affect any existing functionality or messages. The changes mainly involve adjusting the import statements and function definitions for better readability.

--- app/services/(dash)/jobseekers/dashboard/(overview)/events/page.tsx
+++ app/services/(dash)/jobseekers/dashboard/(overview)/events/page.tsx
@@ -1,15 +1,19 @@
-import { Grid2 } from '@mui/material';
-import { EventTypeEnum, getAllEvents, getRegisteredEvents } from '@/app/lib/events';
-import Event from '@/app/ui/components/Event';
-import React from 'react';
+import { Grid2 } from "@mui/material";
+import {
+  EventTypeEnum,
+  getAllEvents,
+  getRegisteredEvents,
+} from "@/app/lib/events";
+import Event from "@/app/ui/components/Event";
+import React from "react";
 
 export default async function Page() {
   const allEventsResponse = await getAllEvents(true);
   const registeredEventsResponse = await getRegisteredEvents(true);
 
-  const registered = function(id: string) {
+  const registered = function (id: string) {
     return registeredEventsResponse?.some((e) => e.eventId === id);
-  }
+  };
 
   return (
     <div className="m-0 tablet:m-4 laptop:m-8 flex-1">
@@ -17,16 +21,21 @@ export default async function Page() {
         spacing={1}
         container
         size={1}
-        sx={{ justifyContent: 'space-between' }}
+        sx={{ justifyContent: "space-between" }}
       >
         <p className="self-center text-xl font-medium text-button-secondary-idle-text">
           Upcoming Events
         </p>
       </Grid2>
-      <Grid2 container gap={2} sx={{ width: '100%' }}>
+      <Grid2 container gap={2} sx={{ width: "100%" }}>
         <div className="w-full p-2 items-center rounded-lg border text-lg shadow">
           {(await allEventsResponse)?.events?.map((event, i) => (
-            <Event key={i} showLink={false} registered={registered(event.id)} event={{ ...event, eventType: event.eventType as EventTypeEnum }} />
+            <Event
+              key={i}
+              showLink={false}
+              registered={registered(event.id)}
+              event={{ ...event, eventType: event.eventType as EventTypeEnum }}
+            />
           ))}
         </div>
       </Grid2>
