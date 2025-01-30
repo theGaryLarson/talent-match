9c13b4fa64549ae0d501b8943c1559d45e3cf453
theGaryLarson <gary.larson@seattlecolleges.edu>
2025-01-30 11:08:08 -0800

    feat(events-frontend): Refactor layout and indentation in layout.tsx (#feature/events-frontend)
    In layout.tsx file, adjust string literals to use double quotes instead of single quotes. Also, replace 'let' declaration with 'const' for session and status variables. Further, reformat the style object in the Box component for better readability.

--- app/services/(dash)/layout.tsx
+++ app/services/(dash)/layout.tsx
@@ -1,9 +1,9 @@
-'use client';
-import Link from 'next/link';
-import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
-import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
-import FolderOutlinedIcon from '@mui/icons-material/FolderOutlined';
-import { BookmarkIcon } from '@heroicons/react/24/outline';
+"use client";
+import Link from "next/link";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import FolderOutlinedIcon from "@mui/icons-material/FolderOutlined";
+import { BookmarkIcon } from "@heroicons/react/24/outline";
 import {
   BookmarkBorderRounded,
   CalendarTodayRounded,
@@ -17,7 +17,7 @@ import {
   TaskOutlined,
   TimelineRounded,
   WorkOutlineRounded,
-} from '@mui/icons-material';
+} from "@mui/icons-material";
 import {
   AppBar,
   Box,
@@ -26,15 +26,15 @@ import {
   IconButton,
   Stack,
   Toolbar,
-} from '@mui/material';
-import Image from 'next/image';
-import Avatar from '@/app/ui/components/Avatar';
-import { useSession } from 'next-auth/react';
-import { useCallback, useMemo, useState } from 'react';
-import { usePathname } from 'next/navigation';
-import PillButton from '@/app/ui/components/PillButton';
-import Header from '@/app/ui/Header';
-import Footer from '@/app/ui/Footer';
+} from "@mui/material";
+import Image from "next/image";
+import Avatar from "@/app/ui/components/Avatar";
+import { useSession } from "next-auth/react";
+import { useCallback, useState } from "react";
+import { usePathname } from "next/navigation";
+import PillButton from "@/app/ui/components/PillButton";
+import Header from "@/app/ui/Header";
+import Footer from "@/app/ui/Footer";
 
 const drawerWidth = 260;
 
@@ -43,58 +43,58 @@ const JobseekerDrawer = ({ session }: { session: any }) => {
 
   const links = [
     {
-      href: '/services/jobseekers/dashboard',
+      href: "/services/jobseekers/dashboard",
       icon: <PersonOutlineRounded />,
-      label: 'Home',
+      label: "Home",
     },
-    { href: '/underconstruction', icon: <InboxOutlined />, label: 'Inbox' },
+    { href: "/underconstruction", icon: <InboxOutlined />, label: "Inbox" },
     {
-      href: '/services/joblistings',
+      href: "/services/joblistings",
       icon: <WorkOutlineRounded />,
-      label: 'Jobs',
+      label: "Jobs",
     },
     {
-      href: '/services/jobseekers/dashboard/bookmarks',
+      href: "/services/jobseekers/dashboard/bookmarks",
       icon: <BookmarkBorderRounded />,
-      label: 'Saved Jobs',
+      label: "Saved Jobs",
     },
     {
-      href: '/services/jobseekers/dashboard/my-applications',
+      href: "/services/jobseekers/dashboard/my-applications",
       icon: <TaskOutlined />,
-      label: 'Applications',
+      label: "Applications",
     },
     {
-      href: '/services/jobseekers/dashboard/events',
+      href: "/services/jobseekers/dashboard/events",
       icon: <CalendarTodayRounded />,
-      label: 'Events',
+      label: "Events",
     },
     {
-      href: '/underconstruction',
+      href: "/underconstruction",
       icon: <TimelineRounded />,
-      label: 'Career Services',
+      label: "Career Services",
     },
     {
-      href: 'https://forum.watechwfcoalition.org/',
+      href: "https://forum.watechwfcoalition.org/",
       icon: <QuestionAnswerOutlined />,
-      label: 'Community Network',
+      label: "Community Network",
       external: true,
     },
   ];
 
   return (
     <>
-      <Toolbar sx={{ height: '76px' }} />
+      <Toolbar sx={{ height: "76px" }} />
       <div className="flex flex-col text-button-secondary-idle-text">
         <Stack
-          direction={'row'}
+          direction={"row"}
           gap={1}
-          sx={{mt: '25px', alignItems: 'center', ml: 5, mb: 3 }}
+          sx={{ mt: "25px", alignItems: "center", ml: 5, mb: 3 }}
         >
-          <Avatar scale={0.66} imgsrc={session?.user.image || ''} />
+          <Avatar scale={0.66} imgsrc={session?.user.image || ""} />
           <div>
             <p className="text-wrap font-bold">{session?.user.name}</p>
             <Link
-              href={'/edit-profile/jobseeker/introduction'}
+              href={"/edit-profile/jobseeker/introduction"}
               className="text-wrap text-sm text-primary-600 sm-tablet:text-base"
             >
               Edit Profile
@@ -107,10 +107,10 @@ const JobseekerDrawer = ({ session }: { session: any }) => {
             href={link.href}
             className={`inline-flex items-center justify-start rounded-l-[20px] p-2 ${
               pathname === link.href
-                ? 'bg-button-secondary-idle-text text-button-secondary-idle-bg'
-                : 'hover:bg-gray-200'
+                ? "bg-button-secondary-idle-text text-button-secondary-idle-bg"
+                : "hover:bg-gray-200"
             }`}
-            target={link.external ? '_blank' : undefined}
+            target={link.external ? "_blank" : undefined}
           >
             <div className="ml-10 space-x-1">
               {link.icon}
@@ -126,9 +126,9 @@ const JobseekerDrawer = ({ session }: { session: any }) => {
             ml: 5,
             mt: 3,
             mb: 3,
-            width: '123px',
-            backgroundColor: '#f6f6f6',
-            color: '#014260',
+            width: "123px",
+            backgroundColor: "#f6f6f6",
+            color: "#014260",
           }}
         >
           Log Out
@@ -139,14 +139,14 @@ const JobseekerDrawer = ({ session }: { session: any }) => {
 };
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  let { data: session, status } = useSession();
+  const { data: session, status } = useSession();
   const [mobileOpen, setMobileOpen] = useState(false);
 
   const handleDrawerToggle = useCallback(() => {
     setMobileOpen(!mobileOpen);
   }, [mobileOpen]);
 
-  if (status === 'loading') {
+  if (status === "loading") {
     return (
       <div className="min-h-screen">
         {/* Header skeleton */}
@@ -226,18 +226,18 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     return (
       <div
         style={{
-          minHeight: '100vh',
-          display: 'flex',
-          flexDirection: 'column',
+          minHeight: "100vh",
+          display: "flex",
+          flexDirection: "column",
         }}
       >
         <AppBar
           position="sticky"
           variant="outlined"
           color="inherit"
-          sx={{ zIndex: '10000', height: '76px', justifyContent: 'center' }}
+          sx={{ zIndex: "10000", height: "76px", justifyContent: "center" }}
         >
-          <Toolbar sx={{ justifyContent: 'space-between', zIndex: '20000' }}>
+          <Toolbar sx={{ justifyContent: "space-between", zIndex: "20000" }}>
             <Link href="/">
               <span className="sr-only">Tech Workforce Coalition</span>
               <Image
@@ -247,7 +247,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
                 height={50}
               />
             </Link>
-            <Stack direction={'row'}>
+            <Stack direction={"row"}>
               <IconButton
                 color="inherit"
                 aria-label="open notifications"
@@ -261,7 +261,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
                 aria-label="open drawer"
                 edge="start"
                 onClick={handleDrawerToggle}
-                sx={{ display: { xs: 'block', md: 'none' } }}
+                sx={{ display: { xs: "block", md: "none" } }}
               >
                 <Menu />
               </IconButton>
@@ -269,16 +269,16 @@ export default function Layout({ children }: { children: React.ReactNode }) {
           </Toolbar>
         </AppBar>
         <Grid2
-          sx={{ display: { xs: 'none', md: 'block' }, width: drawerWidth }}
+          sx={{ display: { xs: "none", md: "block" }, width: drawerWidth }}
         >
           <Drawer
             elevation={0}
             variant="permanent"
             anchor="right"
             sx={{
-              display: { xs: 'none', md: 'block' },
+              display: { xs: "none", md: "block" },
               width: drawerWidth,
-              '& .MuiDrawer-paper': { border: 0, width: drawerWidth },
+              "& .MuiDrawer-paper": { border: 0, width: drawerWidth },
             }}
           >
             <JobseekerDrawer session={session} />
@@ -292,9 +292,9 @@ export default function Layout({ children }: { children: React.ReactNode }) {
               keepMounted: true,
             }}
             sx={{
-              display: { xs: 'block', md: 'none' },
-              '& .MuiDrawer-paper': {
-                boxSizing: 'border-box',
+              display: { xs: "block", md: "none" },
+              "& .MuiDrawer-paper": {
+                boxSizing: "border-box",
                 width: drawerWidth,
               },
             }}
@@ -304,7 +304,11 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         </Grid2>
         <Box
           component="main"
-          sx={{ mt: '25px', flexGrow: 1, width: { md: `calc(100% - ${drawerWidth}px)` } }}
+          sx={{
+            mt: "25px",
+            flexGrow: 1,
+            width: { md: `calc(100% - ${drawerWidth}px)` },
+          }}
         >
           {children}
         </Box>
@@ -312,23 +316,23 @@ export default function Layout({ children }: { children: React.ReactNode }) {
           gap={2}
           container
           sx={{
-            position: 'relative',
-            alignItems: 'center',
-            justifyContent: 'space-between',
+            position: "relative",
+            alignItems: "center",
+            justifyContent: "space-between",
             px: 5,
             py: 2.5,
             zIndex: 9000,
-            bgcolor: '#F6F6F6',
+            bgcolor: "#F6F6F6",
           }}
         >
-          <Stack direction={{ xs: 'column', sm: 'row' }} gap={2}>
-            <Link href={'/policies/terms-of-service'}>Terms of Service</Link>
+          <Stack direction={{ xs: "column", sm: "row" }} gap={2}>
+            <Link href={"/policies/terms-of-service"}>Terms of Service</Link>
             {/*<Link href={'/underconstruction'}>Privacy Policy</Link>
             <Link href={'/underconstruction'}>Cookie Settings</Link>*/}
           </Stack>
           <div>
             <p>
-              Follow Us{' '}
+              Follow Us{" "}
               <Link
                 target="_blank"
                 href="https://www.linkedin.com/company/washington-tech-workforce-coalition"
