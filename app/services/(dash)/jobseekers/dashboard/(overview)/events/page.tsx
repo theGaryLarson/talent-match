e89506c6bb7335574db0e52818e40aa6e43324da
Rory.Hayes <rorosaurus@gmail.com>
2025-01-31 14:04:12 -0800

    initial public events page refactor

--- app/services/(dash)/jobseekers/dashboard/(overview)/events/page.tsx
+++ app/services/(dash)/jobseekers/dashboard/(overview)/events/page.tsx
@@ -1,57 +1,15 @@
-import { Card, Divider, Grid2 } from "@mui/material";
-import {
-  EventTypeEnum,
-  getAllEvents,
-  getRegisteredEvents,
-} from "@/app/lib/events";
-import Event from "@/app/ui/components/Event";
 import React from "react";
+import EventsList from "@/app/ui/components/EventsList";
 
-export default async function Page() {
-  const allEventsResponse = await getAllEvents(true);
-  const registeredEventsResponse = await getRegisteredEvents(true);
-
-  const registered = function (id: string) {
-    return registeredEventsResponse?.some((e) => e.eventId === id);
-  };
-
+export default function Page() {
   return (
     <div className="m-0 tablet:m-4 laptop:m-8 flex-1">
-      <Grid2
-        spacing={1}
-        container
-        size={1}
-        sx={{ justifyContent: "space-between" }}
+      <EventsList
+        headerText={"Registered Events"}
+        showOnlyRegisteredEvents={true}
+        showMeetingLinks={true}
       >
-        <p className="self-center text-xl font-medium text-button-secondary-idle-text">
-          Upcoming Events
-        </p>
-      </Grid2>
-      <Grid2 container sx={{ width: "100%" }}>
-        <Card
-          variant="outlined"
-          sx={{
-            width: "100%",
-            pt: 1,
-            px: 1,
-            alignItems: "center",
-          }}
-        >
-          {allEventsResponse?.events?.map((event, i, arr) => (
-            <React.Fragment key={i}>
-              <Event
-                showLink={false}
-                registered={registered(event.id)}
-                event={{
-                  ...event,
-                  eventType: event.eventType as EventTypeEnum,
-                }}
-              />
-              {i < arr.length - 1 && <Divider sx={{ my: 1 }} />}
-            </React.Fragment>
-          ))}
-        </Card>
-      </Grid2>
+      </EventsList>
     </div>
   );
 }
---
+++ app/ui/components/EventsList.tsx
@@ -0,0 +1,83 @@
+import { Card, Divider, Grid2 } from "@mui/material";
+import PillButton from "./PillButton";
+import { EventTypeEnum, getAllEvents, getRegisteredEvents } from "@/app/lib/events";
+import Event from "@/app/ui/components/Event";
+import React from "react";
+
+interface EventsListProps {
+  headerText: string;
+  showOnlyRegisteredEvents: boolean;
+  showMeetingLinks: boolean;
+}
+
+export default async function EventsList({ headerText, showOnlyRegisteredEvents, showMeetingLinks }: EventsListProps) {
+  const allEventsResponse = await getAllEvents(true);
+  const registeredEventsResponse = await getRegisteredEvents(true);
+
+  const registered = function (id: string) {
+    return registeredEventsResponse?.some((e) => e.eventId === id);
+  };
+
+  return (
+    <Grid2 container rowSpacing={2} columns={1}>
+      <Grid2
+        spacing={1}
+        container
+        size={1}
+        sx={{ justifyContent: "space-between" }}
+      >
+        <p className="self-center text-xl font-medium text-button-secondary-idle-text">
+          {headerText}
+        </p>
+        {showOnlyRegisteredEvents && <PillButton
+          href="/services/events"
+          disableElevation
+          sx={{
+            backgroundColor: "#f6f6f6",
+            color: "secondary.main",
+          }}
+        >
+          Events Calendar
+        </PillButton>}
+      </Grid2>
+      <Grid2 container sx={{ width: "100%" }}>
+        <Card
+          variant="outlined"
+          sx={{
+            width: "100%",
+            pt: 1,
+            px: 1,
+            alignItems: "center",
+          }}
+        >
+          {showOnlyRegisteredEvents && registeredEventsResponse?.map((item, i, arr) => (
+            <React.Fragment key={i}>
+              <Event
+                showLink={showMeetingLinks}
+                registered={registered(item.event.id)}
+                event={{
+                  ...item.event,
+                  eventType: item.event.eventType as EventTypeEnum,
+                }}
+              />
+              {i < arr.length - 1 && <Divider sx={{ my: 1 }} />}
+            </React.Fragment>
+          ))}
+          {!showOnlyRegisteredEvents && allEventsResponse?.events?.map((event, i, arr) => (
+            <React.Fragment key={i}>
+              <Event
+                showLink={showMeetingLinks}
+                registered={registered(event.id)}
+                event={{
+                  ...event,
+                  eventType: event.eventType as EventTypeEnum,
+                }}
+              />
+              {i < arr.length - 1 && <Divider sx={{ my: 1 }} />}
+            </React.Fragment>
+          ))}
+        </Card>
+      </Grid2>
+    </Grid2>
+  );
+}
