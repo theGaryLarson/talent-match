f5ac47818f84fd7f05bcd4dd263f607c90bcfcee
Gage Berz <gageberz@computingforall.org>
2025-02-10 13:04:48 -0500

    TalentSearch: shape should be rounded. rounded-xs is tailwind, not MUI

--- app/ui/components/TalentSearch.tsx
+++ app/ui/components/TalentSearch.tsx
@@ -452,7 +452,7 @@ export default function TalentSearch() {
         {!loading ? (
           <Pagination
             variant="outlined"
-            shape="rounded-xs"
+            shape="rounded"
             count={Math.ceil((totalResults ?? 1) / resultsPerPage)}
             page={getParam("page") != "" ? +getParam("page") : 1}
             onChange={handlePageChange}
