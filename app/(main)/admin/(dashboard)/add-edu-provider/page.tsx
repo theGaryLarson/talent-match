6269753be6f3e2b961e9a1c81b620251acf096ba
not-damien <93546644+not-damien@users.noreply.github.com>
2025-02-11 14:29:54 -0800

    add mui Button to add edu providers form

--- app/(main)/admin/(dashboard)/add-edu-provider/page.tsx
+++ app/(main)/admin/(dashboard)/add-edu-provider/page.tsx
@@ -5,6 +5,8 @@ import AvatarUpload from "@/app/ui/components/AvatarUpload";
 import SelectOptionsWithLabel from "@/app/ui/components/SelectOptionsWithLabel";
 import { v4 as uuidv4 } from "uuid";
 import { EducationLevel } from "@/data/dtos/JobSeekerProfileCreationDTOs";
+import { Button } from "@mui/material";
+import { ArrowCircleRightOutlined, HighlightOffOutlined } from "@mui/icons-material";
 
 export default function Page() {
   const [eduProviderId, setEduProviderId] = useState(uuidv4()); // eslint-disable-line @typescript-eslint/no-unused-vars
@@ -187,8 +189,21 @@ export default function Page() {
       </div>
 
       {/* Submit Button */}
-      <div>
-        <button type="submit">Create Provider</button>
+      <div className="grid grid-cols-2 gap-2">
+        <Button
+          type="reset"
+          variant="outlined"
+          startIcon={<HighlightOffOutlined />}
+        >
+          Reset Form
+        </Button>
+        <Button
+          type="submit"
+          endIcon={<ArrowCircleRightOutlined />}
+          variant="contained"
+        >
+          Create Provider
+        </Button>
       </div>
     </form>
   );
