4c42b67a3bfde8872d348a73fde401fb9131a76a
cfa-jonathan <91294137+cfa-jonathan@users.noreply.github.com>
2025-02-21 10:53:17 -0800

    Implement about us page committee updates

--- app/(main)/about-us/page.tsx
+++ app/(main)/about-us/page.tsx
@@ -356,32 +356,13 @@ export default function page() {
               </h2>
             </header>
             <p>
-              The Industry Committee, led by{" "}
-              <Link
-                href="https://www.computingforall.org/"
-                target="_blank"
-                className="underline"
-              >
-                Computing for All
-              </Link>{" "}
-              in collaboration with the{" "}
-              <Link
-                href="https://www.washingtontechnology.org/"
-                target="_blank"
-                className="underline"
-              >
-                Washington Technology Industry Association
-              </Link>{" "}
-              (WTIA), facilitates strategies for creating a robust talent
-              pipeline and methods for employers to better recruit and
-              assimilate new talent into their organization. The committee
-              supports employers by clarifying and understanding their needs,
-              identifying challenges and barriers to participation, and
-              addressing these challenges through development of new or existing
-              programs. As a committee member, you’ll attend quarterly meetings
-              and any additional focused efforts per your interest.
+              As a member of the Industry Committee, you will play a critical role in informing curriculum in collaboration with other employers to ensure it stays relevant and effectively prepares students for the workforce. Currently, the Industry Committee is prioritizing these essential roles across all sectors:
             </p>
-            <div className="pt-[40px]">
+            <blockquote className="pl-[40px]">Software Development | Cybersecurity | IT/Cloud Support | Data Analytics​</blockquote>
+            <p>
+              With guidance from AWS, Accenture, Microsoft and WTIA, this committee proactively collaborates with the Education Committee ensuring key industry trends are considered and addressed by educators. Quarterly meetings: January, April, July, and October.
+            </p>
+            <div className="mt-[40px]">
               <RoundedButton
                 content="Join"
                 link="/join"
