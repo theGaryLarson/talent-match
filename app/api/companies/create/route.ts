4424aebf234a1e88c652b9c524bd8ceb8afb2bdb
Quincy Pham <quincytpham@gmail.com>
2025-02-24 15:08:51 -0500

    Updated companies create route to try catch to handle error

--- app/api/companies/create/route.ts
+++ app/api/companies/create/route.ts
@@ -3,8 +3,16 @@ import { CompanyAdminCreationDTO } from "@/data/dtos/CompanyAdminCreationDTO";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
-  const body: CompanyAdminCreationDTO = await req.json();
-  console.log("Request body:", body);
-  const result = await adminCreateCompany(body);
-  return NextResponse.json(result);
+  try {
+    const body: CompanyAdminCreationDTO = await req.json();
+    console.log("Request body:", body);
+    const result = await adminCreateCompany(body);
+    return NextResponse.json(result);
+  } catch (error) {
+    console.error("Error in POST handler:", error);
+    return NextResponse.json(
+      { error: "Internal server error" },
+      { status: 500 },
+    );
+  }
 }
