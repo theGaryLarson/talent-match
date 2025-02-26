86ca337f4fb4dc8ba9a1aa67910bbdd413b4e774
not-damien <93546644+not-damien@users.noreply.github.com>
2025-02-26 15:09:22 -0800

    changed title of career prep grid

--- app/(main)/career-prep/(dashboard)/page.tsx
+++ app/(main)/career-prep/(dashboard)/page.tsx
@@ -1,7 +1,5 @@
-import { getAllCareerPrepStudentsCardView } from "@/app/lib/admin/careerPrep";
 import { ExecuteSqlFile } from "@/app/lib/sql/sqlUtils";
 import CareerPrepDataGrid, { CareerPrepGridData } from "@/app/ui/components/careerPrep/CareerPrepDataGrid";
-import CareerPrepStudentsCard from "@/app/ui/components/careerPrep/CareerPrepStudentsCard";
 export const metadata = {
   title: "My Dashboard",
 };
@@ -9,7 +7,7 @@ export default async function Page() {
   const clients = await ExecuteSqlFile("prisma/sql/Weekly Reports/Career Prep/CareerPrepCandidates.sql") as CareerPrepGridData[]
   return (
     <main className="space-y-3 py-8 font-['Roboto'] bg-gray-bg grow px-[50px]">
-      <h1 className="text-2xl font-medium">All Career Prep Students</h1>
+      <h1 className="text-2xl font-medium">Career Prep Candidates</h1>
       <CareerPrepDataGrid clients={clients??[]}/>
     </main>
   );
