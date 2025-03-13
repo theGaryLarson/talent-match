2cc067f5ce273136e803fbd14363d12d15fa077f
Quincy Pham <quincytpham@gmail.com>
2025-03-13 17:23:20 -0400

    Updated test to point at mui/Header instead of headless

--- tests/ui/Header.test.tsx
+++ tests/ui/Header.test.tsx
@@ -6,7 +6,7 @@ import {
   within,
 } from "@testing-library/react";
 import { describe, it, expect, vi, afterEach } from "vitest";
-import Header from "../../app/ui/Header";
+import Header from "../../app/ui/components/mui/Header";
 import { usePathname } from "next/navigation";
 import { SessionProvider } from "next-auth/react";
 
