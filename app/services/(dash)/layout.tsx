a8278e7eecf89b5782675b961a8571209b4a7231
Quincy Pham <quincytpham@gmail.com>
2025-03-14 14:09:31 -0400

    Removed headless Header.tsx and any references

--- app/services/(dash)/layout.tsx
+++ app/services/(dash)/layout.tsx
@@ -26,7 +26,7 @@ import { useSession } from "next-auth/react";
 import { useCallback, useState } from "react";
 import { usePathname } from "next/navigation";
 import PillButton from "@/app/ui/components/PillButton";
-import Header from "@/app/ui/Header";
+import Header from "@/app/ui/components/mui/Header";
 import Footer from "@/app/ui/Footer";
 
 const drawerWidth = 260;
