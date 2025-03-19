60b71ab00adbc12fbfa9c55404543848f2911299
theGaryLarson <gary.larson@seattlecolleges.edu>
2025-03-19 13:58:17 -0700

    update Projects mb to 1

--- app/services/(dash)/jobseekers/[id]/page.tsx
+++ app/services/(dash)/jobseekers/[id]/page.tsx
@@ -405,7 +405,7 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
                       direction={"row"}
                       sx={{ justifyContent: "space-between" }}
                     >
-                      <Typography variant="h5">Projects</Typography>
+                      <Typography variant="h5" sx={{ mb: 1 }}>Projects</Typography>
                       {isOwnProfile && (
                         <IconButton href="/edit-profile/jobseeker/education">
                           <EditIcon />
