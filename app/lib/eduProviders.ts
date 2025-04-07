03676f9ef190fa90bb00773618ca5af9bf6d8d78
theGaryLarson <gary.larson@seattlecolleges.edu>
2025-04-07 16:36:39 -0700

    TODO: update EduProviderPathways enum to reflect new pathways.

--- app/lib/eduProviders.ts
+++ app/lib/eduProviders.ts
@@ -79,6 +79,7 @@ export type PostEduProviderProgramDetailDTO = {
   pathways: EduProviderPathways[]; // provider_programs.pathways will need converted from string to list and each item cast into EduProviderPathways enum
 };
 
+// TODO: align with ICT Job Group Families
 export enum EduProviderPathways {
   SoftwareDeveloper = "Software Developer",
   ITCloudSupport = "IT & Cloud Support",
