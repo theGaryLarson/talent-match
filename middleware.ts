1fcea236f7fc6f3589dc2d71c5528d59b1b653bf
Gage Berz <gageberz@protonmail.ch>
2025-04-08 15:03:14 -0400

    AccountMenu: Use firstName over name

--- middleware.ts
+++ middleware.ts
@@ -21,6 +21,7 @@ export default auth((req) => {
       "/api/employers/create",
     ],
     [Role.JOBSEEKER]: [
+      "/pdf.worker.min.mjs",
       "/edit-profile/jobseeker/",
       "/services/jobseekers/career-prep/skill-assessment",
       "/services/jobseekers/career-prep/enrollment",
