dca92e5b44830ddfce1177db5184c2caf2fda7a5
Gage Berz <gageberz@protonmail.ch>
2025-04-17 11:40:26 -0400

    ApplyToJobButton: Button is Apply and redirects to booking appointment with career services.
    Adds loading indicators to the apply and withdraw actions. Redirects users to the appointment booking page after successfully applying. Removes the apply/withdraw button from the job listing card view.

--- app/ui/components/jobPostings/JobListingCardView.tsx
+++ app/ui/components/jobPostings/JobListingCardView.tsx
@@ -152,7 +152,6 @@ function JobListingCardView({
       )}
       <Stack
         direction={{ xs: "column", sm: "row" }}
-        gap={1}
         sx={{ mt: 2, justifyContent: "flex-end" }}
       >
         <PillButton
@@ -163,13 +162,6 @@ function JobListingCardView({
         >
           View job posting
         </PillButton>
-        {(!session?.user || isJobseeker) && (
-          <ApplyToJobButton
-            id={joblisting.job_posting_id}
-            appliedStatus={joblisting.jobStatus}
-            unPublishDate={joblisting.unpublish_date}
-          />
-        )}
       </Stack>
     </Card>
   );
