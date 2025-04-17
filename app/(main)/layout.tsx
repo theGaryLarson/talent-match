ae1fe7d2cf2daabe9eb63493671bdc76c5ff1d79
not-damien <93546644+not-damien@users.noreply.github.com>
2025-04-16 18:48:52 -0700

    make build happy

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,6 +1,5 @@
 import Header from "@/app/ui/components/mui/Header";
 import Footer from "@/app/ui/Footer";
-import GreenMarketingBanner from "../ui/components/GreenMarketingBanner";
 
 export default function RootLayout({
   children,
